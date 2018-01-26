c5c6e4147b1170e6044239b501f4929e2d17d655
Jon Edvald <edvald@gmail.com>
2018-01-26 13:36:56 +0100

    Flipped dependency order in hello-world project and added very basic server to hello-container

--- examples/hello-world/services/hello-function/index.js
+++ examples/hello-world/services/hello-function/index.js
@@ -4,6 +4,4 @@
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.helloWorld = (req, res) => {
-  res.send('Hello World!');
-};
+exports.helloWorld = (req, res) => res.send(process.env.HELLO_WORLD_MESSAGE)
