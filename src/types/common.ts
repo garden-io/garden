bf3af9d3a6b212d2cde27695fae402aaef69ca17
Jon Edvald <edvald@gmail.com>
2018-01-26 13:35:57 +0100

    Tightened constraints on identifier names (to match k8s standards and avoid potential uncaught naming conflicts)

--- src/types/common.ts
+++ src/types/common.ts
@@ -5,12 +5,13 @@ export type Primitive = string | number | boolean
 
 export interface PrimitiveMap { [key: string]: Primitive }
 
-export const identifierRegex = /^[a-z0-9][\w\d\-]*$/i
 export const JoiPrimitive = () => Joi.alternatives().try(Joi.number(), Joi.string(), Joi.boolean())
+
+export const identifierRegex = /^[a-z0-9][a-z0-9\-]*$/
 
 export const JoiIdentifier = () => Joi
   .string().regex(identifierRegex)
-  .description("may contain letters, numbers, underscores and dashes and must start with a letter or number")
+  .description("may contain lowercase letters, numbers and dashes and must start with a letter or number")
 
 export interface Environment {
   name: string
