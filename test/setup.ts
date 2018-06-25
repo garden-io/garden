ff762ae2b9be1970138f99965ef744c3a7d0b2c7
Jon Edvald <edvald@gmail.com>
2018-06-26 00:16:56 +0200

    chore: add mocha.debug.opts for running/debugging individual modules

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,12 @@
 import * as td from "testdouble"
 import { Module } from "../src/types/module"
+import { RootLogNode } from "../src/logger/logger"
+import { LogLevel } from "../src/logger/types"
+
+// make sure logger is initialized
+try {
+  RootLogNode.initialize({ level: LogLevel.info })
+} catch (_) { }
 
 // Global hooks
 beforeEach(() => { })
