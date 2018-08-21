e68584defd6025f4bc562f263699929040d38ae7
Eyþór Magnússon <eysi09@gmail.com>
2018-08-21 10:17:28 +0200

    chore: add missing license headers

--- garden-cli/src/config/config-context.ts
+++ garden-cli/src/config/config-context.ts
@@ -5,6 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 import { isString, flatten } from "lodash"
 import { Module } from "../types/module"
 import { PrimitiveMap, isPrimitive, Primitive, joiIdentifierMap, joiStringMap, joiPrimitive } from "./common"
