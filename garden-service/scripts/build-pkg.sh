b3437f6eeb42ef2f0797bd0b04aa42ec97ce6060
Jon Edvald <edvald@gmail.com>
2018-10-28 20:57:14 +0100

    chore(release): fix minor issues in pkg build script

--- garden-service/scripts/build-pkg.sh
+++ garden-service/scripts/build-pkg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-garden_cli_root=$(cd `dirname $0` && cd .. && pwd)
+garden_service_root=$(cd `dirname $0` && cd .. && pwd)
 
-cd ${garden_cli_root}
+cd ${garden_service_root}
 
 commit_hash=$(git rev-parse --short HEAD)
 version=$(cat package.json | jq -r .version)
@@ -11,12 +11,11 @@ echo "Packaging version ${version}-${commit_hash}"
 
 echo "-> Copying files to tmp build dir..."
 mkdir -p dist
+rm -rf tmp/dist
 mkdir -p tmp/dist
 
-cp -r package.json build static node_modules tmp/dist
-
-echo "-> Cleaning up .garden directories..."
-find tmp/dist -depth -type d -name ".garden" -exec rm -r "{}" \;
+cp -r package.json build node_modules tmp/dist
+rsync -r --exclude=.garden --exclude=.git static tmp/dist
 
 echo "-> Building executables..."
 cd dist
@@ -25,18 +24,18 @@ pkg --target node10-linux-x64,node10-win-x64 ../tmp/dist
 
 echo "-> Preparing packages..."
 
-echo "  -> linux-x64"
-rm -rf linux-x64
-mkdir linux-x64
-mv garden-cli-linux linux-x64/garden
-cp -r ../tmp/dist/static linux-x64
-tar -czf garden-${version}-${commit_hash}.tar.gz linux-x64
+echo "  -> linux-amd64"
+rm -rf linux-amd64
+mkdir linux-amd64
+mv garden-cli-linux linux-amd64/garden
+cp -r ../tmp/dist/static linux-amd64
+tar -czf garden-${version}-${commit_hash}-linux-amd64.tar.gz linux-amd64
 
-echo "  -> win-x64"
-rm -rf win-x64
-mkdir win-x64
-mv garden-cli-win.exe win-x64/garden.exe
-cp -r ../tmp/dist/static win-x64
-zip -q -r garden-${version}-${commit_hash}.zip win-x64
+echo "  -> win-amd64"
+rm -rf win-amd64
+mkdir win-amd64
+mv garden-cli-win.exe win-amd64/garden.exe
+cp -r ../tmp/dist/static win-amd64
+zip -q -r garden-${version}-${commit_hash}-win-amd64.zip win-amd64
 
 echo "Done!"
