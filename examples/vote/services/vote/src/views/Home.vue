31fef81cb9c4b31846e28e3c588d9ce7135e1b28
David Rubin <davidrub@gmail.com>
2018-12-03 14:22:01 +0100

    chore(examples): update vote example to showcase more Garden features
    * Renamed the multi-container example to vote.
    * Allow multiple votes per session.
    * Added front-end service and basic README.
    * Add missing service dependencies to the relevant modules' config.
    * Added a health check to the db (postgres) service, and moved the
    table creation to a task. Removed logic from jworker accordingly.

---
+++ examples/vote/services/vote/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <Vote optionA="Cats" optionB="Dogs"/> -->
+    <Vote optionA="Pizza" optionB="Hot Dogs"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Vote from '@/components/Vote.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Vote,
+  },
+};
+</script>
