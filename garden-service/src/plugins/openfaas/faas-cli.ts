7915008b9e34708ee78c16dab11f9f8bab5d0aba
Jon Edvald <edvald@gmail.com>
2019-03-05 15:12:00 +0100

    fix(openfaas): update faas-cli to 0.8.3

--- garden-service/src/plugins/openfaas/faas-cli.ts
+++ garden-service/src/plugins/openfaas/faas-cli.ts
@@ -12,13 +12,16 @@ export const faasCli = new BinaryCmd({
   name: "faas-cli",
   specs: {
     darwin: {
-      url: "https://github.com/openfaas/faas-cli/releases/download/0.7.3/faas-cli-darwin",
+      url: "https://github.com/openfaas/faas-cli/releases/download/0.8.3/faas-cli-darwin",
+      sha256: "fe3d7933189e234fe9a395ed685584cafac9b36013c3898d1c0dc046a0bdd127",
     },
     linux: {
-      url: "https://github.com/openfaas/faas-cli/releases/download/0.7.3/faas-cli",
+      url: "https://github.com/openfaas/faas-cli/releases/download/0.8.3/faas-cli",
+      sha256: "d6a633248b89f4d72ee7113e33e1489e016f111472f5669ff37a01730d20445a",
     },
     win32: {
-      url: "https://github.com/openfaas/faas-cli/releases/download/0.7.3/faas-cli.exe",
+      url: "https://github.com/openfaas/faas-cli/releases/download/0.8.3/faas-cli.exe",
+      sha256: "07a191342c7cbbf3d27dbca13a3d318e6eb8941bf055eef09c6e65ba93c77d80",
     },
   },
 })
