fcc5bd5c4d31f7d338923e6fbe8875d9c04bf70c
Jordy Baylac <jbaylacc@gmail.com>
2019-03-06 01:35:55 -0500

    chore(release): fix windows installer release url

--- support/install.ps1
+++ support/install.ps1
@@ -122,7 +122,7 @@ md -Force $gardenBinPath | Out-Null
 md -Force $gardenTmpPath | Out-Null
 
 # Download and extract the archive to $gardenBinPath
-$latestRelease = Invoke-WebRequest "https://github.com/garden-io/releases/latest" -Headers @{"Accept"="application/json"}
+$latestRelease = Invoke-WebRequest "https://github.com/garden-io/garden/releases/latest" -Headers @{"Accept"="application/json"}
 # The releases are returned in the format {"id":3622206,"tag_name":"hello-1.0.0.11",...}, we have to extract the tag_name.
 $json = $latestRelease.Content | ConvertFrom-Json
 $latestVersion = $json.tag_name
