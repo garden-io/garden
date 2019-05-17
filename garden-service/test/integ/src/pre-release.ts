1a12f53a9bc0b7e08189872853d28040ccccf713
Eyþór Magnússon <eysi09@gmail.com>
2019-05-17 23:42:23 +0200

    ci: run integ tests

--- garden-service/test/integ/src/pre-release.ts
+++ garden-service/test/integ/src/pre-release.ts
@@ -20,13 +20,63 @@ import {
   removeExampleDotGardenDirs,
 } from "../../integ-helpers"
 
-const prereleaseSequences = ["demo-project", "hello-world", "tasks", "vote-helm", "remote-sources"]
-const sequencesToRun = parsedArgs["only"] ? [parsedArgs["only"]] : prereleaseSequences
+type ProjectName =
+  "demo-project" |
+  "hot-reload" |
+  "hello-world" |
+  "tasks" |
+  "vote" |
+  "vote-helm" |
+  "remote-sources"
+
+const prereleaseSequences: ProjectName[] = [
+  "demo-project",
+  "hello-world",
+  "tasks",
+  "vote",
+  "vote-helm",
+  "remote-sources",
+]
+
+export const sequencesToRun = parsedArgs["only"] ? [parsedArgs["only"]] : prereleaseSequences
+const env = parsedArgs["env"]
+const timestamp = Date.now()
+
+export function getProjectNamespace(project: ProjectName) {
+  return `${project}-testing-${timestamp}`
+}
+
+async function runWithOpts(project: ProjectName, command: string[]) {
+  const dir = resolve(examplesDir, project)
+  const namespace = getProjectNamespace(project)
+  if (env) {
+    command.push("--env", env)
+  }
+  return runGarden(dir, command, { NAMESPACE: namespace })
+}
+
+function getGardenWatch(project: ProjectName, command: string[]) {
+  const dir = resolve(examplesDir, project)
+  const namespace = getProjectNamespace(project)
+  if (env) {
+    command.push("--env", env)
+  }
+  return new GardenWatch(dir, command, { NAMESPACE: namespace })
+}
+
+// We need to force build before deploying to a remote env, see: https://github.com/garden-io/garden/issues/767
+async function forceBuildIfRemote(project: ProjectName) {
+  // Assume env is remote if passed as arg
+  if (env) {
+    mlog.log("force building project", project)
+    await runWithOpts(project, ["build", "--force"])
+    mlog.log("finished force building")
+  }
+}
 
 // TODO: Add test for verifying that CLI returns with an error when called with an unknown command
-
 describe("PreReleaseTests", () => {
-  const demoProjectPath = resolve(examplesDir, "demo-project")
+  const namespaces = sequencesToRun.map(p => getProjectNamespace(p))
 
   before(async () => {
     mlog.log("deleting .garden folders")
@@ -34,28 +84,40 @@ describe("PreReleaseTests", () => {
   })
 
   after(async () => {
+    mlog.log("deleting example namespaces")
+    // FIXME: This should just be a fire and forget without waiting for the function to return.
+    // However, it actually does wait until every namespace is deleted before returning.
+    // This adds a lot of time to the test run.
+    // tslint:disable-next-line: no-floating-promises
+    deleteExampleNamespaces(namespaces)
     mlog.log("checking out example project directories to HEAD")
     await execa("git", ["checkout", examplesDir])
   })
 
-  if (sequencesToRun.includes("demo-project")) {
-    describe("demo-project: top-level sanity checks", () => {
+  if (sequencesToRun.includes("simple-project")) {
+    describe("simple-project: top-level sanity checks", () => {
+      const demoProjectPath = resolve(examplesDir, "demo-project")
+
+      before(async () => {
+        await forceBuildIfRemote("demo-project")
+      })
+
       it("runs the validate command", async () => {
-        await runGarden(demoProjectPath, ["validate"])
+        await runWithOpts("demo-project", ["validate"])
       })
 
       it("runs the deploy command", async () => {
-        const logEntries = await runGarden(demoProjectPath, ["deploy"])
+        const logEntries = await runWithOpts("demo-project", ["deploy"])
         expect(searchLog(logEntries, /Done!/), "expected to find 'Done!' in log output").to.eql("passed")
       })
 
       it("runs the test command", async () => {
-        const logEntries = await runGarden(demoProjectPath, ["test"])
+        const logEntries = await runWithOpts("demo-project", ["test"])
         expect(searchLog(logEntries, /Done!/), "expected to find 'Done!' in log output").to.eql("passed")
       })
 
       it("runs the dev command", async () => {
-        const gardenWatch = new GardenWatch(demoProjectPath, ["dev"])
+        const gardenWatch = getGardenWatch("demo-project", ["dev"])
 
         const testSteps = [
           taskCompletedStep("deploy.backend", 1),
@@ -70,19 +132,17 @@ describe("PreReleaseTests", () => {
 
         await gardenWatch.run({ testSteps })
       })
-
-      after(async () => {
-        await deleteExampleNamespaces(["demo-project"])
-      })
     })
   }
 
   if (sequencesToRun.includes("tasks")) {
     describe("tasks", () => {
-      const tasksProjectPath = resolve(examplesDir, "tasks")
+      before(async () => {
+        await forceBuildIfRemote("tasks")
+      })
 
       it("runs the deploy command", async () => {
-        const logEntries = await runGarden(tasksProjectPath, ["deploy"])
+        const logEntries = await runWithOpts("tasks", ["deploy"])
         expect(searchLog(logEntries, /Done!/), "expected to find 'Done!' in log output").to.eql("passed")
       })
 
@@ -91,13 +151,9 @@ describe("PreReleaseTests", () => {
          * Verify that the output includes the usernames populated by the ruby-migration task.
          * The users table was created by the node-migration task.
          */
-        const logEntries = await runGarden(tasksProjectPath, ["call", "hello"])
+        const logEntries = await runWithOpts("tasks", ["call", "hello"])
         expect(searchLog(logEntries, /John, Paul, George, Ringo/), "expected to find populated usernames in log output")
           .to.eql("passed")
-      })
-
-      after(async () => {
-        await deleteExampleNamespaces(["tasks"])
       })
     })
   }
@@ -111,10 +167,9 @@ describe("PreReleaseTests", () => {
     */
     describe.skip("hot-reload", () => {
 
-      const hotReloadProjectPath = resolve(examplesDir, "hot-reload")
-
       it("runs the dev command with hot reloading enabled", async () => {
-        const gardenWatch = new GardenWatch(hotReloadProjectPath, ["dev", "--hot=node-service"])
+        const hotReloadProjectPath = resolve(examplesDir, "hot-reload")
+        const gardenWatch = getGardenWatch("hot-reload", ["dev", "--hot=node-service"])
 
         const testSteps = [
           dashboardUpStep(),
@@ -131,7 +186,7 @@ describe("PreReleaseTests", () => {
           {
             description: "node-service returns the updated response text",
             condition: async () => {
-              const callLogEntries = await runGarden(hotReloadProjectPath, ["call", "node-service"])
+              const callLogEntries = await runWithOpts("hot-reload", ["call", "node-service"])
               return searchLog(callLogEntries, /Hello from Edge/)
             },
           },
@@ -140,19 +195,22 @@ describe("PreReleaseTests", () => {
         await gardenWatch.run({ testSteps })
 
       })
-
-      after(async () => {
-        await deleteExampleNamespaces(["hot-reload"])
-      })
     })
   }
 
   if (sequencesToRun.includes("vote-helm")) {
-    describe("vote-helm: helm & dependency calculations", () => {
+    /*
+    * TODO: Re-enable once https://github.com/garden-io/garden/issues/780 is resolved
+    */
+    describe.skip("vote-helm: helm & dependency calculations", () => {
+      const voteHelmProjectPath = resolve(examplesDir, "vote-helm")
+
+      before(async () => {
+        await forceBuildIfRemote("vote-helm")
+      })
 
       it("runs the dev command", async () => {
-        const voteHelmProjectPath = resolve(examplesDir, "vote-helm")
-        const gardenWatch = new GardenWatch(voteHelmProjectPath, ["dev"])
+        const gardenWatch = getGardenWatch("vote-helm", ["dev"])
 
         const testSteps = [
           waitingForChangesStep(),
@@ -166,30 +224,48 @@ describe("PreReleaseTests", () => {
         await gardenWatch.run({ testSteps })
 
       })
+    })
+  }
 
-      after(async () => {
-        await deleteExampleNamespaces(["vote-helm"])
+  if (sequencesToRun.includes("vote")) {
+    describe("vote: dependency calculations", () => {
+      const voteProjectPath = resolve(examplesDir, "vote")
+
+      before(async () => {
+        await forceBuildIfRemote("vote")
+      })
+
+      it("runs the dev command", async () => {
+        const gardenWatch = getGardenWatch("vote", ["dev"])
+
+        const testSteps = [
+          waitingForChangesStep(),
+          changeFileStep(resolve(voteProjectPath, "services/api/app.py"), "change services/api/app.py"),
+          taskCompletedStep("build.api", 2),
+          taskCompletedStep("deploy.api", 2),
+          taskCompletedStep("deploy.vote", 2),
+        ]
+
+        await gardenWatch.run({ testSteps })
       })
     })
   }
 
   if (sequencesToRun.includes("remote-sources")) {
     describe("remote sources", () => {
-      const remoteSourcesProjectPath = resolve(examplesDir, "remote-sources")
+      before(async () => {
+        await forceBuildIfRemote("remote-sources")
+      })
 
       it("runs the deploy command", async () => {
-        const logEntries = await runGarden(remoteSourcesProjectPath, ["deploy"])
+        const logEntries = await runWithOpts("remote-sources", ["deploy"])
         expect(searchLog(logEntries, /Done!/), "expected to find 'Done!' in log output").to.eql("passed")
       })
 
       it("calls the result service to get a 200 OK response including the HTML for the result page", async () => {
-        const logEntries = await runGarden(remoteSourcesProjectPath, ["call", "result"])
+        const logEntries = await runWithOpts("remote-sources", ["call", "result"])
         expect(searchLog(logEntries, /200 OK/), "expected to find '200 OK' in log output").to.eql("passed")
         expect(searchLog(logEntries, /Cats/), "expected to find 'Cats' in log output").to.eql("passed")
-      })
-
-      after(async () => {
-        await deleteExampleNamespaces(["remote-sources"])
       })
     })
   }
