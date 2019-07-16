15fa13ae651ed078fc435c798fe36050c04f9d78
Jon Edvald <edvald@gmail.com>
2019-07-16 19:52:09 +0200

    chore(build): use static binary in container image builds

--- garden-service/bin/push-containers.sh
+++ garden-service/bin/push-containers.sh
@@ -7,15 +7,7 @@ cd ${garden_service_root}
 args=( $@ )
 version=${args[0]:-$(git rev-parse --short HEAD)}
 
-echo "Building version ${version}"
-
-cd ..
-# Run build from root, to make sure dashboard is built as well
-npm run build
-cd ${garden_service_root}
-
-docker build -t gardendev/garden:${version} .
-docker build -t gardendev/garden-gcloud:${version} --build-arg TAG=${version} -f gcloud.Dockerfile .
+./bin/build-containers.sh "$@"
 
 echo "Pushing images"
 
