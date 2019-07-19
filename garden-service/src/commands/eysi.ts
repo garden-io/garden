9930a47dad5faa3b418da8620715a53a83415940
Jon Edvald <edvald@gmail.com>
2019-07-19 15:27:26 +0200

    improvement(core): make contributor more visible in CLI
    Some of my finest work, I think you'll find.

---
+++ garden-service/src/commands/eysi.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2018 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import {
+  Command,
+  CommandResult,
+} from "./base"
+import dedent = require("dedent")
+import { readFile } from "fs-extra"
+import { STATIC_DIR } from "../constants"
+import { join } from "path"
+import execa = require("execa")
+
+export class EysiCommand extends Command {
+  name = "eysi"
+  help = "Meet our CTO."
+
+  description = dedent`
+    Just try it.
+  `
+
+  loggerType: "basic"
+
+  async action(): Promise<CommandResult> {
+    const eysi = (await readFile(join(STATIC_DIR, "eysi.txt"))).toString()
+    console.log(eysi)
+
+    try {
+      // Close enough.
+      await execa("say", ["Hello", "I", "am", "Aysey"])
+    } catch (_) { }
+
+    return { result: { eysi } }
+  }
+}
