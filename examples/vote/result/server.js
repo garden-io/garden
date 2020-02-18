84f2edff1007773ba844a49d2ea9b2cc3c0d0d9f
Jon Edvald <edvald@gmail.com>
2020-02-18 13:15:16 +0100

    chore: update postgres version in our example projects

--- examples/vote/result/server.js
+++ examples/vote/result/server.js
@@ -1,14 +1,14 @@
 var express = require('express'),
-    async = require('async'),
-    path = require("path")
-    pg = require("pg"),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
-    // io = require('socket.io')(server, { path: "/result/views/socket.io" });
+  async = require('async'),
+  path = require("path"),
+  pg = require("pg"),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  app = express(),
+  server = require('http').Server(app),
+  io = require('socket.io')(server);
+// io = require('socket.io')(server, { path: "/result/views/socket.io" });
 
 io.set('transports', ['polling']);
 
@@ -16,7 +16,7 @@ var port = process.env.PORT || 4000;
 
 io.sockets.on('connection', function (socket) {
 
-  socket.emit('message', { text : 'Welcome!' });
+  socket.emit('message', { text: 'Welcome!' });
 
   socket.on('subscribe', function (data) {
     socket.join(data.channel);
@@ -24,16 +24,22 @@ io.sockets.on('connection', function (socket) {
 });
 
 async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
+  { times: 1000, interval: 1000 },
+  function (callback) {
+    const database = process.env.PGDATABASE
+    const username = process.env.PGUSER
+    const password = process.env.PGPASSWORD
+
+    const url = `postgres://${username}:${password}@db/${database}`
+
+    pg.connect(url, function (err, client, done) {
       if (err) {
         console.error("Waiting for db");
       }
       callback(err, client);
     });
   },
-  function(err, client) {
+  function (err, client) {
     if (err) {
       return console.err("Giving up");
     }
@@ -43,7 +49,7 @@ async.retry(
 );
 
 function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
+  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function (err, result) {
     if (err) {
       console.error("Error performing query: " + err);
     } else {
@@ -51,12 +57,12 @@ function getVotes(client) {
       io.sockets.emit("scores", JSON.stringify(votes));
     }
 
-    setTimeout(function() {getVotes(client) }, 1000);
+    setTimeout(function () { getVotes(client) }, 1000);
   });
 }
 
 function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
+  var votes = { a: 0, b: 0 };
 
   result.rows.forEach(function (row) {
     votes[row.vote] = parseInt(row.count);
@@ -68,7 +74,7 @@ function collectVotesFromResult(result) {
 app.use(cookieParser());
 app.use(bodyParser());
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
