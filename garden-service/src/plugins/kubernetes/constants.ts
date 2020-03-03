f7889c131083caf2d634991596bec5e8105c5e3f
Jon Edvald <edvald@gmail.com>
2020-03-03 18:37:58 +0100

    fix(k8s): error when task logs were longer than 500kB
    This fix is a hacky temporary fix, just to get rid of the error case.
    A better solution would be to store logs as files somehow, e.g. in a
    persistent volume.

--- garden-service/src/plugins/kubernetes/constants.ts
+++ garden-service/src/plugins/kubernetes/constants.ts
@@ -10,7 +10,9 @@ export const RSYNC_PORT = 873
 export const CLUSTER_REGISTRY_PORT = 5000
 export const CLUSTER_REGISTRY_DEPLOYMENT_NAME = "garden-docker-registry"
 export const MAX_CONFIGMAP_DATA_SIZE = 1024 * 1024 // max ConfigMap data size is 1MB
-export const MAX_RUN_RESULT_OUTPUT_LENGTH = 900 * 1024 // max ConfigMap data size is 1MB, so 900kB gives enough margin
+// max ConfigMap data size is 1MB but we need to factor in overhead, plus in some cases the log is duplicated in
+// the outputs field, so we cap at 250kB.
+export const MAX_RUN_RESULT_LOG_LENGTH = 250 * 1024
 
 export const dockerAuthSecretName = "builder-docker-config"
 export const dockerAuthSecretKey = ".dockerconfigjson"
