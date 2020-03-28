2f5a2146ad8484202549c9f2f0eaffc1f12496cd
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2020-03-28 20:05:48 +0100

    docs: correct spelling of against

--- garden-service/src/config/project.ts
+++ garden-service/src/config/project.ts
@@ -97,7 +97,7 @@ export const environmentSchema = () =>
       Flag the environment as a production environment.
 
       Setting this flag to \`true\` will activate the protection on the \`deploy\`, \`test\`, \`task\`, \`build\`,
-      and \`dev\` commands. A protected command will ask for a user confirmation every time is run agains
+      and \`dev\` commands. A protected command will ask for a user confirmation every time is run against
       an environment marked as production.
       Run the command with the "--yes" flag to skip the check (e.g. when running Garden in CI).
 
@@ -285,7 +285,7 @@ export const projectDocsSchema = () =>
         .description((<any>environmentsSchema().describe().flags).description),
       providers: joiArray(providerConfigBaseSchema()).description(
         "A list of providers that should be used for this project, and their configuration. " +
-          "Please refer to individual plugins/providers for details on how to configure them."
+        "Please refer to individual plugins/providers for details on how to configure them."
       ),
       defaultEnvironment: joi
         .string()
