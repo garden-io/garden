1a08593e7c2710a8fc8627246703b171fb977ed3
Daniel Killenberger <daniel.killenberger@gmail.com>
2020-06-23 15:57:23 +0200

    feat: add garden-dev/garden-azure docker image (#1893)
    * feat: add garden-dev/garden-azure docker image
    
    * ci: add build-docker-azure step to circleci config

--- garden-service/bin/build-containers.sh
+++ garden-service/bin/build-containers.sh
@@ -9,6 +9,7 @@ version=${args[0]:-$(git rev-parse --short HEAD)}
 
 base_tag=gardendev/garden:${version}
 aws_tag=gardendev/garden-aws:${version}
+azure_tag=gardendev/garden-azure:${version}
 gcloud_tag=gardendev/garden-gcloud:${version}
 aws_gcloud_tag=gardendev/garden-aws-gcloud:${version}
 buster_tag=gardendev/garden:${version}-buster
@@ -25,6 +26,11 @@ docker build -t ${aws_tag} --build-arg TAG=${version} -f aws.Dockerfile .
 echo "-> Check ${aws_tag}"
 docker run --rm -it ${aws_tag} version
 docker run --rm -it --entrypoint=aws ${aws_tag} --version
+
+echo "-> Build ${azure_tag}"
+docker build -t ${azure_tag} --build-arg TAG=${version} -f azure.Dockerfile .
+echo "-> Check ${azure_tag}"
+docker run --rm -it ${azure_tag} version
 
 echo "-> Build ${gcloud_tag}"
 docker build -t ${gcloud_tag} --build-arg TAG=${version} -f gcloud.Dockerfile .
