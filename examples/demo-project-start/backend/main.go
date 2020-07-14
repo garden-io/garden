a517943b68c672f7b085dd47fc52f00a5cffff7e
Eyþór Magnússon <eysi09@gmail.com>
2020-07-14 20:41:19 +0200

    docs: fix broken test in starter project

--- examples/demo-project-start/backend/main.go
+++ examples/demo-project-start/backend/main.go
@@ -9,10 +9,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello from Go!")
 }
 
-
 func main() {
 	http.HandleFunc("/hello-backend", handler)
 	fmt.Println("Server running...")
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8080", nil)
 }
