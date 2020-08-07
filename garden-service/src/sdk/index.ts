04518f97c395c98163c58b8e55773a56948ef19e
Jon Edvald <edvald@gmail.com>
2020-08-07 12:45:20 +0200

    refactor(core): start collecting SDK types and functions in one place

---
+++ garden-service/src/sdk/index.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2018-2020 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export { createGardenPlugin } from "../types/plugin/plugin"
+export { GardenModule } from "../types/module"
+export { GenericProviderConfig as ProviderConfig, Provider } from "../config/provider"
