7fa943c96a4ec4eb91951e7cfd0b810e7411c36a
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2020-11-04 15:47:14 +0100

    fix(enterprise): don't resolve config on login
    Fixes an issue where using secrets outside of providers in project
    configuration would break the login command.

--- core/src/commands/login.ts
+++ core/src/commands/login.ts
@@ -10,12 +10,19 @@ import { Command, CommandParams, CommandResult } from "./base"
 import { printHeader } from "../logger/util"
 import dedent = require("dedent")
 import { login } from "../enterprise/auth"
-import { ConfigurationError } from "../exceptions"
+import { CommandError, ConfigurationError } from "../exceptions"
+import { findProjectConfig } from "../config/base"
 
 export class LoginCommand extends Command {
   name = "login"
   help = "Log in to Garden Enterprise."
   hidden = true
+
+  /**
+   * Since we're logging in, we don't want to resolve e.g. the project config (since it may use secrets, which are
+   * only available after we've logged in).
+   */
+  noProject = true
 
   description = dedent`
     Logs you in to Garden Enterprise. Subsequent commands will have access to enterprise features.
@@ -23,7 +30,16 @@ export class LoginCommand extends Command {
 
   async action({ garden, log, headerLog }: CommandParams): Promise<CommandResult> {
     printHeader(headerLog, "Login", "cloud")
-    const enterpriseDomain = garden.enterpriseDomain
+    // Since this command has `noProject = true`, `garden` only has a placeholder project config.
+    // So we find and load it here, without resolving any template strings.
+    const currentDirectory = garden.projectRoot
+    const projectConfig = await findProjectConfig(garden.projectRoot)
+    if (!projectConfig) {
+      throw new CommandError(`Not a project directory (or any of the parent directories): ${currentDirectory}`, {
+        currentDirectory,
+      })
+    }
+    const enterpriseDomain = projectConfig.domain
     if (!enterpriseDomain) {
       throw new ConfigurationError(`Error: Your project configuration does not specify a domain.`, {})
     }
