c9650fab9a1d7038fd61f3a350d0606c4b33e0a9
Jon Edvald <edvald@gmail.com>
2021-01-17 22:08:26 +0100

    chore(examples): update vote and vote-helm examples
    This fixes an issue with hostname conflicts and also cleans them up
    a bit, and a small general update to how they flow.

--- examples/vote-helm/vote-image/tests/integ/test.js
+++ examples/vote-helm/vote-image/tests/integ/test.js
@@ -1,14 +1,21 @@
 const axios = require('axios');
 const { expect } = require('chai');
 
-describe('POST /vote', () => {
-  it('respond with message from hello-function', async () => {
+describe('GET /', () => {
+  it('respond with 200', async () => {
+    const result = await axios.get('http://vote', {});
+    expect(result.status).to.eql(200);
+  });
+});
+
+describe('POST /api/vote', () => {
+  it('respond with 200', async () => {
     const headers = {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Access-Control-Allow-Origin': '*',
     };
     const voteResult = 'a';
-    const result = await axios.post('http://api/vote/', `vote=${voteResult}`, { headers });
+    const result = await axios.post('http://vote/api/vote', `vote=${voteResult}`, { headers });
     expect(result.status).to.eql(200);
   });
 });
