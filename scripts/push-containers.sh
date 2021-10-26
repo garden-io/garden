407b43a416608abbe06ac47d44a71bc6a9b03eca
Eyþór Magnússon <eysi09@gmail.com>
2021-10-26 00:51:18 -0700

    chore: publish aws-gcloud-azure Docker image on releases

--- scripts/push-containers.sh
+++ scripts/push-containers.sh
@@ -16,4 +16,5 @@ docker push gardendev/garden-aws:${version}
 docker push gardendev/garden-azure:${version}
 docker push gardendev/garden-gcloud:${version}
 docker push gardendev/garden-aws-gcloud:${version}
+docker push gardendev/garden-aws-gcloud-azure:${version}
 docker push gardendev/garden:${version}-buster
