4f0b5c13f1fb71a81343961366639b4f1d971dd7
Mikael Hoegqvist Tabor <mikael@garden.io>
2022-02-03 11:46:36 +0100

    test(terraform): removed tf syntax warnings

--- core/test/data/test-projects/terraform-provider/tf/foo.tf
+++ core/test/data/test-projects/terraform-provider/tf/foo.tf
@@ -2,6 +2,10 @@ variable "my-variable" {
   type = string
 }
 
+variable "env" {
+  type = string
+}
+
 resource "local_file" "test-file" {
   content  = terraform.workspace
   filename = "${path.module}/test.log" # using .log extension so that it's ignored by git
