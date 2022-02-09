c1538f8de155f6266c8a4e25d7dadb54d640b2e2
Jon Edvald <edvald@gmail.com>
2022-02-09 23:27:37 +0100

    chore(test): allow integ tests to run in parallel (WIP)

---
+++ core/test/integ/setup.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2018-2021 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+// This is used in some tests and test projects to alter behavior
+process.env.INTEG = "1"
+
+require("../setup")
