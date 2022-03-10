487cac8135fd0afad4637fc88d945d8af2d1ed86
Jon Edvald <edvald@gmail.com>
2022-03-10 22:49:13 +0100

    fix(k8s): sync error with space in user home directory name

--- core/src/plugins/kubernetes/mutagen.ts
+++ core/src/plugins/kubernetes/mutagen.ts
@@ -578,7 +578,7 @@ export async function getKubectlExecDestination({
   targetPath: string
 }) {
   const kubectl = ctx.tools["kubernetes.kubectl"]
-  const kubectlPath = await kubectl.getPath(log)
+  const kubectlPath = (await kubectl.getPath(log)).replaceAll(" ", "\\ ") // Escape spaces in path
 
   const connectionOpts = prepareConnectionOpts({
     provider: ctx.provider,
