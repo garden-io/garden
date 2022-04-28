6e09e1095bd4150e5c8fe26a7c7a8b43b6ccf123
Orzelius <albertkostusev@gmail.com>
2022-04-28 14:06:44 +0200

    chore: update gatsby example to use dev mode code sync

--- examples/gatsby-hot-reload/src/pages/index.js
+++ examples/gatsby-hot-reload/src/pages/index.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1 style={{color: '#000000'}}>Hot reload demo</h1>
-    <p>We're using <a href="https://docs.garden.io/guides/hot-reload">Garden's hot reload</a> functionality in combination with <a href="https://github.com/gatsbyjs/gatsby">Gatsby</a> (running inside a container) to live-update this page.</p>
+    <h1 style={{color: '#000000'}}>Code Synchronization</h1>
+    <p>We're using <a href="https://docs.garden.io/guides/code-synchronization-dev-mode">Garden's code synchronization</a> functionality in combination with <a href="https://github.com/gatsbyjs/gatsby">Gatsby</a> (running inside a container) to live-update this page.</p>
   </Layout>
 )
 
