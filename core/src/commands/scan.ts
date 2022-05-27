33fe831722412d2dbbfb0790d4210a6bf6ee1046
Jon Edvald <edvald@gmail.com>
2022-05-27 15:51:55 +0200

    fix(cli): typo in field filtering in scan command

--- core/src/commands/scan.ts
+++ core/src/commands/scan.ts
@@ -24,7 +24,7 @@ export class ScanCommand extends Command {
     const graph = await garden.getConfigGraph({ log, emit: false })
     const modules = graph.getModules().map((m) => {
       return {
-        ...omit(m, ["_config", "cacheContext", "serviceNames", "taskNames", "buildDepdendencies"]),
+        ...omit(m, ["_config", "cacheContext", "serviceNames", "taskNames", "buildDependencies"]),
         version: omit(m.version, ["dependencyVersions"]),
       }
     })
