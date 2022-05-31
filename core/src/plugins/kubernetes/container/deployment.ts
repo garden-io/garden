6298d4f75cffcd4eb0b60e4dd087730f3dba94ac
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2022-05-31 15:08:11 +0200

    chore: cr-based logs correction
    Co-authored-by: Jon Edvald <edvald@gmail.com>

--- core/src/plugins/kubernetes/container/deployment.ts
+++ core/src/plugins/kubernetes/container/deployment.ts
@@ -139,7 +139,7 @@ export async function startLocalMode({
       section: service.name,
       msg: chalk.yellow(
         `Service "${service.name}" has been started with the --local-mode flag ` +
-          "but doesn't have a localMode configuration section! Please check its garden.yml config file."
+          "but doesn't have a localMode configuration section. Deploying the service normally instead."
       ),
     })
     return
