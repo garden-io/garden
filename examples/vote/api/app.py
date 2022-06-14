199d8cc28d49ca7070c0981c54f30706eab0578e
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2022-06-14 11:24:13 +0200

    style(examples): more pythonic code-style and formatting

--- examples/vote/api/app.py
+++ examples/vote/api/app.py
@@ -1,55 +1,59 @@
-from flask import Flask, render_template, request, make_response, g
+import json
+import os
+import random
+import socket
+
+from flask import Flask, request, g
 from flask_cors import CORS
 from redis import Redis
-import os
-import socket
-import random
-import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', 'Cats')
+option_b = os.getenv('OPTION_B', 'Dogs')
 hostname = socket.gethostname()
 
 app = Flask(__name__)
 CORS(app)
 
-def get_redis():
-    if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
-    return g.redis
 
-@app.route("/api", methods=['GET'])
+def get_redis():
+  if not hasattr(g, 'redis'):
+    g.redis = Redis(host='redis', db=0, socket_timeout=5)
+  return g.redis
+
+
+@app.route('/api', methods=['GET'])
 def hello():
+  return app.response_class(
+    response='Hello, I am the api service',
+    status=200,
+  )
+
+
+@app.route('/api/vote', methods=['POST', 'GET'])
+def vote():
+  app.logger.info('Received request')
+
+  voter_id = hex(random.getrandbits(64))[2:-1]
+
+  if request.method == 'POST':
+    redis = get_redis()
+    vote = request.form['vote']
+    data = json.dumps({'voter_id': voter_id, 'vote': vote})
+
+    redis.rpush('votes', data)
+    app.logger.info('Registered vote')
     return app.response_class(
-        response="Hello, I am the api service",
-        status=200,
+      response=json.dumps(data),
+      status=200,
+      mimetype='application/json'
+    )
+  else:
+    return app.response_class(
+      response=json.dumps({}),
+      status=404,
+      mimetype='application/json'
     )
 
-@app.route("/api/vote", methods=['POST','GET'])
-def vote():
-    app.logger.info("received request")
 
-    voter_id = hex(random.getrandbits(64))[2:-1]
-    vote = None
-
-    if request.method == 'POST':
-        redis = get_redis()
-        vote = request.form['vote']
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-
-        redis.rpush('votes', data)
-        print("Registered vote")
-        return app.response_class(
-            response=json.dumps(data),
-            status=200,
-            mimetype='application/json'
-        )
-    else:
-        return app.response_class(
-            response=json.dumps({}),
-            status=404,
-            mimetype='application/json'
-        )
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
