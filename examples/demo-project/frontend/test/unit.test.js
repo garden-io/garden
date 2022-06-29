a1fd33e7b6149e6a9047737dcb8bcf76db490fe7
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2022-06-29 11:41:44 +0000

    Save

---
+++ examples/demo-project/frontend/test/unit.test.js
@@ -0,0 +1,13 @@
+test('sample test case', () => {
+  for (let i = 0; i < 50; i++) {
+    console.log(`${i} a`)
+  }
+  expect(2+2).toBe(4)
+})
+
+test('another sample test case', () => {
+  for (let i = 0; i < 50; i++) {
+    console.log(`${i} b`)
+  }
+  expect(2+2).toBe(4)
+})
