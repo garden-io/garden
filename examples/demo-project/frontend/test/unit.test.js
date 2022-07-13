5987c43cb7437a87ec60e8e3e2e130841179dc1e
Mikael Hoegqvist Tabor <mikael@garden.io>
2022-07-13 10:14:04 +0200

    chore: another save

--- examples/demo-project/frontend/test/unit.test.js
+++ examples/demo-project/frontend/test/unit.test.js
@@ -1,13 +1,13 @@
 test('sample test case', () => {
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 1; i++) {
     console.log(`${i} a`)
   }
   expect(2+2).toBe(4)
 })
 
 test('another sample test case', () => {
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 1; i++) {
     console.log(`${i} b`)
   }
-  expect(2+2).toBe(4)
+  expect(2+2).toBe(5)
 })
