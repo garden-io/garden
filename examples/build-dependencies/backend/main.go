a46475c26f242d46f505322efe8e95cf1e424e69
Orzelius <albertkostusev@gmail.com>
2022-08-25 10:19:13 +0200

    docs(examples): improve shared-config example
    use a json config instead of a dotenv file
    and also utilize config on the go backend service

--- examples/build-dependencies/backend/main.go
+++ examples/build-dependencies/backend/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
+type Config struct {
+	SharedConfigMessage string
+}
+
+func readConfig() Config {
+	content, err := ioutil.ReadFile("./config/config.json")
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
+	}
+
+	var payload Config
+	err = json.Unmarshal(content, &payload)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
+
+	log.Printf("shared config loaded: %s\n", payload.SharedConfigMessage)
+	return payload
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello from Go!")
+	config := readConfig()
+
+	fmt.Fprint(w, "Hello from Go! "+config.SharedConfigMessage)
 }
 
 func main() {
@@ -18,4 +43,3 @@ func main() {
 		panic(err)
 	}
 }
-
