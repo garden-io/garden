6a6504e1d13aea1b570a1b8a5cb274f30dfa8d4d
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2022-09-02 13:59:30 +0200

    improvement: upgraded gradle version to 7.5.1

--- plugins/jib/gradle.ts
+++ plugins/jib/gradle.ts
@@ -15,12 +15,14 @@ import { resolve } from "path"
 import { pathExists } from "fs-extra"
 import { Writable } from "stream"
 
+const gradleVersion = "7.5.1"
+
 const spec = {
-  url: "https://services.gradle.org/distributions/gradle-7.1.1-bin.zip",
-  sha256: "bf8b869948901d422e9bb7d1fa61da6a6e19411baa7ad6ee929073df85d6365d",
+  url: `https://services.gradle.org/distributions/gradle-${gradleVersion}-bin.zip`,
+  sha256: "f6b8596b10cce501591e92f229816aa4046424f3b24d771751b06779d58c8ec4",
   extract: {
     format: "zip",
-    targetPath: "gradle-7.1.1/bin/gradle",
+    targetPath: `gradle-${gradleVersion}/bin/gradle`,
   },
 }
 
