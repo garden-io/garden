91657fb33a10d5b875423fd14896253568770ab5
Orzelius <albertkostusev@gmail.com>
2022-09-02 13:18:31 +0200

    test: fix stub and make it typesafe

--- core/test/helpers.ts
+++ core/test/helpers.ts
@@ -719,3 +719,16 @@ export async function assertAsyncError(action: () => Promise<any>, ...expectedEr
     expectedErrorMessages.forEach((msg) => expect(errorString).to.contain(msg))
   }
 }
+
+// https://stackoverflow.com/a/66836940
+// useful for typesafe stubbing
+export function getPropertyName<T>(
+  obj: T,
+  expression: (x: { [Property in keyof T]: () => string }) => () => string
+): string {
+  const res: { [Property in keyof T]: () => string } = {} as { [Property in keyof T]: () => string }
+
+  Object.keys(obj).map((k) => (res[k as keyof T] = () => k))
+
+  return expression(res)()
+}
