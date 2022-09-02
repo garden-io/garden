dd8396c089af2181eb7df7015f410c5806280bf0
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2022-09-02 14:33:32 +0200

    improvement: configurable maven binary

--- plugins/jib/maven.ts
+++ plugins/jib/maven.ts
@@ -9,18 +9,20 @@
 import AsyncLock from "async-lock"
 import { LogEntry, PluginContext, PluginToolSpec } from "@garden-io/sdk/types"
 import { find } from "lodash"
-import { PluginError } from "@garden-io/core/build/src/exceptions"
+import { PluginError, RuntimeError } from "@garden-io/core/build/src/exceptions"
 import { Writable } from "node:stream"
 import execa from "execa"
 
 const buildLock = new AsyncLock()
 
+const mvnVersion = "3.8.5"
+
 const spec = {
-  url: "https://archive.apache.org/dist/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz",
+  url: `https://archive.apache.org/dist/maven/maven-3/${mvnVersion}/binaries/apache-maven-${mvnVersion}-bin.tar.gz`,
   sha256: "88e30700f32a3f60e0d28d0f12a3525d29b7c20c72d130153df5b5d6d890c673",
   extract: {
     format: "tar",
-    targetPath: "apache-maven-3.8.5/bin/mvn",
+    targetPath: `apache-maven-${mvnVersion}/bin/mvn`,
   },
 }
 
@@ -43,7 +45,7 @@ export const mavenSpec: PluginToolSpec = {
       platform: "windows",
       architecture: "amd64",
       ...spec,
-      url: "https://archive.apache.org/dist/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.zip",
+      url: `https://archive.apache.org/dist/maven/maven-3/${mvnVersion}/binaries/apache-maven-${mvnVersion}-bin.zip`,
       sha256: "d53e045bc5c02aad179fae2fbc565d953354880db6661a8fab31f3a718d7b62c",
       extract: {
         format: "zip",
@@ -63,6 +65,47 @@ export function getMvnTool(ctx: PluginContext) {
   return tool
 }
 
+const baseErrorMessage = (mvnPath: string): string =>
+  `Maven binary path "${mvnPath}" is incorrect! Please check the \`mavenPath\` configuration option.`
+
+async function checkMavenVersion(mvnPath: string) {
+  try {
+    const res = await execa(mvnPath, ["--version"])
+    return res.stdout
+  } catch (err) {
+    const composeErrorMessage = (err: any): string => {
+      if (err.code === "EACCES") {
+        return `${baseErrorMessage(
+          mvnPath
+        )} It looks like the Maven path defined in the config is not an executable binary.`
+      } else if (err.code === "ENOENT") {
+        return `${baseErrorMessage(mvnPath)} The Maven path defined in the configuration does not exist.`
+      } else {
+        return baseErrorMessage(mvnPath)
+      }
+    }
+    throw new RuntimeError(composeErrorMessage(err), { mvnPath })
+  }
+}
+
+let mavenPathValid = false
+
+async function verifyMavenPath(mvnPath: string) {
+  if (mavenPathValid) {
+    return
+  }
+
+  const versionOutput = await checkMavenVersion(mvnPath)
+  const isMaven = versionOutput.toLowerCase().includes("maven")
+  if (!isMaven) {
+    throw new RuntimeError(
+      `${baseErrorMessage(mvnPath)} It looks like the Maven path points to a non-Maven executable binary.`,
+      { mvnPath }
+    )
+  }
+  mavenPathValid = true
+}
+
 /**
  * Run maven with the specified args in the specified directory.
  */
@@ -72,6 +115,7 @@ export async function mvn({
   cwd,
   log,
   openJdkPath,
+  mavenPath,
   outputStream,
 }: {
   ctx: PluginContext
@@ -79,10 +123,19 @@ export async function mvn({
   cwd: string
   log: LogEntry
   openJdkPath: string
+  mavenPath?: string
   outputStream?: Writable
 }) {
-  const tool = getMvnTool(ctx)
-  const mvnPath = await tool.getPath(log)
+  let mvnPath: string
+  if (!!mavenPath) {
+    log.verbose(`Using explicitly specified Maven binary from ${mavenPath}`)
+    mvnPath = mavenPath
+    await verifyMavenPath(mvnPath)
+  } else {
+    log.verbose(`The Maven binary hasn't been specified explicitly. Maven ${mvnVersion} will be used by default.`)
+    const tool = getMvnTool(ctx)
+    mvnPath = await tool.getPath(log)
+  }
 
   // Maven has issues when running concurrent processes, so we're working around that with a lock.
   // TODO: http://takari.io/book/30-team-maven.html would be a more robust solution.
