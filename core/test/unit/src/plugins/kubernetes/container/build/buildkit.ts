edbb9efe245ac86ccc414c9235d408f11eb2c673
Steffen Neubauer <steffen@garden.io>
2022-09-23 17:14:49 +0200

    tests: restructured

--- core/test/unit/src/plugins/kubernetes/container/build/buildkit.ts
+++ core/test/unit/src/plugins/kubernetes/container/build/buildkit.ts
@@ -35,161 +35,252 @@ describe("getBuildkitModuleFlags", () => {
   })
 })
 
-describe("getBuildkitImageFlags", () => {
-  const cacheConfig = ({ mode = "auto" as ClusterBuildkitCacheConfig["mode"] }): ClusterBuildkitCacheConfig => ({
-    type: "registry",
-    mode,
-    tag: "_buildcache",
-    export: true,
-  })
-
-  const defaultConfig = [cacheConfig({})]
-
-  const modeFlags = ({ mode, registry = "registry", registryModifier = "" }) => [
-    "--output",
-    `type=image,"name=${registry}/namespace/name:v-xxxxxx",push=true${registryModifier}`,
-    "--import-cache",
-    `type=registry,ref=${registry}/namespace/name:_buildcache${registryModifier}`,
-    "--export-cache",
-    `type=registry,ref=${registry}/namespace/name:_buildcache,mode=${mode}${registryModifier}`,
+describe("getBuildkitImageFlags()", () => {
+  const defaultConfig: ClusterBuildkitCacheConfig[] = [
+    {
+      type: "registry",
+      mode: "auto",
+      tag: "_buildcache",
+      export: true,
+    },
   ]
 
-  const inlineFlags = ({ registry = "registry" }) => [
-    "--export-cache",
-    "type=inline",
-    "--output",
-    `type=image,"name=${registry}/namespace/name:v-xxxxxx,${registry}/namespace/name:_buildcache",push=true`,
-    "--import-cache",
-    `type=registry,ref=${registry}/namespace/name:_buildcache`,
+  // test autodetection for mode=inline
+  const expectedInline = [
+    "eu.gcr.io",
+    "gcr.io",
+    "aws_account_id.dkr.ecr.region.amazonaws.com",
+    "keks.dkr.ecr.bla.amazonaws.com",
   ]
+  for (const registry of expectedInline) {
+    it(`returns type=inline cache flags with default config with registry ${registry}`, async () => {
+      const moduleOutputs = {
+        "local-image-id": "name:v-xxxxxx",
+        "local-image-name": "name",
+        "deployment-image-id": `${registry}/namespace/name:v-xxxxxx`,
+        "deployment-image-name": `${registry}/namespace/name`,
+      }
 
-  const moduleOutputs = ({ registry = "registry" }): ContainerModuleOutputs => ({
-    "local-image-id": "name:v-xxxxxx",
-    "local-image-name": "name",
-    "deployment-image-id": `${registry}/namespace/name:v-xxxxxx`,
-    "deployment-image-name": `${registry}/namespace/name`,
-  })
+      const flags = getBuildkitImageFlags(defaultConfig, moduleOutputs, false)
 
-  it("returns mode=max cache flags with default config by default", async () => {
-    const flags = getBuildkitImageFlags(defaultConfig, moduleOutputs({ registry: "hub.docker.com" }), false)
+      expect(flags).to.eql([
+        "--export-cache",
+        "type=inline",
+        "--output",
+        `type=image,"name=${registry}/namespace/name:v-xxxxxx,${registry}/namespace/name:_buildcache",push=true`,
+        "--import-cache",
+        `type=registry,ref=${registry}/namespace/name:_buildcache`,
+      ])
+    })
+  }
 
-    expect(flags).to.eql(modeFlags({ mode: "max", registry: "hub.docker.com" }))
-  })
+  // test autodetection for mode=max
+  const expectedMax = ["anyOtherRegistry", "hub.docker.com", "blakekswtf", "127.0.0.1"]
+  for (const registry of expectedMax) {
+    it(`returns mode=max cache flags with default config with registry ${registry}`, async () => {
+      const moduleOutputs = {
+        "local-image-id": "name:v-xxxxxx",
+        "local-image-name": "name",
+        "deployment-image-id": `${registry}/namespace/name:v-xxxxxx`,
+        "deployment-image-name": `${registry}/namespace/name`,
+      }
 
-  it("returns inline cache flags with default config when mode=max is not", async () => {
-    const flags = getBuildkitImageFlags(defaultConfig, moduleOutputs({ registry: "eu.gcr.io" }), false)
+      const flags = getBuildkitImageFlags(defaultConfig, moduleOutputs, false)
 
-    expect(flags).to.eql(inlineFlags({ registry: "eu.gcr.io" }))
-  })
+      expect(flags).to.eql([
+        "--output",
+        `type=image,"name=${registry}/namespace/name:v-xxxxxx",push=true`,
+        "--import-cache",
+        `type=registry,ref=${registry}/namespace/name:_buildcache`,
+        "--export-cache",
+        `type=registry,ref=${registry}/namespace/name:_buildcache,mode=max`,
+      ])
+    })
+  }
 
-  it("returns mode=max cache flags with mode=max config", async () => {
-    const flags = getBuildkitImageFlags([cacheConfig({ mode: "max" })], moduleOutputs({ registry: "eu.gcr.io" }), false)
+  // explicit min / max
+  const explicitModes: ClusterBuildkitCacheConfig["mode"][] = ["min", "max"]
+  for (const mode of explicitModes) {
+    it(`returns mode=${mode} cache flags if explicitly configured`, async () => {
+      const registry = "explicitTeamRegistry"
 
-    expect(flags).to.eql(modeFlags({ mode: "max", registry: "eu.gcr.io" }))
-  })
+      const moduleOutputs = {
+        "local-image-id": "name:v-xxxxxx",
+        "local-image-name": "name",
+        "deployment-image-id": `${registry}/namespace/name:v-xxxxxx`,
+        "deployment-image-name": `${registry}/namespace/name`,
+      }
 
-  it("returns mode=min cache flags with mode=min config", async () => {
-    const flags = getBuildkitImageFlags([cacheConfig({ mode: "min" })], moduleOutputs({ registry: "eu.gcr.io" }), false)
-
-    expect(flags).to.eql(modeFlags({ mode: "min", registry: "eu.gcr.io" }))
-  })
-
-  it("returns inline cache flags with mode=inline config", async () => {
-    const flags = getBuildkitImageFlags(
-      [cacheConfig({ mode: "inline" })],
-      moduleOutputs({ registry: "hub.docker.com" }),
-      false
-    )
-
-    expect(flags).to.eql(inlineFlags({ registry: "hub.docker.com" }))
-  })
-
-  it("uses registry.insecure=true with the in-cluster registry", async () => {
-    const flags = getBuildkitImageFlags(
-      defaultConfig,
-      moduleOutputs({ registry: "in-cluster-registry" }),
-      true // deploymentRegistryInsecure
-    )
-
-    expect(flags).to.eql(
-      modeFlags({ mode: "max", registry: "in-cluster-registry", registryModifier: ",registry.insecure=true" })
-    )
-  })
-
-  it("returns correct flags with separate cache registry", async () => {
-    const flags = getBuildkitImageFlags(
-      [
+      const config: ClusterBuildkitCacheConfig[] = [
         {
           type: "registry",
-          registry: {
-            hostname: "cacheRegistry",
-            namespace: "namespace",
-            insecure: false,
-          },
-          mode: "auto",
+          mode,
           tag: "_buildcache",
           export: true,
         },
-      ],
-      moduleOutputs({ registry: "deploymentRegistry" }),
-      false
+      ]
+
+      const flags = getBuildkitImageFlags(config, moduleOutputs, false)
+
+      expect(flags).to.eql([
+        "--output",
+        `type=image,"name=${registry}/namespace/name:v-xxxxxx",push=true`,
+        "--import-cache",
+        `type=registry,ref=${registry}/namespace/name:_buildcache`,
+        "--export-cache",
+        `type=registry,ref=${registry}/namespace/name:_buildcache,mode=${mode}`,
+      ])
+    })
+  }
+
+  // explicit inline
+  it(`returns type=inline cache flags when explicitly configured`, async () => {
+    const registry = "someExplicitInlineRegistry"
+
+    const moduleOutputs = {
+      "local-image-id": "name:v-xxxxxx",
+      "local-image-name": "name",
+      "deployment-image-id": `${registry}/namespace/name:v-xxxxxx`,
+      "deployment-image-name": `${registry}/namespace/name`,
+    }
+
+    const config: ClusterBuildkitCacheConfig[] = [
+      {
+        type: "registry",
+        mode: "inline",
+        tag: "_buildcache",
+        export: true,
+      },
+    ]
+
+    const flags = getBuildkitImageFlags(config, moduleOutputs, false)
+
+    expect(flags).to.eql([
+      "--export-cache",
+      "type=inline",
+      "--output",
+      `type=image,"name=${registry}/namespace/name:v-xxxxxx,${registry}/namespace/name:_buildcache",push=true`,
+      "--import-cache",
+      `type=registry,ref=${registry}/namespace/name:_buildcache`,
+    ])
+  })
+
+  it("uses registry.insecure=true with the in-cluster registry", async () => {
+    const registry = "in-kubernetes-registry.local:5000"
+
+    const moduleOutputs = {
+      "local-image-id": "name:v-xxxxxx",
+      "local-image-name": "name",
+      "deployment-image-id": `${registry}/namespace/name:v-xxxxxx`,
+      "deployment-image-name": `${registry}/namespace/name`,
+    }
+
+    const flags = getBuildkitImageFlags(
+      defaultConfig,
+      moduleOutputs,
+      true // deploymentRegistryInsecure
     )
+
+    expect(flags).to.eql([
+      "--output",
+      `type=image,"name=${registry}/namespace/name:v-xxxxxx",push=true,registry.insecure=true`,
+      "--import-cache",
+      `type=registry,ref=${registry}/namespace/name:_buildcache,registry.insecure=true`,
+      "--export-cache",
+      `type=registry,ref=${registry}/namespace/name:_buildcache,mode=max,registry.insecure=true`,
+    ])
+  })
+
+  it("returns correct flags with separate cache registry", async () => {
+    const deploymentRegistry = "deploymentRegistry"
+    const cacheRegistry = "cacheRegistry"
+
+    const moduleOutputs = {
+      "local-image-id": "name:v-xxxxxx",
+      "local-image-name": "name",
+      "deployment-image-id": `${deploymentRegistry}/namespace/name:v-xxxxxx`,
+      "deployment-image-name": `${deploymentRegistry}/namespace/name`,
+    }
+
+    const config: ClusterBuildkitCacheConfig[] = [
+      {
+        type: "registry",
+        registry: {
+          hostname: cacheRegistry,
+          namespace: "namespace",
+          insecure: false,
+        },
+        mode: "auto",
+        tag: "_buildcache",
+        export: true,
+      },
+    ]
+
+    const flags = getBuildkitImageFlags(config, moduleOutputs, false)
 
     expect(flags).to.eql([
       // output to deploymentRegistry
       "--output",
-      `type=image,"name=deploymentRegistry/namespace/name:v-xxxxxx",push=true`,
+      `type=image,"name=${deploymentRegistry}/namespace/name:v-xxxxxx",push=true`,
 
       // import and export to cacheRegistry with mode=max
       "--import-cache",
-      `type=registry,ref=cacheRegistry/namespace/name:_buildcache`,
+      `type=registry,ref=${cacheRegistry}/namespace/name:_buildcache`,
       "--export-cache",
-      `type=registry,ref=cacheRegistry/namespace/name:_buildcache,mode=max`,
+      `type=registry,ref=${cacheRegistry}/namespace/name:_buildcache,mode=max`,
     ])
   })
 
   it("returns correct flags for complex feautureBranch / Main branch + separate cache registry use case", async () => {
-    const flags = getBuildkitImageFlags(
-      [
-        {
-          type: "registry",
-          registry: {
-            hostname: "cacheRegistry",
-            namespace: "namespace",
-            insecure: false,
-          },
-          mode: "auto",
-          tag: "_buildcache-featureBranch",
-          export: true,
+    const deploymentRegistry = "someBigTeamDeploymentRegistry"
+    const cacheRegistry = "someBigTeamCacheRegistry"
+
+    const moduleOutputs = {
+      "local-image-id": "name:v-xxxxxx",
+      "local-image-name": "name",
+      "deployment-image-id": `${deploymentRegistry}/namespace/name:v-xxxxxx`,
+      "deployment-image-name": `${deploymentRegistry}/namespace/name`,
+    }
+
+    const config: ClusterBuildkitCacheConfig[] = [
+      {
+        type: "registry",
+        registry: {
+          hostname: cacheRegistry,
+          namespace: "namespace",
+          insecure: false,
         },
-        {
-          type: "registry",
-          registry: {
-            hostname: "cacheRegistry",
-            namespace: "namespace",
-            insecure: false,
-          },
-          mode: "auto",
-          tag: "_buildcache-main",
-          export: false,
+        mode: "auto",
+        tag: "_buildcache-featureBranch",
+        export: true,
+      },
+      {
+        type: "registry",
+        registry: {
+          hostname: cacheRegistry,
+          namespace: "namespace",
+          insecure: false,
         },
-      ],
-      moduleOutputs({ registry: "deploymentRegistry" }),
-      false
-    )
+        mode: "auto",
+        tag: "_buildcache-main",
+        export: false,
+      },
+    ]
+
+    const flags = getBuildkitImageFlags(config, moduleOutputs, false)
 
     expect(flags).to.eql([
       // output to deploymentRegistry
       "--output",
-      `type=image,"name=deploymentRegistry/namespace/name:v-xxxxxx",push=true`,
+      `type=image,"name=${deploymentRegistry}/namespace/name:v-xxxxxx",push=true`,
       // import and export to cacheRegistry with mode=max
       // import first _buildcache-featureBranch, then _buildcache-main
       "--import-cache",
-      `type=registry,ref=cacheRegistry/namespace/name:_buildcache-featureBranch`,
+      `type=registry,ref=${cacheRegistry}/namespace/name:_buildcache-featureBranch`,
       "--export-cache",
-      `type=registry,ref=cacheRegistry/namespace/name:_buildcache-featureBranch,mode=max`,
+      `type=registry,ref=${cacheRegistry}/namespace/name:_buildcache-featureBranch,mode=max`,
       "--import-cache",
-      `type=registry,ref=cacheRegistry/namespace/name:_buildcache-main`,
+      `type=registry,ref=${cacheRegistry}/namespace/name:_buildcache-main`,
     ])
   })
 })
