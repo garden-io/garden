a4b861c8d06106ebfcdba3ca2b6aa28cacd62147
Orzelius <albertkostusev@gmail.com>
2022-09-28 15:33:15 +0200

    chore: update actionStatusSchema

--- core/src/actions/base.ts
+++ core/src/actions/base.ts
@@ -244,7 +244,7 @@ export const baseRuntimeActionConfig = () =>
 
 export const actionStatusSchema = (detailSchema?: Joi.ObjectSchema) =>
   joi.object().keys({
-    status: joi
+    state: joi
       .string()
       .allow(...actionStateTypes)
       .only()
