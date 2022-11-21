926b8456c6c9b8562845a4f0a26c7eee4939ddd9
Ishant Agarwal <workstation@XAV-602000298.TI.ads>
2022-11-21 20:28:34 +0530

    feat(jib-container): hash change for darwin_aarch64,darwin_amd64 and win

--- plugins/jib/mavend.ts
+++ plugins/jib/mavend.ts
@@ -20,23 +20,23 @@ const mvndSpec = {
   description: "The Maven Daemon CLI.",
   baseUrl: `https://github.com/apache/maven-mvnd/releases/download/${mvndVersion}/`,
   linux: {
-    filename: `maven-mvnd-${mvndVersion}-linux-amd64.tar.gz`,
+    filename: `maven-mvnd-${mvndVersion}-darwin-amd64.tar.gz`,
     sha256: "5bcd4c3e45b767d562aa8d81583461abeb4fd6626ea1b8a1d961f34ef472f115",
-    targetPath: `maven-mvnd-${mvndVersion}-linux-amd64/bin/mvnd`,
+    targetPath: `maven-mvnd-${mvndVersion}-darwin-amd64/bin/mvnd`,
   },
   darwin_aarch64: {
     filename: `maven-mvnd-${mvndVersion}-darwin-aarch64.tar.gz`,
-    sha256: "0949505fcf42a1765558048451bb2a22e84b3635b1a31dd6191780eeccaa4ada",
+    sha256: "b3fab0126188072ea80784c4bcc726bf398e0115ed37f3e243e14c84a2fe7e45",
     targetPath: `maven-mvnd-${mvndVersion}-darwin-aarch64/bin/mvnd`,
   },
   darwin_amd64: {
     filename: `maven-mvnd-${mvndVersion}-darwin-amd64.tar.gz`,
-    sha256: "0949505fcf42a1765558048451bb2a22e84b3635b1a31dd6191780eeccaa4ada",
+    sha256: "889278f2e2a88450dcb074558a136fe06f9874db9b8d224674a5163a92ef2b69",
     targetPath: `maven-mvnd-${mvndVersion}-darwin-amd64/bin/mvnd`,
   },
   windows: {
     filename: `maven-mvnd-${mvndVersion}-windows-amd64.zip`,
-    sha256: "2405e11f9f3603e506cf7ab01fcb67a3e3a1cf3e7858e14d629a72c9a24c6c42",
+    sha256: "1991f977d9f73b8fa13a21c062e0fa3bd850e874f2f5682dca1f567548024bc6",
     targetPath: `maven-mvnd-${mvndVersion}-windows-amd64/bin/mvnd.cmd`,
   },
 }
