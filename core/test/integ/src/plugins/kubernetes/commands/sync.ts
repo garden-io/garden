cc688109ec7f0ffe092c0794fc4ca0abc2661102
Eythor Magnusson <eysi09@gmail.com>
2023-01-23 16:24:18 -0800

    feat(kubernetes): add sync helper commands
    This commit adds Kubernetes plugin commands for getting the Mutagen sync
    status, pausing syncs, and resuming syncs.
    
    I also refactored the Mutagen code to use a class which makes it a
    little easier to manage some of the daemon process life cycle logic.
    
    The class is currently a singleton but should be refactored after v0.13
    into a "normal" class that we can attach to the `garden` instance.
    
    Finally, the new MutagenDaemon class also logs more information about
    what Mutagen is doing under the hood. This is e.g. useful when
    Mutagen is syncing large change sets because without the added
    logs it looks like nothing is happening.

---
+++ core/test/integ/src/plugins/kubernetes/commands/sync.ts
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2018-2022 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { expect } from "chai"
+import { Garden } from "../../../../../../src/garden"
+import { getContainerTestGarden } from "../container/container"
+import { PluginContext } from "../../../../../../src/plugin-context"
+import { KubernetesProvider } from "../../../../../../src/plugins/kubernetes/config"
+import { MutagenDaemon } from "../../../../../../src/plugins/kubernetes/mutagen"
+import { syncPause, syncResume, syncStatus } from "../../../../../../src/plugins/kubernetes/commands/sync"
+import { DeployTask } from "../../../../../../src/tasks/deploy"
+import { Log } from "../../../../../../src/logger/log-entry"
+import { ConfigGraph } from "../../../../../../src/graph/config-graph"
+
+describe("sync plugin commands", () => {
+  let garden: Garden
+  let graph: ConfigGraph
+  let provider: KubernetesProvider
+  let ctx: PluginContext
+  let log: Log
+
+  before(async () => {
+    await init("local")
+  })
+
+  after(async () => {
+    if (garden) {
+      await garden.close()
+      await MutagenDaemon.clearInstance()
+    }
+  })
+
+  const init = async (environmentName: string) => {
+    garden = await getContainerTestGarden(environmentName)
+    graph = await garden.getConfigGraph({ log: garden.log, emit: false })
+    provider = <KubernetesProvider>await garden.resolveProvider(garden.log, "local-kubernetes")
+    ctx = await garden.getPluginContext({ provider, templateContext: undefined, events: undefined })
+    log = garden.log
+    const action = graph.getDeploy("dev-mode")
+
+    // The deploy task actually creates the sync
+    const deployTask = new DeployTask({
+      garden,
+      graph,
+      log,
+      action,
+      force: true,
+      forceBuild: false,
+      syncModeDeployNames: [action.name],
+      localModeDeployNames: [],
+    })
+
+    await garden.processTasks({ log, tasks: [deployTask], throwOnError: true })
+
+    await MutagenDaemon.start({ ctx, log })
+  }
+
+  describe("sync-status", () => {
+    it("should print the Mutagen sync status", async () => {
+      const res = (await syncStatus.handler({ ctx, log, garden, graph, args: [] })) as any
+
+      expect(res.result.syncSessions.length).to.equal(1)
+      expect(res.result.syncSessions[0].alpha).to.exist
+      expect(res.result.syncSessions[0].beta).to.exist
+      expect(res.result.syncSessions[0].status).to.equal("watching")
+      expect(res.result.syncSessions[0].paused).to.equal(false)
+    })
+  })
+
+  describe("sync-pause and sync-resume", () => {
+    it("should pause all Mutagen syncs", async () => {
+      await syncPause.handler({ ctx, log, garden, graph, args: [] })
+      const pausedStatus = (await syncStatus.handler({ ctx, log, garden, graph, args: [] })) as any
+
+      await syncResume.handler({ ctx, log, garden, graph, args: [] })
+      const resumedStatus = (await syncStatus.handler({ ctx, log, garden, graph, args: [] })) as any
+
+      expect(pausedStatus.result.syncSessions[0].paused).to.equal(true)
+      expect(resumedStatus.result.syncSessions[0].paused).to.equal(false)
+    })
+  })
+})
