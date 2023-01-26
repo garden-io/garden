ae5be4f3a41d6baed0c9ff9d37a76791e95df16f
Eythor Magnusson <eysi09@gmail.com>
2023-01-25 16:53:41 -0800

    improvement(server): ensure serverReady event has correct type
    This event was introduced awhile ago but isn't used by any client at the
    moment.
    
    It was made into its own event type by mistake, it should've just been a
    normal event with the name 'serverReady'. This fixes that.

--- core/test/unit/src/server/server.ts
+++ core/test/unit/src/server/server.ts
@@ -219,7 +219,7 @@ describe("GardenServer", () => {
         const parsed = JSON.parse(msg.toString())
         // This message is always sent at the beginning and we skip it here
         // to simplify testing.
-        if (parsed.type !== "serverReady") {
+        if (parsed.name !== "serverReady") {
           cb(parsed)
         }
       })
@@ -268,7 +268,7 @@ describe("GardenServer", () => {
 
         if (msgs.length === 2) {
           expect(msgs).to.eql([
-            { type: "serverReady", message: "Server ready" },
+            { type: "event", name: "serverReady", payload: {} },
             { type: "event", name: "_test", payload: "foo" },
           ])
           done()
