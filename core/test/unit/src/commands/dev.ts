cf9ca0c5e7c62d436b242cb6372a816aa5b4c33b
Jon Edvald <edvald@gmail.com>
2023-01-26 12:59:12 -0800

    refactor: remove old `garden dev` command implementation
    For now, the `dev` command implementation is basically that of the `serve`
    command, with an added "under construction" message to steer users to the
    `deploy --dev` command.
    
    Later we will add interactivity on top, hopefully before the 0.13 GA.
    
    BREAKING CHANGE:
    
    The behavior of the `garden dev` command is being fully replaced. It will no
    longer automatically perform all tests and deployments, but instead connect to
    the new dashboard, and later feature a command input in the terminal.

--- core/test/unit/src/commands/dev.ts
+++ core/test/unit/src/commands/dev.ts
@@ -6,286 +6,61 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import pEvent from "p-event"
 import { expect } from "chai"
-import { DevCommand, DevCommandArgs, DevCommandOpts, getDevCommandWatchTasks } from "../../../../src/commands/dev"
-import { makeTestGardenA, withDefaultGlobalOpts, TestGarden } from "../../../helpers"
-import { GlobalOptions, ParameterValues } from "../../../../src/cli/params"
+import { DevCommand } from "../../../../src/commands/dev"
 
 // TODO-G2: rename test cases to match the new graph model semantics
 describe("DevCommand", () => {
   const command = new DevCommand()
 
-  async function waitForEvent(garden: TestGarden, name: string) {
-    return pEvent(<any>garden.events, name, { timeout: 10000 })
-  }
+  // async function waitForEvent(garden: TestGarden, name: string) {
+  //   return pEvent(<any>garden.events, name, { timeout: 10000 })
+  // }
 
-  async function completeFirstTasks(
-    garden: TestGarden,
-    args: ParameterValues<DevCommandArgs>,
-    opts: ParameterValues<GlobalOptions & DevCommandOpts>
-  ) {
-    const log = garden.log
+  // async function completeFirstTasks(
+  //   garden: TestGarden,
+  //   args: ParameterValues<DevCommandArgs>,
+  //   opts: ParameterValues<GlobalOptions & DevCommandOpts>
+  // ) {
+  //   const log = garden.log
 
-    await command.prepare({ log, footerLog: log, headerLog: log, args, opts })
+  //   await command.prepare({ log, footerLog: log, headerLog: log, args, opts })
 
-    const promise = command
-      .action({
-        garden,
-        log,
-        headerLog: log,
-        footerLog: log,
-        args,
-        opts,
-      })
-      .then(({ errors }) => {
-        if (errors) {
-          throw errors[0]
-        }
-      })
-      .catch((err) => {
-        // tslint:disable-next-line: no-console
-        console.error(err)
-      })
+  //   const promise = command
+  //     .action({
+  //       garden,
+  //       log,
+  //       headerLog: log,
+  //       footerLog: log,
+  //       args,
+  //       opts,
+  //     })
+  //     .then(({ errors }) => {
+  //       if (errors) {
+  //         throw errors[0]
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       // tslint:disable-next-line: no-console
+  //       console.error(err)
+  //     })
 
-    await waitForEvent(garden, "watchingForChanges")
+  //   await waitForEvent(garden, "watchingForChanges")
 
-    garden.events.emit("_exit", {})
+  //   garden.events.emit("_exit", {})
 
-    const completedTasks = garden.events.eventLog
-      .filter((e) => e.name === "taskComplete")
-      .map((e) => e.payload["key"])
-      .filter((key) => !key.startsWith("resolve-module."))
-      .sort()
+  //   const completedTasks = garden.events.eventLog
+  //     .filter((e) => e.name === "taskComplete")
+  //     .map((e) => e.payload["key"])
+  //     .filter((key) => !key.startsWith("resolve-module."))
+  //     .sort()
 
-    return { promise, completedTasks }
-  }
+  //   return { promise, completedTasks }
+  // }
 
   it("should be protected", async () => {
     expect(command.protected).to.be.true
   })
 
-  it("should deploy, run and test everything in a project", async () => {
-    const garden = await makeTestGardenA()
-
-    const args = { deploys: undefined }
-    const opts = withDefaultGlobalOpts({
-      "force-build": false,
-      "force": false,
-
-      "local-mode": undefined,
-      "skip-tests": false,
-      "test-names": undefined,
-    })
-
-    const { promise, completedTasks } = await completeFirstTasks(garden, args, opts)
-
-    expect(completedTasks).to.eql([
-      "build.module-a",
-      "build.module-b",
-      "build.module-c",
-      "deploy.service-a",
-      "deploy.service-b",
-      "deploy.service-c",
-      "get-service-status.service-a",
-      "get-service-status.service-b",
-      "get-service-status.service-c",
-      "get-task-result.task-c",
-      "resolve-provider.container",
-      "resolve-provider.exec",
-      "resolve-provider.templated",
-      "resolve-provider.test-plugin",
-      "resolve-provider.test-plugin-b",
-
-      "task.task-c",
-      "test.module-a.integration",
-      "test.module-a.unit",
-      "test.module-b.unit",
-      "test.module-c.integ",
-      "test.module-c.unit",
-    ])
-
-    return promise
-  })
-
-  it("should skip disabled services", async () => {
-    const garden = await makeTestGardenA()
-
-    await garden.scanAndAddConfigs()
-    garden["moduleConfigs"]["module-c"].spec.services[0].disabled = true
-
-    const args = { deploys: undefined }
-    const opts = withDefaultGlobalOpts({
-      "force-build": false,
-      "force": false,
-
-      "local-mode": undefined,
-      "skip-tests": false,
-      "test-names": undefined,
-    })
-
-    const { promise, completedTasks } = await completeFirstTasks(garden, args, opts)
-
-    expect(completedTasks).to.not.include("deploy.service-c")
-
-    return promise
-  })
-
-  it("should skip disabled tasks", async () => {
-    const garden = await makeTestGardenA()
-
-    await garden.scanAndAddConfigs()
-    garden["moduleConfigs"]["module-c"].spec.tasks[0].disabled = true
-
-    const args = { deploys: undefined }
-    const opts = withDefaultGlobalOpts({
-      "force-build": false,
-      "force": false,
-
-      "local-mode": undefined,
-      "skip-tests": false,
-      "test-names": undefined,
-    })
-
-    const { promise, completedTasks } = await completeFirstTasks(garden, args, opts)
-
-    expect(completedTasks).to.not.include("task.task-c")
-
-    return promise
-  })
-
-  it("should skip disabled tests", async () => {
-    const garden = await makeTestGardenA()
-
-    await garden.scanAndAddConfigs()
-    garden["moduleConfigs"]["module-b"].spec.tests[0].disabled = true
-
-    const args = { deploys: undefined }
-    const opts = withDefaultGlobalOpts({
-      "force-build": false,
-      "force": false,
-
-      "local-mode": undefined,
-      "skip-tests": false,
-      "test-names": undefined,
-    })
-
-    const { promise, completedTasks } = await completeFirstTasks(garden, args, opts)
-
-    expect(completedTasks).to.not.include("test.module-b.unit")
-
-    return promise
-  })
-
-  it("should skip services from disabled modules", async () => {
-    const garden = await makeTestGardenA()
-
-    await garden.scanAndAddConfigs()
-    garden["moduleConfigs"]["module-c"].disabled = true
-
-    const args = { deploys: undefined }
-    const opts = withDefaultGlobalOpts({
-      "force-build": false,
-      "force": false,
-
-      "local-mode": undefined,
-      "skip-tests": false,
-      "test-names": undefined,
-    })
-
-    const { promise, completedTasks } = await completeFirstTasks(garden, args, opts)
-
-    expect(completedTasks).to.not.include("deploy.service-c")
-
-    return promise
-  })
-
-  it("should skip tasks from disabled modules", async () => {
-    const garden = await makeTestGardenA()
-
-    await garden.scanAndAddConfigs()
-    garden["moduleConfigs"]["module-c"].disabled = true
-
-    const args = { deploys: undefined }
-    const opts = withDefaultGlobalOpts({
-      "force-build": false,
-      "force": false,
-
-      "local-mode": undefined,
-      "skip-tests": false,
-      "test-names": undefined,
-    })
-
-    const { promise, completedTasks } = await completeFirstTasks(garden, args, opts)
-
-    expect(completedTasks).to.not.include("task.task-c")
-
-    return promise
-  })
-
-  it("should skip tests from disabled modules", async () => {
-    const garden = await makeTestGardenA()
-
-    await garden.scanAndAddConfigs()
-    garden["moduleConfigs"]["module-c"].disabled = true
-
-    const args = { deploys: undefined }
-    const opts = withDefaultGlobalOpts({
-      "force-build": false,
-      "force": false,
-
-      "local-mode": undefined,
-      "skip-tests": false,
-      "test-names": undefined,
-    })
-
-    const { promise, completedTasks } = await completeFirstTasks(garden, args, opts)
-
-    expect(completedTasks).to.not.include("test.module-c.unit")
-    expect(completedTasks).to.not.include("test.module-c.integ")
-
-    return promise
-  })
-})
-
-describe("getDevCommandWatchTasks", () => {
-  it("should deploy, run and test appropriately on watch change", async () => {
-    const garden = await makeTestGardenA()
-    const log = garden.log
-    const graph = await garden.getConfigGraph({ log, emit: false })
-
-    // TODO-G2: fix the semantics of the test
-    const deployAction = graph.getDeploy("deploy.module-b.service-b")
-    const watchTasks = await getDevCommandWatchTasks({
-      garden,
-      log,
-      updatedGraph: graph,
-      updatedAction: deployAction,
-      // module: graph.getModule("module-b"),
-      //servicesWatched: graph.getDeploys().map((s) => s.name),
-      devModeDeployNames: [],
-      localModeDeployNames: [],
-      testNames: undefined,
-      skipTests: false,
-    })
-
-    const results = await garden.processTasks({ log, tasks: watchTasks })
-    expect(Object.keys(results).sort()).to.eql([
-      "build.module-a",
-      "build.module-b",
-      "build.module-c",
-      "deploy.service-a",
-      "deploy.service-b",
-      "deploy.service-c",
-      "get-service-status.service-a",
-      "get-service-status.service-b",
-      "get-service-status.service-c",
-      "get-task-result.task-c",
-
-      "task.task-c",
-      "test.module-b.unit",
-      "test.module-c.integ",
-      "test.module-c.unit",
-    ])
-  })
+  // TODO-G2
 })
