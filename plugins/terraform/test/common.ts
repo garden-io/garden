766f75152c65bdfd620cc0fe30edfd09f0af779f
Steffen Neubauer <steffen@garden.io>
2023-01-31 17:26:16 +0100

    test(terraform): run tests for multiple tf versions

--- plugins/terraform/test/common.ts
+++ plugins/terraform/test/common.ts
@@ -13,109 +13,112 @@ import { makeTestGarden, TestGarden } from "@garden-io/sdk/testing"
 import { Log, PluginContext } from "@garden-io/sdk/types"
 import { getWorkspaces, setWorkspace } from "../common"
 import { expect } from "chai"
-import { terraform } from "../cli"
+import { defaultTerraformVersion, terraform } from "../cli"
 
-describe("Terraform common", () => {
-  const testRoot = join(__dirname, "test-project")
+for (const terraformVersion of ["0.13.3", defaultTerraformVersion]) {
+  describe(`Terraform common with version ${terraformVersion}`, () => {
+    const testRoot = join(__dirname, "test-project")
 
-  let root: string
-  let terraformDirPath: string
-  let stateDirPath: string
-  let testFilePath: string
+    let root: string
+    let terraformDirPath: string
+    let stateDirPath: string
+    let testFilePath: string
 
-  let garden: TestGarden
-  let log: Log
-  let ctx: PluginContext
-  let provider: TerraformProvider
+    let garden: TestGarden
+    let log: Log
+    let ctx: PluginContext
+    let provider: TerraformProvider
 
-  async function reset() {
-    if (terraformDirPath && (await pathExists(terraformDirPath))) {
-      await remove(terraformDirPath)
+    async function reset() {
+      if (terraformDirPath && (await pathExists(terraformDirPath))) {
+        await remove(terraformDirPath)
+      }
+      if (testFilePath && (await pathExists(testFilePath))) {
+        await remove(testFilePath)
+      }
+      if (stateDirPath && (await pathExists(stateDirPath))) {
+        await remove(stateDirPath)
+      }
     }
-    if (testFilePath && (await pathExists(testFilePath))) {
-      await remove(testFilePath)
-    }
-    if (stateDirPath && (await pathExists(stateDirPath))) {
-      await remove(stateDirPath)
-    }
-  }
 
-  before(async () => {
-    garden = await makeTestGarden(testRoot, {
-      plugins: [gardenPlugin()],
-      environmentString: "prod",
-      forceRefresh: true,
-    })
-    log = garden.log
-    provider = (await garden.resolveProvider(log, "terraform")) as TerraformProvider
-    ctx = await garden.getPluginContext({ provider, events: undefined, templateContext: undefined })
-    root = join(garden.projectRoot, "tf")
-    terraformDirPath = join(root, ".terraform")
-    stateDirPath = join(root, "terraform.tfstate.d")
-    testFilePath = join(root, "test.log")
-  })
-
-  beforeEach(async () => {
-    await reset()
-  })
-
-  after(async () => {
-    await reset()
-  })
-
-  describe("getWorkspaces", () => {
-    it("returns just the default workspace if none other exists", async () => {
-      const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
-      expect(selected).to.equal("default")
-      expect(workspaces).to.eql(["default"])
+    before(async () => {
+      garden = await makeTestGarden(testRoot, {
+        plugins: [gardenPlugin()],
+        environmentString: "prod",
+        forceRefresh: true,
+        variableOverrides: { "tf-version": terraformVersion },
+      })
+      log = garden.log
+      provider = (await garden.resolveProvider(log, "terraform")) as TerraformProvider
+      ctx = await garden.getPluginContext({ provider, events: undefined, templateContext: undefined })
+      root = join(garden.projectRoot, "tf")
+      terraformDirPath = join(root, ".terraform")
+      stateDirPath = join(root, "terraform.tfstate.d")
+      testFilePath = join(root, "test.log")
     })
 
-    it("returns all workspaces and which is selected", async () => {
-      await terraform(ctx, provider).exec({ args: ["workspace", "new", "foo"], cwd: root, log })
-      await terraform(ctx, provider).exec({ args: ["workspace", "new", "bar"], cwd: root, log })
-
-      const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
-      expect(selected).to.equal("bar")
-      expect(workspaces).to.eql(["default", "bar", "foo"])
-    })
-  })
-
-  describe("setWorkspace", () => {
-    it("does nothing if no workspace is set", async () => {
-      await terraform(ctx, provider).exec({ args: ["workspace", "new", "foo"], cwd: root, log })
-
-      await setWorkspace({ ctx, provider, log, root, workspace: null })
-
-      const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
-      expect(selected).to.equal("foo")
-      expect(workspaces).to.eql(["default", "foo"])
+    beforeEach(async () => {
+      await reset()
     })
 
-    it("does nothing if already on requested workspace", async () => {
-      await setWorkspace({ ctx, provider, log, root, workspace: "default" })
-
-      const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
-      expect(selected).to.equal("default")
-      expect(workspaces).to.eql(["default"])
+    after(async () => {
+      await reset()
     })
 
-    it("selects the given workspace if it already exists", async () => {
-      await terraform(ctx, provider).exec({ args: ["workspace", "new", "foo"], cwd: root, log })
-      await terraform(ctx, provider).exec({ args: ["workspace", "select", "default"], cwd: root, log })
+    describe("getWorkspaces", () => {
+      it("returns just the default workspace if none other exists", async () => {
+        const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
+        expect(selected).to.equal("default")
+        expect(workspaces).to.eql(["default"])
+      })
 
-      await setWorkspace({ ctx, provider, log, root, workspace: "foo" })
+      it("returns all workspaces and which is selected", async () => {
+        await terraform(ctx, provider).exec({ args: ["workspace", "new", "foo"], cwd: root, log })
+        await terraform(ctx, provider).exec({ args: ["workspace", "new", "bar"], cwd: root, log })
 
-      const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
-      expect(selected).to.equal("foo")
-      expect(workspaces).to.eql(["default", "foo"])
+        const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
+        expect(selected).to.equal("bar")
+        expect(workspaces).to.eql(["default", "bar", "foo"])
+      })
     })
 
-    it("creates a new workspace if it doesn't already exist", async () => {
-      await setWorkspace({ ctx, provider, log, root, workspace: "foo" })
+    describe("setWorkspace", () => {
+      it("does nothing if no workspace is set", async () => {
+        await terraform(ctx, provider).exec({ args: ["workspace", "new", "foo"], cwd: root, log })
 
-      const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
-      expect(selected).to.equal("foo")
-      expect(workspaces).to.eql(["default", "foo"])
+        await setWorkspace({ ctx, provider, log, root, workspace: null })
+
+        const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
+        expect(selected).to.equal("foo")
+        expect(workspaces).to.eql(["default", "foo"])
+      })
+
+      it("does nothing if already on requested workspace", async () => {
+        await setWorkspace({ ctx, provider, log, root, workspace: "default" })
+
+        const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
+        expect(selected).to.equal("default")
+        expect(workspaces).to.eql(["default"])
+      })
+
+      it("selects the given workspace if it already exists", async () => {
+        await terraform(ctx, provider).exec({ args: ["workspace", "new", "foo"], cwd: root, log })
+        await terraform(ctx, provider).exec({ args: ["workspace", "select", "default"], cwd: root, log })
+
+        await setWorkspace({ ctx, provider, log, root, workspace: "foo" })
+
+        const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
+        expect(selected).to.equal("foo")
+        expect(workspaces).to.eql(["default", "foo"])
+      })
+
+      it("creates a new workspace if it doesn't already exist", async () => {
+        await setWorkspace({ ctx, provider, log, root, workspace: "foo" })
+
+        const { workspaces, selected } = await getWorkspaces({ ctx, provider, log, root })
+        expect(selected).to.equal("foo")
+        expect(workspaces).to.eql(["default", "foo"])
+      })
     })
   })
-})
+}
