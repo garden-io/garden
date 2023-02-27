b6a3c42432845e47d199a8f883153bbccfaabf46
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2023-02-27 17:39:58 +0000

    Airgap images without CircleCi image Airgap images add sha256sum

--- scripts/package-airgap.sh
+++ scripts/package-airgap.sh
@@ -5,6 +5,7 @@
 set -e -x
 
 ARCH="$1"
+VERSION="$2"
 
 cd $(dirname $0)/..
 
@@ -12,11 +13,13 @@ mkdir -p scripts/airgap
 mkdir -p dist
 
 airgap_image_file='scripts/airgap/image-list.txt'
-echo "$(find images/*/garden.yml | xargs perl -lne 'print $1 if /image: (.*)/')" > "$airgap_image_file"
+rm -f "$airgap_image_file"
+echo "$(find images/*/garden.yml -not -path '*/circleci-runner/*' | xargs perl -lne 'print $1 if /image: (.*)/')" > "$airgap_image_file"
 
 images=$(cat "${airgap_image_file}")
 xargs -n1 docker pull <<< "${images}"
-docker save ${images} | gzip > dist/garden-airgap-images-${ARCH}.tar.gz
+docker save ${images} | gzip > "dist/garden-${VERSION}-airgap-images-${ARCH}.tar.gz"
+sha256sum "dist/garden-${VERSION}-airgap-images-${ARCH}.tar.gz" | cut -d " " -f 1 > "dist/garden-${VERSION}-airgap-images-${ARCH}.tar.gz.sha256"
 if [ ${ARCH} = amd64 ]; then
   cp "${airgap_image_file}" dist/garden-images.txt
 fi
