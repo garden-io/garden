16502d7ceffa2a19dd050be07731c8feb06841cf
Walther <veeti.haapsamo@gmail.com>
2023-03-01 13:03:56 +0200

    chore: allow slightly longer commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,19 +1,12 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
     // Extend to add improvement key
-    "type-enum": [2, "always", [
-      "chore",
-      "ci",
-      "docs",
-      "feat",
-      "fix",
-      "improvement",
-      "perf",
-      "refactor",
-      "revert",
-      "style",
-      "test",
-    ]]
-  }
-};
+    "type-enum": [
+      2,
+      "always",
+      ["chore", "ci", "docs", "feat", "fix", "improvement", "perf", "refactor", "revert", "style", "test"],
+    ],
+    "header-max-length": [2, "always", 120],
+  },
+}
