e6c7798a4a690cfad1c5b480d67f69b8c9cf76e8
Mikael Hoegqvist Tabor <mikael@garden.io>
2023-05-11 18:41:31 +0200

    test: re-enabled cli analytics tests

---
+++ core/test/unit/src/cli/analytics.ts
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2018-2023 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import nock from "nock"
+import { expect } from "chai"
+
+import { GardenCli } from "../../../../src/cli/cli"
+import { GlobalConfigStore } from "../../../../src/config-store/global"
+import { TestGarden, enableAnalytics, makeTestGardenA } from "../../../helpers"
+import { Command } from "../../../../src/commands/base"
+import { isEqual } from "lodash"
+import { getRootLogger, RootLogger } from "../../../../src/logger/logger"
+
+describe("cli analytics", () => {
+  let cli: GardenCli
+  const globalConfigStore = new GlobalConfigStore()
+  const log = getRootLogger().createLog()
+
+  beforeEach(async () => {
+    cli = new GardenCli()
+    garden = await makeTestGardenA()
+    resetAnalyticsConfig = await enableAnalytics(garden)
+  })
+
+  afterEach(async () => {
+    if (cli.processRecord && cli.processRecord.pid) {
+      await globalConfigStore.delete("activeProcesses", String(cli.processRecord.pid))
+    }
+
+    await resetAnalyticsConfig()
+    nock.cleanAll()
+  })
+
+  let garden: TestGarden
+  let resetAnalyticsConfig: Function
+
+  class TestCommand extends Command {
+    name = "test-command"
+    help = "hilfe!"
+    noProject = true
+
+    printHeader() {}
+
+    async action({ args }) {
+      return { result: { args } }
+    }
+  }
+
+  it("should access the version check service", async () => {
+    const scope = nock("https://get.garden.io")
+    scope.get("/version").query(true).reply(200)
+
+    const command = new TestCommand()
+    cli.addCommand(command)
+
+    await cli.run({ args: ["test-command"], exitOnError: false })
+
+    expect(scope.done()).to.not.throw
+  })
+
+  // TODO: @eysi This test always passes locally but fails consistently in CI.
+  // I'm pretty stumped so simply skipping this for now but definitely revisiting.
+  // Let's make sure we keep an eye on our analytics data after we release this.
+  // If nothing looks off there, we can assume the test was bad. Otherwise
+  // we'll need to revert.
+  it("should wait for queued analytic events to flush", async () => {
+    const scope = nock("https://api.segment.io")
+
+    // Each command run result in two events:
+    // 'Run Command' and 'Command Result'
+    scope
+      .post(`/v1/batch`, (body) => {
+        const events = body.batch.map((event: any) => ({
+          event: event.event,
+          type: event.type,
+          name: event.properties.name,
+        }))
+
+        return isEqual(events, [
+          {
+            event: "Run Command",
+            type: "track",
+            name: "test-command",
+          },
+          {
+            event: "Command Result",
+            type: "track",
+            name: "test-command",
+          },
+        ])
+      })
+      .reply(200)
+
+    const command = new TestCommand()
+    cli.addCommand(command)
+
+    await cli.run({ args: ["test-command"], exitOnError: false })
+
+    expect(scope.done()).to.not.throw
+  })
+})
