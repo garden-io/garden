182f8db6954d1626db036ae26b84310fcc8a53cf
Steffen Neubauer <steffen@garden.io>
2023-05-11 20:32:14 +0200

    test: add binary run tests

--- support/docker-bake-test.sh
+++ support/docker-bake-test.sh
@@ -42,43 +42,73 @@ TEST() {
 }
 
 TEST "prerelease tags for buster"
-MAJOR_VERSION=0 MINOR_VERSION=13 PATCH_VERSION=0 PRERELEASE=edge CODENAME=bonsai \
-docker buildx bake --progress=plain -f support/docker-bake.hcl buster
-should_not_exist gardendev/garden 0.13-buster
-should_exist gardendev/garden 0.13-edge-buster
-should_exist gardendev/garden bonsai-edge-buster
+  MAJOR_VERSION=0 MINOR_VERSION=13 PATCH_VERSION=0 PRERELEASE=edge CODENAME=bonsai \
+    docker buildx bake --progress=plain -f support/docker-bake.hcl buster
+
+  should_not_exist gardendev/garden 0.13-buster
+  should_exist gardendev/garden 0.13-edge-buster
+  should_exist gardendev/garden bonsai-edge-buster
 
 TEST "prerelease tags for alpine"
-MAJOR_VERSION=0 MINOR_VERSION=13 PATCH_VERSION=0 PRERELEASE=edge CODENAME=bonsai \
-docker buildx bake --progress=plain -f support/docker-bake.hcl alpine
-for image in gardendev/garden{,-aws,-azure,-gcloud,-aws-gcloud,-aws-gcloud-azure}
-  do
-  should_not_exist $image latest
-  should_not_exist $image 0.13-alpine
-  should_exist $image 0.13-edge-alpine
-  should_exist $image bonsai-edge-alpine
-done
+  MAJOR_VERSION=0 MINOR_VERSION=13 PATCH_VERSION=0 PRERELEASE=edge CODENAME=bonsai \
+    docker buildx bake --progress=plain -f support/docker-bake.hcl alpine
+
+  for image in gardendev/garden{,-aws,-azure,-gcloud,-aws-gcloud,-aws-gcloud-azure}
+    do
+    should_not_exist $image latest
+    should_not_exist $image 0.13-alpine
+    should_exist $image 0.13-edge-alpine
+    should_exist $image bonsai-edge-alpine
+  done
 
 TEST "production release tags for buster"
-MAJOR_VERSION=0 MINOR_VERSION=13 PATCH_VERSION=0 CODENAME=bonsai \
-docker buildx bake --progress=plain -f support/docker-bake.hcl buster
-should_not_exist gardendev/garden latest
-should_exist gardendev/garden 0.13-buster
-should_exist gardendev/garden 0.13.0-buster
-should_exist gardendev/garden bonsai-buster
-should_not_exist gardendev/garden 0.13-edge-buster
-should_not_exist gardendev/garden bonsai-edge-buster
+  MAJOR_VERSION=0 MINOR_VERSION=13 PATCH_VERSION=0 CODENAME=bonsai \
+    docker buildx bake --progress=plain -f support/docker-bake.hcl buster
+
+  should_not_exist gardendev/garden latest
+  should_exist gardendev/garden 0.13-buster
+  should_exist gardendev/garden 0.13.0-buster
+  should_exist gardendev/garden bonsai-buster
+  should_not_exist gardendev/garden 0.13-edge-buster
+  should_not_exist gardendev/garden bonsai-edge-buster
 
 TEST "production release tags for alpine"
-MAJOR_VERSION=0 MINOR_VERSION=13 PATCH_VERSION=0 CODENAME=bonsai \
-docker buildx bake --progress=plain -f support/docker-bake.hcl alpine
-for image in gardendev/garden{,-aws,-azure,-gcloud,-aws-gcloud,-aws-gcloud-azure}
-  do
-  should_exist $image latest
-  should_exist $image 0.13-alpine
-  should_exist $image 0.13.0-alpine
-  should_exist $image bonsai-alpine
-  should_not_exist $image 0.13-edge-alpine
-  should_not_exist $image bonsai-edge-alpine
-done
+  MAJOR_VERSION=0 MINOR_VERSION=13 PATCH_VERSION=0 CODENAME=bonsai \
+    docker buildx bake --progress=plain -f support/docker-bake.hcl alpine
 
+  for image in gardendev/garden{,-aws,-azure,-gcloud,-aws-gcloud,-aws-gcloud-azure}
+    do
+    should_exist $image latest
+    should_exist $image 0.13-alpine
+    should_exist $image 0.13.0-alpine
+    should_exist $image bonsai-alpine
+    should_not_exist $image 0.13-edge-alpine
+    should_not_exist $image bonsai-edge-alpine
+  done
+
+TEST "run all binaries"
+  MAJOR_VERSION=0 MINOR_VERSION=13 PATCH_VERSION=0 CODENAME=bonsai \
+    docker buildx bake --progress=plain -f support/docker-bake.hcl all
+
+  # garden
+  docker run --platform=linux/amd64 --rm -it --entrypoint=garden gardendev/garden-aws-gcloud-azure --version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=garden gardendev/garden-aws-gcloud --version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=garden gardendev/garden-aws --version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=garden gardendev/garden-gcloud --version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=garden gardendev/garden-azure --version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=garden gardendev/garden:bonsai-alpine --version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=garden gardendev/garden:bonsai-buster --version
+
+  # aws
+  docker run --platform=linux/amd64 --rm -it --entrypoint=aws gardendev/garden-aws-gcloud-azure --version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=aws gardendev/garden-aws-gcloud --version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=aws gardendev/garden-aws --version
+
+  # Gcloud
+  docker run --platform=linux/amd64 --rm -it --entrypoint=gcloud gardendev/garden-aws-gcloud-azure version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=gcloud gardendev/garden-aws-gcloud version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=gcloud gardendev/garden-gcloud version
+
+  # Azure
+  docker run --platform=linux/amd64 --rm -it --entrypoint=az gardendev/garden-aws-gcloud-azure version
+  docker run --platform=linux/amd64 --rm -it --entrypoint=az gardendev/garden-azure version
