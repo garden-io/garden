57c5e11de7928f9e5e3cc614b1bfd366c130d8b5
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2023-05-19 20:04:43 +0200

    fix(exec): fix delete handler for persistent mode
    Before this fix, `exec` Deploys that were deployed with `persistent:
    true` in their config weren't being terminated when deleted (e.g. via
    the `cleanup deployment` command).
    
    This was fixed by killing child processes of the PID as well.

--- core/src/plugins/exec/deploy.ts
+++ core/src/plugins/exec/deploy.ts
@@ -11,7 +11,7 @@ import { join } from "path"
 import split2 = require("split2")
 import { PrimitiveMap } from "../../config/common"
 import { dedent } from "../../util/string"
-import { ExecOpts, sleep } from "../../util/util"
+import { ExecOpts, sleep, spawn } from "../../util/util"
 import { TimeoutError } from "../../exceptions"
 import { Log } from "../../logger/log-entry"
 import execa from "execa"
@@ -28,8 +28,7 @@ import { DeployActionHandler } from "../../plugin/action-types"
 import { deployStateToActionState, DeployStatus } from "../../plugin/handlers/Deploy/get-status"
 import { Resolved } from "../../actions/types"
 import { convertCommandSpec, execRun, getDefaultEnvVars } from "./common"
-import { kill } from "process"
-import { isRunning } from "../../process"
+import { isRunning, killRecursive } from "../../process"
 
 const persistentLocalProcRetryIntervalMs = 2500
 
@@ -93,7 +92,7 @@ export const getExecDeployLogs: DeployActionHandler<"getLogs", ExecDeploy> = asy
   return {}
 }
 
-export const execDeployAction: DeployActionHandler<"deploy", ExecDeploy> = async (params) => {
+export const deployExec: DeployActionHandler<"deploy", ExecDeploy> = async (params) => {
   const { action, log, ctx } = params
   const spec = action.getSpec()
 
@@ -287,8 +286,13 @@ async function killProcess(log: Log, pidFilePath: string, deployName: string) {
     if (pidString) {
       const oldPid = parseInt(pidString, 10)
       if (isRunning(oldPid)) {
-        kill(oldPid, "SIGINT")
-        log.debug(`Sent SIGINT to existing ${deployName} process (PID ${oldPid})`)
+        try {
+          await killRecursive("INT", oldPid)
+          log.debug(`Sent SIGINT to existing ${deployName} process (PID ${oldPid})`)
+        } catch (err) {
+          // This most likely means that the process had already been terminated, which is fine for our purposes here.
+          log.debug(`An error occurred while deleting existing ${deployName} process (PID ${oldPid}): ${err.message}`)
+        }
       }
     }
   } catch (err) {
