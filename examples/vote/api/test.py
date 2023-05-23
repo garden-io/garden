efe41512276dec5232e9ecc6bb0922fff926894b
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2023-05-23 09:10:45 +0200

    chore: update `requests` to `2.31.0` in Python examples and test apps

--- examples/vote/api/test.py
+++ examples/vote/api/test.py
@@ -1,7 +1,7 @@
 import requests
 import unittest
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from urllib3 import Retry
 
 retry_strategy = Retry(
     total=10,
