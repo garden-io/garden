ae86ce86c2ee4d947d63ef0bc0548b4c077803f5
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2023-05-23 14:27:12 +0200

    revert: fix: temporary hard-code 0.13.0 version in api client
    This reverts commit a3d2306cb86acc1a2d27961e2a943e9e1be00170.

--- core/src/cloud/api.ts
+++ core/src/cloud/api.ts
@@ -461,7 +461,7 @@ export class CloudApi {
     const requestObj = {
       method,
       headers: {
-        "x-garden-client-version": "0.13.0",
+        "x-garden-client-version": gardenClientVersion,
         "x-garden-client-name": gardenClientName,
         ...headers,
         ...makeAuthHeader(token || ""),
