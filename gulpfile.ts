1f7b5e1931563be5d27d84abe9b60200d2262c84
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2023-06-06 19:47:35 +0200

    chore: update critical dependencies

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,27 +5,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+import { gulpLicenseCheck } from "./scripts/gulp-license-check-plugin"
 
 const gulp = require("gulp")
-const checkLicense = require("gulp-license-check")
-
-const sources = [
-  "core/src/**/*.ts",
-  "core/test/**/*.ts",
-  "core/src/**/*.pegjs",
-  "plugins/**/*.ts",
-  "sdk/**/*.ts",
-]
+const sources = ["core/src/**/*.ts", "core/test/**/*.ts", "core/src/**/*.pegjs", "plugins/**/*.ts", "sdk/**/*.ts"]
 const licenseHeaderPath = "support/license-header-js.txt"
 
 process.env.FORCE_COLOR = "true"
 
 gulp.task("check-licenses", () =>
-  gulp.src(sources, { ignore: ["**/*.d.ts", "**/node_modules/**/*", "core/src/lib/**/*"] })
-    .pipe(checkLicense({
+  gulp.src(sources, { ignore: ["**/*.d.ts", "**/node_modules/**/*", "core/src/lib/**/*"] }).pipe(
+    gulpLicenseCheck({
       path: licenseHeaderPath,
       blocking: true,
-      logInfo: false,
+      logInfo: true,
       logError: true,
-    })),
+    })
+  )
 )
