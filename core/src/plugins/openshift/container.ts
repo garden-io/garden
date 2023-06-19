8ba4d69b862888ebd32ff14fcca78586f19cf1c6
Walther <veeti.haapsamo@gmail.com>
2023-06-19 15:36:05 +0300

    chore(openshift): add license header

--- core/src/plugins/openshift/container.ts
+++ core/src/plugins/openshift/container.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2018-2023 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { DeployActionExtension, DeployActionHandler } from "../../plugin/action-types"
 import { ContainerDeployAction } from "../container/moduleConfig"
 import { KubeApi } from "../kubernetes/api"
