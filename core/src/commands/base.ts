garden-io/garden

commit 4cdad7f502d88dd2b08d9bec663684b5359d87ce

fix(cloud): properly handle dev delegation (#4675)
When a command is run with an option that results in it being run inside
the `dev` command instead (e.g. `garden deploy --sync`), we now register
the initial command session with Cloud as a `dev` command with the
`--cmd` option set appropriately. This is useful for Cloud when listing
command sessions created this way.

For good measure, we include an explicit `isDevCommand` parameter with
the session registration request to Cloud to make this setup more robust
against future changes (instead of inferring whether the command is a
`dev` command from the `commandInfo` map).

core/src/commands/base.ts
@@ -55,6 +55,7 @@ import { GraphResultMapWithoutTask, GraphResultWithoutTask, GraphResults } from 
 import { splitFirst } from "../util/string"
 import { ActionMode } from "../actions/types"
 import { AnalyticsHandler } from "../analytics/analytics"
+import { getCmdOptionForDev } from "./helpers"
 
 export interface CommandConstructor {
   new (parent?: CommandGroup): Command
@@ -258,17 +259,18 @@ export abstract class Command<A extends Parameters = {}, O extends Parameters = 
    *
    * @returns The result from the command action
    */
-  async run({
-    garden: parentGarden,
-    args,
-    opts,
-    cli,
-    commandLine,
-    sessionId,
-    parentCommand,
-    parentSessionId,
-    overrideLogLevel,
-  }: RunCommandParams<A, O>): Promise<CommandResult<R>> {
+  async run(params: RunCommandParams<A, O>): Promise<CommandResult<R>> {
+    const {
+      garden: parentGarden,
+      args,
+      opts,
+      cli,
+      commandLine,
+      sessionId,
+      parentCommand,
+      parentSessionId,
+      overrideLogLevel,
+    } = params
     const commandStartTime = new Date()
     const server = this.server
 
@@ -284,6 +286,17 @@ export abstract class Command<A extends Parameters = {}, O extends Parameters = 
 
     let cloudSession: CloudSession | undefined
 
+    // It's not ideal that we have to update the command info here after init, but we don't know whether we're
+    // going into a subcommand in the `GardenCli` class where the `commandInfo` object is first created.
+    if (!["dev", "serve"].includes(this.name) && this.maybePersistent(params) && !params.parentCommand) {
+      // Then this command will be starting a `dev` command and then running itself from there, so we update
+      // the `commandInfo` accordingly.
+      // Example: `garden deploy --sync`.
+      const outerName = this.name
+      garden.commandInfo.name = "dev"
+      garden.commandInfo.opts.cmd = getCmdOptionForDev(outerName, params)
+    }
+
     if (garden.cloudApi && garden.projectId && this.streamEvents) {
       cloudSession = await garden.cloudApi.registerSession({
         parentSessionId: parentSessionId || undefined,
@@ -293,6 +306,7 @@ export abstract class Command<A extends Parameters = {}, O extends Parameters = 
         localServerPort: server?.port,
         environment: garden.environmentName,
         namespace: garden.namespace,
+        isDevCommand: garden.commandInfo.name === "dev"
       })
     }
 

core/src/commands/deploy.ts
@@ -21,7 +21,7 @@ import {
   emptyActionResults,
 } from "./base"
 import { printEmoji, printHeader } from "../logger/util"
-import { watchParameter, watchRemovedWarning } from "./helpers"
+import { getCmdOptionForDev, watchParameter, watchRemovedWarning } from "./helpers"
 import { DeployTask } from "../tasks/deploy"
 import { naturalList } from "../util/string"
 import { StringsParameter, BooleanParameter } from "../cli/params"
@@ -182,8 +182,7 @@ export class DeployCommand extends Command<Args, Opts> {
     const monitor = this.maybePersistent(params)
     if (monitor && !params.parentCommand) {
       // Then we're not in the dev command yet, so we call that instead with the appropriate initial command.
-      // TODO: Abstract this delegation process into a helper if we write more commands that do this sort of thing.
-      params.opts.cmd = ["deploy " + params.args.$all!.join(" ")]
+      params.opts.cmd = getCmdOptionForDev("deploy", params)
       const devCmd = new DevCommand()
       devCmd.printHeader(params)
       await devCmd.prepare(params)

core/src/commands/helpers.ts
@@ -19,9 +19,14 @@ import { ActionKind } from "../actions/types"
 import isGlob from "is-glob"
 import { ParameterError } from "../exceptions"
 import { naturalList } from "../util/string"
+import { CommandParams } from "./base"
 
 export function makeGetTestOrTaskLog(actions: (TestAction | RunAction)[]) {
   return actions.map((t) => prettyPrintTestOrTask(t)).join("\n")
+}
+
+export function getCmdOptionForDev(commandName: string, params: CommandParams) {
+  return [commandName + " " + params.args.$all?.join(" ")]
 }
 
 export function prettyPrintWorkflow(workflow: WorkflowConfig): string {

core/src/commands/serve.ts
@@ -166,6 +166,7 @@ export class ServeCommand<
             localServerPort: this.server.port,
             environment: defaultGarden.environmentName,
             namespace: defaultGarden.namespace,
+            isDevCommand: false,
           })
         }
       }

core/src/commands/up.ts
@@ -13,6 +13,7 @@ import { deployArgs, DeployCommand, deployOpts } from "./deploy"
 import { serveOpts } from "./serve"
 import { DevCommand } from "./dev"
 import type { LoggerType } from "../logger/logger"
+import { getCmdOptionForDev } from "./helpers"
 
 const upArgs = {
   ...deployArgs,
@@ -51,7 +52,7 @@ export class UpCommand extends Command<UpArgs, UpOpts> {
       cmd = new DeployCommand()
       params.opts.logs = true
     } else {
-      params.opts.cmd = ["deploy --logs " + params.args.$all!.join(" ")]
+      params.opts.cmd = getCmdOptionForDev("deploy --logs", params)
     }
 
     cmd.printHeader(params)

core/src/cloud/api.ts
@@ -586,6 +586,7 @@ export class CloudApi {
     localServerPort,
     environment,
     namespace,
+    isDevCommand,
   }: {
     parentSessionId: string | undefined
     sessionId: string
@@ -594,6 +595,7 @@ export class CloudApi {
     localServerPort?: number
     environment: string
     namespace: string
+    isDevCommand: boolean
   }): Promise<CloudSession | undefined> {
     let session = this.registeredSessions.get(sessionId)
 
@@ -610,6 +612,7 @@ export class CloudApi {
         projectUid: projectId,
         environment,
         namespace,
+        isDevCommand,
       }
       this.log.debug(`Registering session with ${this.distroName} for ${projectId} in ${environment}/${namespace}.`)
       const res: CloudSessionResponse = await this.post("sessions", {

commit 2a2513619e230c2ca3b23f2833be4ea06eea4a4e

fix(cloud): fix session registration flow for dev
This fixes some problems/limitations with the changes introduced in
`4cdad7f502d88dd2b08d9bec663684b5359d87ce` (see
https://github.com/garden-io/garden/pull/4675).

Tests for the total flow will follow in a separate PR.

core/src/commands/base.ts
@@ -55,7 +55,6 @@ import { GraphResultMapWithoutTask, GraphResultWithoutTask, GraphResults } from 
 import { splitFirst } from "../util/string"
 import { ActionMode } from "../actions/types"
 import { AnalyticsHandler } from "../analytics/analytics"
-import { getCmdOptionForDev } from "./helpers"
 
 export interface CommandConstructor {
   new (parent?: CommandGroup): Command
@@ -286,18 +285,16 @@ export abstract class Command<A extends Parameters = {}, O extends Parameters = 
 
     let cloudSession: CloudSession | undefined
 
-    // It's not ideal that we have to update the command info here after init, but we don't know whether we're
-    // going into a subcommand in the `GardenCli` class where the `commandInfo` object is first created.
-    if (!["dev", "serve"].includes(this.name) && this.maybePersistent(params) && !params.parentCommand) {
-      // Then this command will be starting a `dev` command and then running itself from there, so we update
-      // the `commandInfo` accordingly.
-      // Example: `garden deploy --sync`.
-      const outerName = this.name
-      garden.commandInfo.name = "dev"
-      garden.commandInfo.opts.cmd = getCmdOptionForDev(outerName, params)
-    }
+    // Session registration for the `dev` and `serve` commands is handled in the `serve` command's `action` method,
+    // so we skip registering here to avoid duplication.
+    //
+    // Persistent commands other than `dev` and `serve` (i.e. commands that delegate to the `dev` command, like
+    // `deploy --sync`) are also not registered here, since the `dev` command will have been registered already,
+    // and the `deploy --sync` command which is subsequently run interactively in the `dev` session will register
+    // itself (it will have a parent command, so the last condition in this expression will not match).
+    const skipRegistration = !["dev", "serve"].includes(this.name) && this.maybePersistent(params) && !params.parentCommand
 
-    if (garden.cloudApi && garden.projectId && this.streamEvents) {
+    if (!skipRegistration && garden.cloudApi && garden.projectId && this.streamEvents) {
       cloudSession = await garden.cloudApi.registerSession({
         parentSessionId: parentSessionId || undefined,
         sessionId: garden.sessionId,

core/src/commands/deploy.ts
@@ -21,7 +21,7 @@ import {
   emptyActionResults,
 } from "./base"
 import { printEmoji, printHeader } from "../logger/util"
-import { getCmdOptionForDev, watchParameter, watchRemovedWarning } from "./helpers"
+import { runAsDevCommand, watchParameter, watchRemovedWarning } from "./helpers"
 import { DeployTask } from "../tasks/deploy"
 import { naturalList } from "../util/string"
 import { StringsParameter, BooleanParameter } from "../cli/params"
@@ -35,7 +35,6 @@ import { PortForwardMonitor } from "../monitors/port-forward"
 import { LogMonitor } from "../monitors/logs"
 import { LoggerType, parseLogLevel } from "../logger/logger"
 import { serveOpts } from "./serve"
-import { DevCommand } from "./dev"
 import { gardenEnv } from "../constants"
 
 export const deployArgs = {
@@ -182,12 +181,7 @@ export class DeployCommand extends Command<Args, Opts> {
     const monitor = this.maybePersistent(params)
     if (monitor && !params.parentCommand) {
       // Then we're not in the dev command yet, so we call that instead with the appropriate initial command.
-      params.opts.cmd = getCmdOptionForDev("deploy", params)
-      const devCmd = new DevCommand()
-      devCmd.printHeader(params)
-      await devCmd.prepare(params)
-
-      return devCmd.action(params)
+      return runAsDevCommand("deploy", params)
     }
 
     const disablePortForwards = gardenEnv.GARDEN_DISABLE_PORT_FORWARDS || opts["disable-port-forwards"] || false

core/src/commands/helpers.ts
@@ -20,9 +20,33 @@ import isGlob from "is-glob"
 import { ParameterError } from "../exceptions"
 import { naturalList } from "../util/string"
 import { CommandParams } from "./base"
+import { ServeCommandOpts } from "./serve"
+import { DevCommand } from "./dev"
 
 export function makeGetTestOrTaskLog(actions: (TestAction | RunAction)[]) {
   return actions.map((t) => prettyPrintTestOrTask(t)).join("\n")
+}
+
+/**
+ * Runs a `dev` command and runs `commandName` with the args & opts provided in `params` as the first
+ * interactive command.
+ *
+ * Also updates the `commandInfo` accordinly so that the session registration parameters sent to Cloud are correct.
+ */
+export async function runAsDevCommand(
+  commandName: string, // The calling command's opts need to extend `ServeCommandOpts`.
+  params: CommandParams<{}, ServeCommandOpts>
+) {
+  const commandInfo = params.garden.commandInfo
+  params.opts.cmd = getCmdOptionForDev(commandName, params)
+  commandInfo.name = "dev"
+  commandInfo.args["$all"] = []
+  commandInfo.opts.cmd = params.opts.cmd
+  const devCmd = new DevCommand()
+  devCmd.printHeader(params)
+  await devCmd.prepare(params)
+
+  return devCmd.action(params)
 }
 
 export function getCmdOptionForDev(commandName: string, params: CommandParams) {

core/src/commands/serve.ts
@@ -166,7 +166,7 @@ export class ServeCommand<
             localServerPort: this.server.port,
             environment: defaultGarden.environmentName,
             namespace: defaultGarden.namespace,
-            isDevCommand: false,
+            isDevCommand: true,
           })
           if (session?.shortId) {
             const distroName = getCloudDistributionName(cloudDomain)

core/src/commands/up.ts
@@ -11,9 +11,8 @@ import { Command, CommandParams, CommandResult } from "./base"
 import { dedent } from "../util/string"
 import { deployArgs, DeployCommand, deployOpts } from "./deploy"
 import { serveOpts } from "./serve"
-import { DevCommand } from "./dev"
 import type { LoggerType } from "../logger/logger"
-import { getCmdOptionForDev } from "./helpers"
+import { runAsDevCommand } from "./helpers"
 
 const upArgs = {
   ...deployArgs,
@@ -45,15 +44,14 @@ export class UpCommand extends Command<UpArgs, UpOpts> {
   }
 
   async action(params: CommandParams<UpArgs, UpOpts>): Promise<CommandResult> {
-    let cmd: Command = new DevCommand()
-
-    if (params.commandLine) {
-      // We're already in the dev command
-      cmd = new DeployCommand()
-      params.opts.logs = true
-    } else {
-      params.opts.cmd = getCmdOptionForDev("deploy --logs", params)
+    if (!params.commandLine) {
+      // Then we start a dev command and run `deploy --logs` as the first interactive command.
+      return runAsDevCommand("deploy --logs", params)
     }
+
+    params.opts.logs = true
+    // Else, we're already in the dev command.
+    const cmd = new DeployCommand()
 
     cmd.printHeader(params)
     await cmd.prepare(params)