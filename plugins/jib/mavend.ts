f65f3bb44366312338a8a5760ff75f9de7953cd5
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2023-06-22 16:58:25 +0200

    docs(jib): show current Maven Daemon version in docs

--- plugins/jib/mavend.ts
+++ plugins/jib/mavend.ts
@@ -14,7 +14,7 @@ import { BuildToolParams, runBuildTool, verifyBinaryPath, VerifyBinaryParams } f
 
 const buildLock = new AsyncLock()
 
-const mvndVersion = "0.9.0"
+export const mvndVersion = "0.9.0"
 
 const mvndSpec = {
   description: "The Maven Daemon CLI.",
