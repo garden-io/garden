1a8b31538ae131e9665e973635197b1c2db8ce6c
Walther <veeti.haapsamo@gmail.com>
2023-07-04 13:03:23 +0300

    fix: respect deploymentRegistry in the garden publish command

--- core/src/plugins/kubernetes/container/publish.ts
+++ core/src/plugins/kubernetes/container/publish.ts
@@ -8,14 +8,19 @@
 
 import { ContainerBuildAction } from "../../container/moduleConfig"
 import { KubernetesPluginContext } from "../config"
-import { publishContainerBuild } from "../../container/publish"
 import { pullBuild } from "../commands/pull-image"
 import { BuildActionHandler } from "../../../plugin/action-types"
+import { containerHelpers } from "../../container/helpers"
 
 export const k8sPublishContainerBuild: BuildActionHandler<"publish", ContainerBuildAction> = async (params) => {
   const { ctx, action, log } = params
   const k8sCtx = ctx as KubernetesPluginContext
   const provider = k8sCtx.provider
+
+  const localId = action.getOutput("localImageId")
+  // NOTE: this may contain a custom deploymentRegistry, from the kubernetes provider config
+  // We cannot combine this publish method with the container plugin's publish method, because it won't have the context
+  const remoteId = action.getOutput("deploymentImageId")
 
   if (provider.config.buildMode !== "local-docker") {
     // First pull from the remote registry, then resume standard publish flow.
@@ -25,10 +30,17 @@ export const k8sPublishContainerBuild: BuildActionHandler<"publish", ContainerBu
     // We also generally prefer this because the remote cluster very likely doesn't (and shouldn't) have
     // privileges to push to production registries.
     log.info(`Pulling from remote registry...`)
-    const localId = action.getOutput("localImageId")
-    const remoteId = action.getOutput("deploymentImageId")
     await pullBuild({ ctx: k8sCtx, action, log, localId, remoteId })
   }
 
-  return publishContainerBuild({ ...params, ctx: { ...ctx, provider: provider.dependencies.container } })
+  log.info({ msg: `Publishing image ${remoteId}...` })
+  // TODO: stream output to log if at debug log level
+  await containerHelpers.dockerCli({ cwd: action.getBuildPath(), args: ["push", remoteId], log, ctx })
+
+  return {
+    state: "ready",
+    detail: { published: true, message: `Published ${remoteId}` },
+    // TODO-0.13.1
+    outputs: {},
+  }
 }
