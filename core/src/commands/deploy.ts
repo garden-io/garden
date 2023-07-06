6a6f69ba84b17eeff7f288a14e100a37b93eb67a
Orzelius <albertkostusev@gmail.com>
2023-07-06 11:47:17 +0300

    docs: improve compy

--- core/src/commands/deploy.ts
+++ core/src/commands/deploy.ts
@@ -87,9 +87,8 @@ export const deployOpts = {
   }),
   "skip-dependencies": new BooleanParameter({
     help: deline`
-    Deploy the specified actions, but don't deploy or run any dependencies.
+    Skip deploy and run dependencies. Build dependencies and runtime output reference dependencies are not skipped.
     This can be useful e.g. when your stack has already been deployed, and you want to run specific Deploys in sync mode without deploying or running dependencies that may have changed since you last deployed.
-    Build dependencies and runtime output reference dependencies are not skipped.
     `,
     aliases: ["nodeps"],
   }),
