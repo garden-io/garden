c1deba3ef736e0538cca8bca28e2b1417cee67f6
Tim Beyer <tim.beyer@gmail.com>
2023-07-07 15:13:25 +0200

    fix: license headers

--- core/src/plugins/otel-collector/config/datadog.ts
+++ core/src/plugins/otel-collector/config/datadog.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2018-2023 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { hostname } from "os"
 
 export type DatadogExporterConfigPartial = {
@@ -26,7 +34,6 @@ export type OtelCollectorDatadogConfiguration = {
   site: string
   apiKey: string
 }
-
 
 export function makeDatadogPartialConfig(config: OtelCollectorDatadogConfiguration): DatadogExporterConfigPartial {
   return {
