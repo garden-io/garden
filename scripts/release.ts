garden-io/garden

commit 409899599c81409852f08b4de1e4f44acce1e84f

chore: fix changelog generation (#4827)
* chore: fix changelog generation

Aligned with `draft-release-notes.ts` script.
Used explicit version range instead of experimental `--next-tag` flag.

* refactor(chore): extract helper to generate changelog

scripts/release.ts
@@ -4,12 +4,14 @@ import execa from "execa"
 import semver from "semver"
 import inquirer from "inquirer"
 import chalk from "chalk"
+import { relative, resolve } from "path"
+import { createWriteStream, readFile, writeFile } from "fs-extra"
+import { getPackages } from "./script-utils"
+import { getChangelog } from "./changelog"
 import parseArgs = require("minimist")
 import deline = require("deline")
-import { resolve, relative } from "path"
-import { readFile, createWriteStream, writeFile } from "fs-extra"
-import { getPackages } from "./script-utils"
 import Bluebird = require("bluebird")
+
 const replace = require("replace-in-file")
 
 type ReleaseType = "minor" | "patch" | "preminor" | "prepatch" | "prerelease"
@@ -279,13 +281,7 @@ async function updateChangelog(version: string) {
   const changelogPath = "./CHANGELOG.md"
   // TODO: Use readStream and pipe
   const changelog = await readFile(changelogPath)
-  const nextChangelogEntry = (
-    await execa(
-      "git-chglog",
-      ["--tag-filter-pattern", "^\\d+\\.\\d+\\.\\d+$", "--sort", "semver", "--next-tag", version, version],
-      { cwd: gardenRoot }
-    )
-  ).stdout
+  const nextChangelogEntry = getChangelog(version)
   return new Promise((resolve, reject) => {
     const writeStream = createWriteStream(changelogPath)
     writeStream.write(nextChangelogEntry)

scripts/changelog.ts
@@ -1,0 +1,20 @@
+#!/usr/bin/env ts-node
+
+import execa from "execa"
+import { resolve } from "path"
+
+const gardenRoot = resolve(__dirname, "..")
+
+export async function getChangelog(curReleaseTag: string) {
+  try {
+    return (
+      await execa(
+        "git-chglog",
+        ["--tag-filter-pattern", "^\\d+\\.\\d+\\.\\d+$", "--sort", "semver", `${curReleaseTag}..${curReleaseTag}`],
+        { cwd: gardenRoot }
+      )
+    ).stdout
+  } catch (err) {
+    throw new Error(`Error generating changelog: ${err}`)
+  }
+}

scripts/draft-release-notes.ts
@@ -1,27 +1,13 @@
 #!/usr/bin/env ts-node
 
-import execa from "execa"
 import { writeFile } from "fs-extra"
 import { execSync } from "child_process"
 import { resolve } from "path"
 import { dedent } from "@garden-io/sdk/util/string"
+import { getChangelog } from "./changelog"
 import parseArgs = require("minimist")
 
 const gardenRoot = resolve(__dirname, "..")
-
-async function getChangelog(curReleaseTag: string) {
-  try {
-    return (
-      await execa(
-        "git-chglog",
-        ["--tag-filter-pattern", "^\\d+\\.\\d+\\.\\d+$", "--sort", "semver", `${curReleaseTag}..${curReleaseTag}`],
-        { cwd: gardenRoot }
-      )
-    ).stdout
-  } catch (err) {
-    throw new Error(`Error generating changelog: ${err}`)
-  }
-}
 
 function getContributors(prevReleaseTag: string, curReleaseTag: string) {
   try {

commit 9a2e360d2a13912d3b04879b10ee605c61527d87

chore: fix type error in the release script
Fix type error introduced in #4827.

scripts/release.ts
@@ -281,7 +281,7 @@ async function updateChangelog(version: string) {
   const changelogPath = "./CHANGELOG.md"
   // TODO: Use readStream and pipe
   const changelog = await readFile(changelogPath)
-  const nextChangelogEntry = getChangelog(version)
+  const nextChangelogEntry = await getChangelog(version)
   return new Promise((resolve, reject) => {
     const writeStream = createWriteStream(changelogPath)
     writeStream.write(nextChangelogEntry)