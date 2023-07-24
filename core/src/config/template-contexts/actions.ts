garden-io/garden

commit 3773e2ed31ed91d85e03f1cfd8c5f24b139f1b33

feat(template): allow variable references in include/exclude config

core/src/config/template-contexts/actions.ts
@@ -61,6 +61,7 @@ interface ActionConfigContextParams {
   garden: Garden
   config: ActionConfig | WorkflowConfig
   thisContextParams: ActionConfigThisContextParams
+  variables: DeepPrimitiveMap
 }
 
 /**
@@ -71,9 +72,10 @@ export class ActionConfigContext extends TemplatableConfigContext {
   @schema(ActionConfigThisContext.getSchema().description("Information about the action currently being resolved."))
   public this: ActionConfigThisContext
 
-  constructor({ garden, config, thisContextParams }: ActionConfigContextParams) {
+  constructor({ garden, config, thisContextParams, variables }: ActionConfigContextParams) {
     super(garden, config)
     this.this = new ActionConfigThisContext(this, thisContextParams)
+    this.variables = this.var = variables
   }
 }
 

core/src/graph/actions.ts
@@ -453,7 +453,7 @@ function getActionSchema(kind: ActionKind) {
   }
 }
 
-const preprocessActionConfig = profileAsync(async function preprocessActionConfig({
+export const preprocessActionConfig = profileAsync(async function preprocessActionConfig({
   garden,
   config,
   mode,
@@ -469,6 +469,25 @@ const preprocessActionConfig = profileAsync(async function preprocessActionConfi
   const description = describeActionConfig(config)
   const templateName = config.internal.templateName
 
+  const variables = await mergeVariables({
+    basePath: config.internal.basePath,
+    variables: config.variables,
+    varfiles: config.varfiles,
+  })
+  const resolvedVariables = resolveTemplateStrings(
+    variables,
+    new ActionConfigContext({
+      garden,
+      config: { ...config, internal: { ...config.internal, inputs: {} } },
+      thisContextParams: {
+        mode,
+        name: config.name,
+      },
+      variables,
+    }),
+    { allowPartial: true }
+  )
+
   if (templateName) {
     // Partially resolve inputs
     const partiallyResolvedInputs = resolveTemplateStrings(
@@ -480,10 +499,9 @@ const preprocessActionConfig = profileAsync(async function preprocessActionConfi
           mode,
           name: config.name,
         },
+        variables: resolvedVariables,
       }),
-      {
-        allowPartial: true,
-      }
+      { allowPartial: true }
     )
 
     const template = garden.configTemplates[templateName]
@@ -516,6 +534,7 @@ const preprocessActionConfig = profileAsync(async function preprocessActionConfi
       mode,
       name: config.name,
     },
+    variables: resolvedVariables,
   })
 
   function resolveTemplates() {
@@ -525,7 +544,7 @@ const preprocessActionConfig = profileAsync(async function preprocessActionConfi
       allowPartial: false,
     })
     config = { ...config, ...resolvedBuiltin }
-    const { spec = {}, variables = {} } = config
+    const { spec = {} } = config
 
     // Validate fully resolved keys (the above + those that don't allow any templating)
     // TODO-0.13.1: better error messages when something goes wrong here
@@ -542,7 +561,7 @@ const preprocessActionConfig = profileAsync(async function preprocessActionConfi
       projectRoot: garden.projectRoot,
     })
 
-    config = { ...config, variables, spec }
+    config = { ...config, variables: resolvedVariables, spec }
 
     // Partially resolve other fields
     // TODO-0.13.1: better error messages when something goes wrong here (missing inputs for example)

core/test/unit/src/graph/actions.ts
@@ -1,0 +1,83 @@
+/*
+ * Copyright (C) 2018-2023 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { makeGarden, makeTempDir, noOpTestPlugin, TestGarden } from "../../../helpers"
+import { preprocessActionConfig } from "../../../../src/graph/actions"
+import { RunActionConfig } from "../../../../src/actions/run"
+import { DEFAULT_RUN_TIMEOUT_SEC } from "../../../../src/constants"
+import tmp from "tmp-promise"
+import { expect } from "chai"
+
+// TODO: add more tests
+describe("preprocessActionConfig", () => {
+  let tmpDir: tmp.DirectoryResult
+  let garden: TestGarden
+
+  before(async () => {
+    tmpDir = await makeTempDir({ git: true, initialCommit: false })
+    garden = await makeGarden(tmpDir, noOpTestPlugin())
+  })
+
+  after(async () => {
+    await tmpDir.cleanup()
+  })
+
+  context("template strings", () => {
+    context("include/exclude configs", () => {
+      it("should resolve variables in 'exclude' config", async () => {
+        const config: RunActionConfig = {
+          internal: { basePath: tmpDir.path },
+          timeout: DEFAULT_RUN_TIMEOUT_SEC,
+          kind: "Run",
+          type: "exec",
+          name: "run",
+          exclude: ["${var.anyVar}"],
+          variables: {
+            anyVar: "*/**",
+          },
+          spec: { command: ["echo", "foo"] },
+        }
+        const router = await garden.getActionRouter()
+        const res = await preprocessActionConfig({
+          garden,
+          config,
+          router,
+          mode: "default",
+          log: garden.log,
+        })
+
+        expect(res.config.exclude).to.eql([config.variables?.["anyVar"]])
+      })
+
+      it("should resolve variables in 'include' config", async () => {
+        const config: RunActionConfig = {
+          internal: { basePath: tmpDir.path },
+          timeout: DEFAULT_RUN_TIMEOUT_SEC,
+          kind: "Run",
+          type: "exec",
+          name: "run",
+          include: ["${var.anyVar}"],
+          variables: {
+            anyVar: "*/**",
+          },
+          spec: { command: ["echo", "foo"] },
+        }
+        const router = await garden.getActionRouter()
+        const res = await preprocessActionConfig({
+          garden,
+          config,
+          router,
+          mode: "default",
+          log: garden.log,
+        })
+
+        expect(res.config.include).to.eql([config.variables?.["anyVar"]])
+      })
+    })
+  })
+})

core/src/router/base.ts
@@ -303,6 +303,7 @@ export abstract class BaseActionRouter<K extends ActionKind> extends BaseRouter 
             mode: action.mode(),
             name: action.name,
           },
+          variables: action.getVariables(),
         })
 
     const handlerParams = {

plugins/pulumi/commands.ts
@@ -179,6 +179,7 @@ const makePluginContextForDeploy = async (params: PulumiParams & { garden: Garde
       name: action.name,
       mode: action.mode(),
     },
+    variables: action.getVariables(),
   })
   const ctxForDeploy = await garden.getPluginContext({ provider, templateContext, events: ctx.events })
   return ctxForDeploy

commit aa8d593780bce4e2aa1f8e1701f31bab222f4492

fix(template): respect project level variables in action config context
The regression was introduced in #4659.
In that PR `this.variables` field was overwritten with the action-level values.
It caused the erasure `this.variables = garden.variables` assignment
in the super-class constructor.

core/src/config/template-contexts/actions.ts
@@ -8,6 +8,7 @@
 
 import chalk from "chalk"
 import { merge } from "lodash"
+import type { ActionConfig } from "../../actions/types"
 import { Action, ActionMode, ExecutedAction, ResolvedAction } from "../../actions/types"
 import { Garden } from "../../garden"
 import { GardenModule } from "../../types/module"
@@ -22,12 +23,19 @@ import {
   PrimitiveMap,
 } from "../common"
 import { ProviderMap } from "../provider"
-import { ConfigContext, ErrorContext, schema, ParentContext, TemplateContext } from "./base"
+import { ConfigContext, ErrorContext, ParentContext, schema, TemplateContext } from "./base"
 import { exampleVersion, OutputConfigContext } from "./module"
 import { TemplatableConfigContext } from "./project"
 import { DOCS_BASE_URL } from "../../constants"
-import type { ActionConfig } from "../../actions/types"
 import type { WorkflowConfig } from "../workflow"
+
+function mergeVariables({ garden, variables }: { garden: Garden; variables: DeepPrimitiveMap }): DeepPrimitiveMap {
+  const mergedVariables: DeepPrimitiveMap = {}
+  merge(mergedVariables, garden.variables)
+  merge(mergedVariables, variables)
+  merge(mergedVariables, garden.variableOverrides)
+  return mergedVariables
+}
 
 type ActionConfigThisContextParams = Pick<ActionReferenceContextParams, "name" | "mode">
 
@@ -73,9 +81,10 @@ export class ActionConfigContext extends TemplatableConfigContext {
   public this: ActionConfigThisContext
 
   constructor({ garden, config, thisContextParams, variables }: ActionConfigContextParams) {
+    const mergedVariables = mergeVariables({ garden, variables })
     super(garden, config)
     this.this = new ActionConfigThisContext(this, thisContextParams)
-    this.variables = this.var = variables
+    this.variables = this.var = mergedVariables
   }
 }
 
@@ -279,10 +288,7 @@ export class ActionSpecContext extends OutputConfigContext {
 
     const internal = action.getInternal()
 
-    const mergedVariables: DeepPrimitiveMap = {}
-    merge(mergedVariables, garden.variables)
-    merge(mergedVariables, variables)
-    merge(mergedVariables, garden.variableOverrides)
+    const mergedVariables = mergeVariables({ garden, variables })
 
     super({
       ...params,