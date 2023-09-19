5f2e1026391ee263cf5971a1fe8347e503c2bbfa
Steffen Neubauer <steffen@garden.io>
2023-09-19 17:23:00 +0200

    improvement: allow dev@garden.io to impersonate service account for tests
    This allows google artifact registry access in the garden-ci project.
    Using the service account impersonation has the benefit that we can allow any google group to impersonate this service account, so we do not need to manage access manually:
    This only works because we granted the google group "dev@garden.io" permission to impersonate this service account with the following command:
    `gcloud iam service-accounts add-iam-policy-binding gar-serviceaccount@garden-ci.iam.gserviceaccount.com --member=group:dev@garden.io --role=roles/iam.serviceAccountTokenCreator`
    We could extend this in the future to allow access to the CI cluster in the same way, without the need to manage users manually.
    
    Co-authored-by: twelvemo <anna@garden.io>

--- core/test/integ/helpers.ts
+++ core/test/integ/helpers.ts
@@ -6,59 +6,65 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { GoogleAuth, DownscopedClient } from "google-auth-library"
+import { GoogleAuth, Impersonated } from "google-auth-library"
 import { expect } from "chai"
 import { base64 } from "../../src/util/string"
 
 export async function getGoogleADCImagePullSecret() {
-  let token: string
+  let token: string | null | undefined
 
   try {
     // This will use ADC to get the credentials used for the downscoped client.
-    const googleAuth = new GoogleAuth({
-      scopes: ["https://www.googleapis.com/auth/cloud-platform"],
-      projectId: "garden-ci",
+    const googleAuth = new GoogleAuth()
+
+    const client = await googleAuth.getClient()
+
+    // Impersonate "gar-serviceaccount"
+    // This allows google artifact registry access in the garden-ci project.
+    // Using the service account impersonation has the benefit that we can allow any google group to impersonate this service account, so we do not need to manage access manually:
+    // This only works because we granted the google group "dev@garden.io" permission to impersonate this service account with the following command:
+    // `gcloud iam service-accounts add-iam-policy-binding gar-serviceaccount@garden-ci.iam.gserviceaccount.com --member=group:dev@garden.io --role=roles/iam.serviceAccountTokenCreator`
+    // We could extend this in the future to allow access to the CI cluster in the same way, without the need to manage users manually.
+    let targetClient = new Impersonated({
+      sourceClient: client,
+      targetPrincipal: "gar-serviceaccount@garden-ci.iam.gserviceaccount.com",
+      lifetime: 3600,
+      delegates: [],
+      targetScopes: ["https://www.googleapis.com/auth/cloud-platform"],
     })
 
     // Obtain an authenticated client via ADC.
-    const client = await googleAuth.getClient()
-
-    // Use the client to create a DownscopedClient.
-    const cabClient = new DownscopedClient(client, {
-      accessBoundary: {
-        accessBoundaryRules: [
-          {
-            availablePermissions: ["inRole:roles/artifactregistry.repoAdmin"],
-            availableResource: "//artifactregistry.googleapis.com/projects/garden-ci/locations/_/repositories/_",
-          },
-        ],
-      },
-    })
-
-    // Refresh the tokens.
-    const refreshedAccessToken = await cabClient.getAccessToken()
-
-    // This will need to be passed to the token consumer.
-    if (!refreshedAccessToken.token) {
-      expect.fail("Failed to downscope token for image pull secret via ADC: token was not set")
-    }
-
-    token = refreshedAccessToken.token
+    token = (await targetClient.getAccessToken()).token
   } catch (err) {
     if (!(err instanceof Error)) {
       throw err
     }
-    if (err.message.includes("API has not been used in project") || err.message.includes("Could not load the default credentials")) {
-      expect.fail("Could not get downscoped token: Not authenticated to gcloud. Please run the command 'gcloud auth application-default login --project garden-ci'")
+    if (
+      err.message.includes("API has not been used in project") ||
+      err.message.includes("Could not load the default credentials")
+    ) {
+      expect.fail(
+        "Could not get downscoped token: Not authenticated to gcloud. Please run the command 'gcloud auth application-default login --project garden-ci'"
+      )
     }
     throw err
   }
 
+  if (!token) {
+    expect.fail("Failed to downscope token for image pull secret via ADC: token was not set")
+  }
+
   return {
     auths: {
-      "eu.gcr.io": {
+      "europe-west3-docker.pkg.dev": {
         auth: base64(`oauth2accesstoken:${token}`),
-      }
-    }
+      },
+      "pkg.dev": {
+        auth: base64(`oauth2accesstoken:${token}`),
+      },
+      "europe-docker.pkg.dev": {
+        auth: base64(`oauth2accesstoken:${token}`),
+      },
+    },
   }
 }
