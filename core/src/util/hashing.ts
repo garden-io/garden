163beb3b398e2147dd81607963d903fb6e9e1352
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2023-09-25 11:18:09 +0200

    refactor: move hash-helper function to a dedicated module

--- core/src/util/hashing.ts
+++ core/src/util/hashing.ts
@@ -12,3 +12,9 @@ const sha256Regex = /^[a-f0-9]{64}$/gi
 export function isSha256(str: string): boolean {
   return sha256Regex.test(str)
 }
+
+const sha1Regex = new RegExp(/\b([a-f0-9]{40})\b/)
+
+export function isSha1(str: string): boolean {
+  return sha1Regex.test(str)
+}
