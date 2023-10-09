b7608f118fbf344855fbf8e456fedcc62acc0e40
Tim Beyer <tim.beyer@gmail.com>
2023-10-09 15:22:47 +0200

    fix: enable passing arguments to `run-script` invocations

--- scripts/run-script.ts
+++ scripts/run-script.ts
@@ -9,7 +9,6 @@
 /* eslint-disable no-console */
 
 import execa from "execa"
-import minimist from "minimist"
 import { max, padEnd, padStart } from "lodash"
 import { DepGraph } from "dependency-graph"
 import split2 = require("split2")
@@ -19,16 +18,27 @@ import stripAnsi from "strip-ansi"
 import { join, resolve } from "path"
 import { createWriteStream, WriteStream } from "fs"
 import { getPackages } from "./script-utils"
+import yargs from "yargs/yargs"
 
 const colors = [chalk.blueBright, chalk.green, chalk.yellow, chalk.magenta, chalk.cyan]
 
 const lineChar = "┄"
 
 async function runInPackages(args: string[]) {
-  const argv = minimist(args, { boolean: ["bail", "parallel"], default: { bail: true } })
-  const script = argv._[0]
-  const rest = argv._.slice(1)
-  const { scope, ignore, bail, parallel } = argv
+  const parsed = await yargs()
+    .parserConfiguration({
+      "unknown-options-as-args": true,
+    })
+    .option("bail", { type: "boolean", default: true })
+    .option("parallel", { type: "boolean", default: false })
+    .option("scope", { type: "string" })
+    .option("ignore", { type: "string" })
+    .option("report", { type: "string" })
+    .parse(args)
+
+  const script = parsed._[0] as string
+  const rest = parsed._.slice(1) as string[]
+  const { scope, ignore, bail, parallel } = parsed
   const repoRoot = resolve(__dirname, "..")
 
   if (!script) {
@@ -37,8 +47,8 @@ async function runInPackages(args: string[]) {
 
   let reportStream: WriteStream | undefined = undefined
 
-  if (argv.report) {
-    const path = join(process.cwd(), argv.report)
+  if (parsed.report) {
+    const path = join(process.cwd(), parsed.report)
     console.log(chalk.cyan(`Writing script output to ${path}`))
     reportStream = createWriteStream(path)
   }
@@ -62,8 +72,8 @@ async function runInPackages(args: string[]) {
   process.env.FORCE_COLOR = chalk.supportsColor.toString() || "0"
 
   const maxNameLength = max(packagesWithColor.map((p) => p.shortName.length)) as number
-  let lastPackage: string = ""
-  let failed: string[] = []
+  let lastPackage = ""
+  const failed: string[] = []
 
   function write(line: string) {
     console.log(line)
@@ -81,7 +91,7 @@ async function runInPackages(args: string[]) {
       return
     }
 
-    const proc = execa("npm", ["run", script, ...rest, `--workspace=${pack.name}`], { cwd: repoRoot, reject: false })
+    const proc = execa("npm", ["run", script, `--workspace=${pack.name}`, ...(rest.length > 0 ? ["--", ...rest] : [])], { cwd: repoRoot, reject: false })
 
     void proc.on("error", (error) => {
       write(chalk.redBright(`\nCould not run ${script} script in package ${packageName}: ${error}`))
