8cf5e8ce0aea25aa4b19fe77a24dad1605fef5e4
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2023-10-20 15:51:19 +0200

    chore: fix lint

--- core/src/util/enterprise.ts
+++ core/src/util/enterprise.ts
@@ -1,8 +1,14 @@
-import { DEFAULT_GARDEN_CLOUD_DOMAIN } from "../constants";
-import { PluginContext } from "../plugin-context";
+/*
+ * Copyright (C) 2018-2023 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { DEFAULT_GARDEN_CLOUD_DOMAIN } from "../constants"
+import { PluginContext } from "../plugin-context"
 
 export function isGardenEnterprise(ctx: PluginContext): boolean {
   return !!(ctx.projectId && ctx.cloudApi && ctx.cloudApi?.domain !== DEFAULT_GARDEN_CLOUD_DOMAIN)
 }
-
-
