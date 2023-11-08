e4774eb512c93d6a30d70959525a90d5f5834f9a
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2023-11-08 14:16:14 +0100

    refactor: (2) use interface and classes for ingress setup

--- core/src/plugins/kubernetes/nginx/ingress-controller.ts
+++ core/src/plugins/kubernetes/nginx/ingress-controller.ts
@@ -8,13 +8,12 @@
 
 import type { Log } from "../../../logger/log-entry.js"
 import type { KubernetesPluginContext } from "../config.js"
-import { helmIngressControllerReady, helmNginxInstall, helmNginxUninstall } from "./nginx-helm.js"
-import { getGenericNginxHelmValues } from "./nginx-helm-generic.js"
-import { getK3sNginxHelmValues } from "./nginx-helm-k3s.js"
-import { microk8sNginxInstall, microk8sNginxStatus, microk8sNginxUninstall } from "./nginx-microk8s.js"
-import { minikubeNginxInstall, minikubeNginxStatus, minikubeNginxUninstall } from "./nginx-minikube.js"
-import { kindNginxInstall, kindNginxStatus, kindNginxUninstall } from "./nginx-kind.js"
-import { getEphemeralNginxHelmValues } from "./nginx-helm-ephemeral.js"
+import { GenericHelmGardenIngressController } from "./nginx-helm-generic.js"
+import { K3sHelmGardenIngressController } from "./nginx-helm-k3s.js"
+import { Microk8sGardenIngressController } from "./nginx-microk8s.js"
+import { MinikubeGardenIngressController } from "./nginx-minikube.js"
+import { KindGardenIngressController } from "./nginx-kind.js"
+import { EphemeralHelmGardenIngressController } from "./nginx-helm-ephemeral.js"
 
 export interface GardenIngressController {
   install: (ctx: KubernetesPluginContext, log: Log) => Promise<void>
@@ -22,71 +21,52 @@ export interface GardenIngressController {
   ready: (ctx: KubernetesPluginContext, log: Log) => Promise<boolean>
 }
 
-export async function ingressControllerReady(ctx: KubernetesPluginContext, log: Log): Promise<boolean> {
+export function getGardenIngressController(ctx: KubernetesPluginContext): GardenIngressController | undefined {
   const clusterType = ctx.provider.config.clusterType
   if (clusterType === undefined) {
+    return undefined
+  }
+
+  if (clusterType === "kind") {
+    return new KindGardenIngressController()
+  } else if (clusterType === "microk8s") {
+    return new Microk8sGardenIngressController()
+  } else if (clusterType === "minikube") {
+    return new MinikubeGardenIngressController()
+  } else if (clusterType === "k3s") {
+    return new K3sHelmGardenIngressController()
+  } else if (clusterType === "generic") {
+    return new GenericHelmGardenIngressController()
+  } else if (clusterType === "ephemeral") {
+    return new EphemeralHelmGardenIngressController()
+  } else {
+    return clusterType satisfies never
+  }
+}
+
+export async function ingressControllerReady(ctx: KubernetesPluginContext, log: Log): Promise<boolean> {
+  const gardenIngressController = getGardenIngressController(ctx)
+  if (!gardenIngressController) {
     return false
   }
 
-  if (clusterType === "kind") {
-    return (await kindNginxStatus(ctx, log)) === "ready"
-  } else if (clusterType === "microk8s") {
-    return (await microk8sNginxStatus(log)) === "ready"
-  } else if (clusterType === "minikube") {
-    return (await minikubeNginxStatus(ctx, log)) === "ready"
-  } else if (clusterType === "k3s") {
-    return await helmIngressControllerReady(ctx, log, getK3sNginxHelmValues)
-  } else if (clusterType === "generic") {
-    return await helmIngressControllerReady(ctx, log, getGenericNginxHelmValues)
-  } else if (clusterType === "ephemeral") {
-    return await helmIngressControllerReady(ctx, log, getEphemeralNginxHelmValues)
-  } else {
-    return clusterType satisfies never
-  }
+  return await gardenIngressController.ready(ctx, log)
 }
 
 export async function ingressControllerInstall(ctx: KubernetesPluginContext, log: Log) {
-  const clusterType = ctx.provider.config.clusterType
-  if (clusterType === undefined) {
+  const gardenIngressController = getGardenIngressController(ctx)
+  if (!gardenIngressController) {
     return
   }
 
-  if (clusterType === "kind") {
-    await kindNginxInstall(ctx, log)
-  } else if (clusterType === "microk8s") {
-    await microk8sNginxInstall(ctx, log)
-  } else if (clusterType === "minikube") {
-    await minikubeNginxInstall(ctx, log)
-  } else if (clusterType === "k3s") {
-    await helmNginxInstall(ctx, log, getK3sNginxHelmValues)
-  } else if (clusterType === "generic") {
-    await helmNginxInstall(ctx, log, getGenericNginxHelmValues)
-  } else if (clusterType === "ephemeral") {
-    await helmNginxInstall(ctx, log, getEphemeralNginxHelmValues)
-  } else {
-    return clusterType satisfies never
-  }
+  await gardenIngressController.install(ctx, log)
 }
 
 export async function ingressControllerUninstall(ctx: KubernetesPluginContext, log: Log) {
-  const clusterType = ctx.provider.config.clusterType
-  if (clusterType === undefined) {
+  const gardenIngressController = getGardenIngressController(ctx)
+  if (!gardenIngressController) {
     return
   }
 
-  if (clusterType === "kind") {
-    await kindNginxUninstall(ctx, log)
-  } else if (clusterType === "microk8s") {
-    await microk8sNginxUninstall(ctx, log)
-  } else if (clusterType === "minikube") {
-    await minikubeNginxUninstall(ctx, log)
-  } else if (clusterType === "k3s") {
-    await helmNginxUninstall(ctx, log, getK3sNginxHelmValues)
-  } else if (clusterType === "generic") {
-    await helmNginxUninstall(ctx, log, getGenericNginxHelmValues)
-  } else if (clusterType === "ephemeral") {
-    await helmNginxUninstall(ctx, log, getEphemeralNginxHelmValues)
-  } else {
-    return clusterType satisfies never
-  }
+  return await gardenIngressController.uninstall(ctx, log)
 }
