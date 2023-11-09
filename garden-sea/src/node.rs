fe811f26721ededd2ef29dc62279d0a7de3f6055
Steffen Neubauer <steffen@garden.io>
2023-11-09 16:46:44 +0100

    fix: add env var UV_USE_IO_URING=0 to avoid kernel bug
    Libuv 1.45.0 is affected by a kernel bug on certain kernels (Ubuntu 22)
    This leads to errors where Garden tool downloading errors with ETXTBSY
    Apparently file descriptor accounting is broken when using USE_IO_URING on older kernels
    
    See also https://github.com/libuv/libuv/pull/4141

--- garden-sea/src/node.rs
+++ garden-sea/src/node.rs
@@ -31,6 +31,13 @@ where
     command.env("GARDEN_SEA_EXTRACTED_ROOT", OsString::from(path));
     command.env("GARDEN_SEA_EXECUTABLE_PATH", executable_path);
 
+    // Libuv 1.45.0 is affected by a kernel bug on certain kernels (Ubuntu 22)
+    // This leads to errors where Garden tool downloading errors with ETXTBSY
+    // Apparently file descriptor accounting is broken when using USE_IO_URING on older kernels
+    // See also: https://github.com/libuv/libuv/pull/4141/files
+    // TODO: Remove this once libuv 1.47 landed in a future NodeJS version, and we upgraded to it.
+    command.env("UV_USE_IO_URING", env::var("GARDEN_SEA_UV_USE_IO_URING").unwrap_or("0".into()));
+
     // Allow users to override the heap size if needed.
     let max_old_space_size = env::var("GARDEN_MAX_OLD_SPACE_SIZE").unwrap_or("4096".into());
     let max_semi_space_size = env::var("GARDEN_MAX_SEMI_SPACE_SIZE").unwrap_or("64".into());
