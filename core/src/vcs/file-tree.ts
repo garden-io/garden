48e2603535c8744516480af19813bd0b5c49b9a4
Tim Beyer <tim.beyer@gmail.com>
2023-11-10 15:42:54 +0100

    test: reproduce issue for file tree cache on windows

--- core/src/vcs/file-tree.ts
+++ core/src/vcs/file-tree.ts
@@ -7,23 +7,28 @@
  */
 
 import type { VcsFile } from "./vcs.js"
-import * as path from "path"
+import type { PlatformPath } from "path"
+import * as posixPath from "path/posix"
+import * as winPath from "path/win32"
 
 type FileTreeNodeOptions = {
   ownPath: string
+  pathUtils: PlatformPath
 }
 
 export class FileTreeNode {
+  private pathUtils: PlatformPath
   private ownPath: string
   private files: VcsFile[] = []
   private childrenBySubpath: Map<string, FileTreeNode> = new Map()
 
-  constructor({ ownPath }: FileTreeNodeOptions) {
+  constructor({ ownPath, pathUtils }: FileTreeNodeOptions) {
     this.ownPath = ownPath
+    this.pathUtils = pathUtils
   }
 
   addFile(file: VcsFile): boolean {
-    if (!file.path.startsWith(`/${this.ownPath}`)) {
+    if (!file.path.startsWith(`${this.pathUtils.sep}${this.ownPath}`)) {
       return false
     }
 
@@ -31,7 +36,7 @@ export class FileTreeNode {
 
     const relativePath = file.path.slice(this.ownPath.length)
     // We use absolute paths so the first part of the split is always an empty string
-    const [_, subpathSegment, nextSegment] = relativePath.split(path.sep)
+    const [_, subpathSegment, nextSegment] = relativePath.split(this.pathUtils.sep)
 
     // We have reached the end of this path
     // and arrived at the file.
@@ -42,7 +47,10 @@ export class FileTreeNode {
 
     let child: FileTreeNode
     if (!this.childrenBySubpath.has(subpathSegment)) {
-      child = new FileTreeNode({ ownPath: path.join(this.ownPath, subpathSegment) })
+      child = new FileTreeNode({
+        ownPath: this.pathUtils.join(this.ownPath, subpathSegment),
+        pathUtils: this.pathUtils,
+      })
       this.childrenBySubpath.set(subpathSegment, child)
     } else {
       child = this.childrenBySubpath.get(subpathSegment)!
@@ -64,14 +72,16 @@ export class FileTreeNode {
 
 export class FileTree {
   private root: FileTreeNode
+  private pathUtils: PlatformPath
 
-  constructor(root: FileTreeNode) {
+  constructor(root: FileTreeNode, pathUtils: PlatformPath) {
     this.root = root
+    this.pathUtils = pathUtils
   }
 
   private getNodeAtPath(filesPath: string): FileTreeNode | undefined {
     // TODO: leading slash makes things annoying in many places
-    const segments = filesPath.slice(1).split(path.sep)
+    const segments = filesPath.slice(1).split(this.pathUtils.sep)
 
     let currentNode: FileTreeNode | undefined = this.root
     while (currentNode) {
@@ -104,17 +114,23 @@ export class FileTree {
     return node !== undefined
   }
 
-  static fromFiles(files: VcsFile[]): FileTree {
+  static fromFiles(files: VcsFile[], platform?: "win32" | "posix"): FileTree {
+    // In theory, node picks the right utils automatically depending on platform
+    // However, for testing, we need to be able to specify the platform explicitly here
+    // else we cannot test for example for Windows on a Unix machine.
+    const pathUtils = (platform ?? process.platform) === "win32" ? winPath : posixPath
+
     // We assume all files are rooted from the same directory
     // which is the root filesystem directory for most (all?) use cases here.
     const node = new FileTreeNode({
       ownPath: "",
+      pathUtils,
     })
 
     for (const file of files) {
       node.addFile(file)
     }
 
-    return new FileTree(node)
+    return new FileTree(node, pathUtils)
   }
 }
