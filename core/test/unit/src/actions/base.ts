3ed344b1e16c44a832218de2990a4e31e4118846
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2023-12-05 12:59:17 +0100

    test: test cases for all non-versionable config fields

--- core/test/unit/src/actions/base.ts
+++ core/test/unit/src/actions/base.ts
@@ -8,7 +8,8 @@
 
 import { expect } from "chai"
 import { DEFAULT_BUILD_TIMEOUT_SEC } from "../../../../src/constants.js"
-import type { ActionConfig } from "../../../../src/actions/types.js"
+import type { ActionConfig, BaseActionConfig } from "../../../../src/actions/types.js"
+import type { NonVersionedActionConfigKey } from "../../../../src/actions/base.js"
 import { getActionConfigVersion } from "../../../../src/actions/base.js"
 
 describe("getActionConfigVersion", () => {
@@ -26,16 +27,34 @@ describe("getActionConfigVersion", () => {
   }
 
   context("action config version does not change", () => {
-    it("on exclude field modification", () => {
-      const config1 = minimalActionConfig()
-      config1.exclude = ["dir1"]
-      const version1 = getActionConfigVersion(config1)
+    // Helper types for testing non-versioned config fields.
+    // The tests won't compile if the NonVersionedActionConfigKey type is modified.
+    type TestValuePair<T> = { leftValue: T; rightValue: T }
+    type TestMatrix = {
+      [key in NonVersionedActionConfigKey]: TestValuePair<BaseActionConfig[key]>
+    }
 
-      const config2 = minimalActionConfig()
-      config2.exclude = ["dir2"]
-      const version2 = getActionConfigVersion(config2)
+    const testMatrix: TestMatrix = {
+      description: { leftValue: "Description 1", rightValue: "Description 2" },
+      disabled: { leftValue: true, rightValue: false },
+      exclude: { leftValue: ["file1"], rightValue: ["file2"] },
+      include: { leftValue: ["file1"], rightValue: ["file2"] },
+      internal: { leftValue: { basePath: "./base1" }, rightValue: { basePath: "./base2" } },
+      source: { leftValue: { path: "path1" }, rightValue: { path: "path2" } },
+    }
 
-      expect(version1).to.eql(version2)
-    })
+    for (const [field, valuePair] of Object.entries(testMatrix)) {
+      it(`on ${field} field modification`, () => {
+        const config1 = minimalActionConfig()
+        config1[field] = valuePair.leftValue
+        const version1 = getActionConfigVersion(config1)
+
+        const config2 = minimalActionConfig()
+        config2[field] = valuePair.rightValue
+        const version2 = getActionConfigVersion(config2)
+
+        expect(version1).to.eql(version2)
+      })
+    }
   })
 })
