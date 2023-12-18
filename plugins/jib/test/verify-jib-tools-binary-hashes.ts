aa393bdafcfbe7fa5b17eae08e025fb2baacfe72
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2023-12-18 15:16:01 +0100

    test(jib): verify binary hashes

---
+++ plugins/jib/test/verify-jib-tools-binary-hashes.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2018-2023 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { downloadBinariesAndVerifyHashes } from "@garden-io/core/build/src/util/testing.js"
+import { gradleSpec } from "../src/gradle.js"
+import { mavenSpec } from "../src/maven.js"
+import { mavendSpec } from "../src/mavend.js"
+import { openJdkSpecs } from "../src/openjdk.js"
+
+describe("Jib binaries", () => {
+  describe("Gradle binaries", () => {
+    downloadBinariesAndVerifyHashes([gradleSpec])
+  })
+
+  describe("Maven binaries", () => {
+    downloadBinariesAndVerifyHashes([mavenSpec])
+  })
+
+  describe("Mavend binaries", () => {
+    downloadBinariesAndVerifyHashes([mavendSpec])
+  })
+
+  describe("JDK binaries", () => {
+    downloadBinariesAndVerifyHashes(openJdkSpecs)
+  })
+})
