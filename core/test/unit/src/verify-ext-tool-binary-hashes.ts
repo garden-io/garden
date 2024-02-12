e982f3131b2b42f8948202d80f5d3a5261a1fbb0
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2024-02-12 13:18:07 +0100

    test: verify Docker binary hashes

--- core/test/unit/src/verify-ext-tool-binary-hashes.ts
+++ core/test/unit/src/verify-ext-tool-binary-hashes.ts
@@ -11,6 +11,11 @@ import { kubectlSpec } from "../../../src/plugins/kubernetes/kubectl.js"
 import { kustomizeSpec } from "../../../src/plugins/kubernetes/kubernetes-type/kustomize.js"
 import { helm3Spec } from "../../../src/plugins/kubernetes/helm/helm-cli.js"
 import { downloadBinariesAndVerifyHashes } from "../../../src/util/testing.js"
+import { dockerSpec } from "../../../src/plugins/container/container.js"
+
+describe("Docker binaries", () => {
+  downloadBinariesAndVerifyHashes([dockerSpec])
+})
 
 describe("Mutagen binaries", () => {
   downloadBinariesAndVerifyHashes([mutagenCliSpec])
