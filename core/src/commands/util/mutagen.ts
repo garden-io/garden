garden-io/garden

commit 2698410c8a852ddc33cae072d5498e5e57acefb8

fix(mutagen): use shorter directories for mutagen syncs (#4867)
Use directory at shorter path for mutagen syncs if the project
path is longer than MUTAGEN_DATA_DIRECTORY_LENGTH_LIMIT. 
The shorter directory path is computed using hash of the path of 
.garden dir in project and appending to $HOME/.garden/mutagen.

core/src/mutagen.ts
@@ -7,27 +7,28 @@
  */
 
 import AsyncLock from "async-lock"
-import chalk from "chalk"
-import { join } from "path"
-import { mkdirp, pathExists } from "fs-extra"
-import respawn from "respawn"
-import { Log } from "./logger/log-entry"
-import { PluginToolSpec } from "./plugin/tools"
-import { PluginTool } from "./util/ext-tools"
-import { registerCleanupFunction, sleep } from "./util/util"
-import { GardenBaseError } from "./exceptions"
-import pRetry from "p-retry"
-import { syncGuideLink } from "./plugins/kubernetes/sync"
-import dedent from "dedent"
-import { PluginContext } from "./plugin-context"
 import Bluebird from "bluebird"
-import { MUTAGEN_DIR_NAME } from "./constants"
-import { ExecaReturnValue } from "execa"
+import chalk from "chalk"
+import dedent from "dedent"
 import EventEmitter from "events"
+import { ExecaReturnValue } from "execa"
+import { mkdirp, pathExists } from "fs-extra"
+import hasha from "hasha"
+import pRetry from "p-retry"
+import { join } from "path"
+import respawn from "respawn"
 import split2 from "split2"
-import { TypedEventEmitter } from "./util/events"
+import { GARDEN_GLOBAL_PATH, MUTAGEN_DIR_NAME } from "./constants"
+import { GardenBaseError } from "./exceptions"
 import pMemoize from "./lib/p-memoize"
+import { Log } from "./logger/log-entry"
+import { PluginContext } from "./plugin-context"
+import { PluginToolSpec } from "./plugin/tools"
+import { syncGuideLink } from "./plugins/kubernetes/sync"
+import { TypedEventEmitter } from "./util/events"
+import { PluginTool } from "./util/ext-tools"
 import { deline } from "./util/string"
+import { registerCleanupFunction, sleep } from "./util/util"
 import { emitNonRepeatableWarning } from "./warnings"
 
 const maxRestarts = 10
@@ -134,7 +135,6 @@ export class MutagenError extends GardenBaseError {
 interface MutagenDaemonParams {
   ctx: PluginContext
   log: Log
-  dataDir?: string
 }
 
 interface MutagenMonitorParams {
@@ -328,10 +328,10 @@ export class Mutagen {
   private configLock: AsyncLock
   private monitoring: boolean
 
-  constructor({ ctx, log, dataDir }: MutagenDaemonParams) {
+  constructor({ ctx, log }: MutagenDaemonParams) {
     this.log = log
     this.configLock = new AsyncLock()
-    this.dataDir = dataDir || join(ctx.gardenDirPath, MUTAGEN_DIR_NAME)
+    this.dataDir = getMutagenDataDir(ctx.gardenDirPath, log)
     this.activeSyncs = {}
     this.monitoring = false
 
@@ -629,7 +629,7 @@ export class Mutagen {
           cwd: this.dataDir,
           args,
           log: this.log,
-          env: getMutagenEnv({ dataDir: this.dataDir, log: this.log }),
+          env: getMutagenEnv(this.dataDir),
         })
       } catch (err) {
         const unableToConnect = err.message.match(/unable to connect to daemon/)
@@ -778,13 +778,29 @@ export interface SyncSession {
  */
 const MUTAGEN_DATA_DIRECTORY_LENGTH_LIMIT = 70
 
-export function getMutagenEnv({ dataDir, log }: { dataDir: string; log: Log }) {
-  if (dataDir.length > MUTAGEN_DATA_DIRECTORY_LENGTH_LIMIT) {
-    emitNonRepeatableWarning(
-      log,
-      `Your Garden project path looks too long, that might cause errors while starting the syncs. Consider using a shorter path (no longer than ${MUTAGEN_DATA_DIRECTORY_LENGTH_LIMIT} characters).`
+/**
+ * Returns mutagen data directory path based on the project dir.
+ * If the project path longer than `MUTAGEN_DATA_DIRECTORY_LENGTH_LIMIT`, it computes
+ * hash of project dir path, uses first 9 characters of hash as directory name
+ * and creates a directory in $HOME/.garden/mutagen.
+ *
+ * However, if the path is not longer than `MUTAGEN_DATA_DIRECTORY_LENGTH_LIMIT`, then
+ * it uses the ./project-root/.garden/mutagen directory.
+ */
+export function getMutagenDataDir(path: string, log: Log) {
+  if (path.length > MUTAGEN_DATA_DIRECTORY_LENGTH_LIMIT) {
+    const hash = hasha(path, { algorithm: "sha256" }).slice(0, 9)
+    const shortPath = join(GARDEN_GLOBAL_PATH, MUTAGEN_DIR_NAME, hash)
+    log.verbose(
+      `Your Garden project path looks too long, that might cause errors while starting the syncs. Garden will create a new directory to manage syncs at path: ${shortPath}.`
     )
+    return shortPath
   }
+  // if path is not too long, then use relative directory to the project
+  return join(path, MUTAGEN_DIR_NAME)
+}
+
+export function getMutagenEnv(dataDir: string) {
   return {
     MUTAGEN_DATA_DIRECTORY: dataDir,
   }

core/src/plugins/kubernetes/commands/sync.ts
@@ -7,9 +7,7 @@
  */
 
 import chalk from "chalk"
-import { getMutagenEnv, mutagenCliSpec, parseSyncListResult } from "../../../mutagen"
-import { MUTAGEN_DIR_NAME } from "../../../constants"
-import { join } from "path"
+import { getMutagenDataDir, getMutagenEnv, mutagenCliSpec, parseSyncListResult } from "../../../mutagen"
 import { pathExists } from "fs-extra"
 import { dedent } from "../../../util/string"
 import { Log } from "../../../logger/log-entry"
@@ -24,7 +22,7 @@ export const syncStatus: PluginCommand = {
   title: "Get the current sync status",
 
   handler: async ({ ctx, log }) => {
-    const dataDir = getMutagenDataDir(ctx.gardenDirPath)
+    const dataDir = getMutagenDataDir(ctx.gardenDirPath, log)
     const mutagen = new PluginTool(mutagenCliSpec)
 
     if (!(await pathExists(dataDir))) {
@@ -58,7 +56,7 @@ export const syncPause: PluginCommand = {
   title: "Pause sync",
 
   handler: async ({ ctx, log }) => {
-    const dataDir = getMutagenDataDir(ctx.gardenDirPath)
+    const dataDir = getMutagenDataDir(ctx.gardenDirPath, log)
     const mutagen = new PluginTool(mutagenCliSpec)
 
     if (!(await pathExists(dataDir))) {
@@ -87,7 +85,7 @@ export const syncPause: PluginCommand = {
         await mutagen.exec({
           cwd: dataDir,
           log,
-          env: getMutagenEnv({ dataDir, log }),
+          env: getMutagenEnv(dataDir),
           args: ["sync", "pause", sessionName],
         })
       }
@@ -104,7 +102,7 @@ export const syncResume: PluginCommand = {
   title: "Resume sync",
 
   handler: async ({ ctx, log }) => {
-    const dataDir = getMutagenDataDir(ctx.gardenDirPath)
+    const dataDir = getMutagenDataDir(ctx.gardenDirPath, log)
     const mutagen = new PluginTool(mutagenCliSpec)
 
     if (!(await pathExists(dataDir))) {
@@ -133,7 +131,7 @@ export const syncResume: PluginCommand = {
         await mutagen.exec({
           cwd: dataDir,
           log,
-          env: getMutagenEnv({ dataDir, log }),
+          env: getMutagenEnv(dataDir),
           args: ["sync", "resume", sessionName],
         })
       }
@@ -148,12 +146,8 @@ async function getMutagenSyncSessions({ mutagen, dataDir, log }: { mutagen: Plug
   const res = await mutagen.exec({
     cwd: dataDir,
     log,
-    env: getMutagenEnv({ dataDir, log }),
+    env: getMutagenEnv(dataDir),
     args: ["sync", "list", "--template={{ json . }}"],
   })
   return parseSyncListResult(res)
-}
-
-function getMutagenDataDir(gardenDirPath: string) {
-  return join(gardenDirPath, MUTAGEN_DIR_NAME)
 }

core/test/integ/src/plugins/kubernetes/commands/sync.ts
@@ -8,17 +8,15 @@
 
 import { expect } from "chai"
 import { Garden } from "../../../../../../src/garden"
-import { getContainerTestGarden } from "../container/container"
-import { PluginContext } from "../../../../../../src/plugin-context"
-import { KubernetesProvider } from "../../../../../../src/plugins/kubernetes/config"
-import { getMutagenMonitor } from "../../../../../../src/mutagen"
-import { syncPause, syncResume, syncStatus } from "../../../../../../src/plugins/kubernetes/commands/sync"
-import { DeployTask } from "../../../../../../src/tasks/deploy"
-import { Log } from "../../../../../../src/logger/log-entry"
 import { ConfigGraph } from "../../../../../../src/graph/config-graph"
-import { join } from "path"
-import { MUTAGEN_DIR_NAME } from "../../../../../../src/constants"
+import { Log } from "../../../../../../src/logger/log-entry"
+import { getMutagenDataDir, getMutagenMonitor } from "../../../../../../src/mutagen"
+import { PluginContext } from "../../../../../../src/plugin-context"
+import { syncPause, syncResume, syncStatus } from "../../../../../../src/plugins/kubernetes/commands/sync"
+import { KubernetesProvider } from "../../../../../../src/plugins/kubernetes/config"
+import { DeployTask } from "../../../../../../src/tasks/deploy"
 import { cleanProject } from "../../../../../helpers"
+import { getContainerTestGarden } from "../container/container"
 
 describe("sync plugin commands", () => {
   let garden: Garden
@@ -34,7 +32,7 @@ describe("sync plugin commands", () => {
   after(async () => {
     if (garden) {
       garden.close()
-      const dataDir = join(garden.gardenDirPath, MUTAGEN_DIR_NAME)
+      const dataDir = getMutagenDataDir(garden.gardenDirPath, garden.log)
       await getMutagenMonitor({ log, dataDir }).stop()
       await cleanProject(garden.gardenDirPath)
     }

commit 98a650db728f67e1f40bd4f24e1e0dc1e77c62dc

fix(mutagen): call mutagen commands from the right cwd (#5734)
**What this PR does / why we need it**:

This PR ensure that the commands passed to `garden util mutagen ...` are
always executed from the right `cwd`.

**Which issue(s) this PR fixes**:

This fixes some regression (introduced in #4867) the Mutagen daemon
management:

* A daemon started by Garden (i.e. over `deploy --sync` or `sync start`
commands) cannot be stopped by `garden util mutagen daemon stop`.
* An extra Mutagen daemon is created by `garden util mutagen daemon
start` despite there is another one started by Garden's `deploy` or
`sync` commands.

Patches #4867.

core/src/commands/util/mutagen.ts
@@ -11,11 +11,10 @@ import { Command } from "../base.js"
 import { RuntimeError } from "../../exceptions.js"
 import dedent from "dedent"
 import { findProjectConfig } from "../../config/base.js"
-import { DEFAULT_GARDEN_DIR_NAME, MUTAGEN_DIR_NAME } from "../../constants.js"
-import { join } from "path"
 import { exec } from "../../util/util.js"
-import { mutagenCli } from "../../mutagen.js"
+import { getMutagenDataDir, mutagenCli } from "../../mutagen.js"
 import fsExtra from "fs-extra"
+
 const { mkdirp } = fsExtra
 
 export class MutagenCommand extends Command<{}, {}> {
@@ -40,9 +39,9 @@ export class MutagenCommand extends Command<{}, {}> {
   override printHeader() {}
 
   async action({ garden, log, args }: CommandParams<{}, {}>) {
-    const projectRoot = await findProjectConfig({ log, path: garden.projectRoot })
+    const projectConfig = await findProjectConfig({ log, path: garden.projectRoot })
 
-    if (!projectRoot) {
+    if (!projectConfig) {
       throw new RuntimeError({
         message: dedent`
           Could not find project config in the current directory, or anywhere above.
@@ -51,7 +50,7 @@ export class MutagenCommand extends Command<{}, {}> {
       })
     }
 
-    const mutagenDir = join(projectRoot.path, DEFAULT_GARDEN_DIR_NAME, MUTAGEN_DIR_NAME)
+    const mutagenDir = getMutagenDataDir({ ctx: garden, log })
     const mutagenPath = await mutagenCli.ensurePath(log)
 
     await mkdirp(mutagenDir)

core/src/mutagen.ts
@@ -20,7 +20,7 @@ import { GARDEN_GLOBAL_PATH, MUTAGEN_DIR_NAME } from "./constants.js"
 import { ChildProcessError, GardenError } from "./exceptions.js"
 import pMemoize from "./lib/p-memoize.js"
 import type { Log } from "./logger/log-entry.js"
-import type { PluginContext } from "./plugin-context.js"
+import type { WrappedFromGarden } from "./plugin-context.js"
 import type { PluginToolSpec } from "./plugin/tools.js"
 import { syncGuideLink } from "./plugins/kubernetes/sync.js"
 import { TypedEventEmitter } from "./util/events.js"
@@ -133,7 +133,7 @@ export class MutagenError extends GardenError {
 }
 
 interface MutagenDaemonParams {
-  ctx: PluginContext
+  ctx: WrappedFromGarden
   log: Log
 }
 
@@ -331,10 +331,10 @@ export class Mutagen {
   private configLock: AsyncLock
   private monitoring: boolean
 
-  constructor({ ctx, log }: MutagenDaemonParams) {
-    this.log = log
+  constructor(params: MutagenDaemonParams) {
+    this.log = params.log
     this.configLock = new AsyncLock()
-    this.dataDir = getMutagenDataDir(ctx.gardenDirPath, log)
+    this.dataDir = getMutagenDataDir(params)
     this.activeSyncs = {}
     this.monitoring = false
 
@@ -796,12 +796,12 @@ export interface SyncSession {
  *   <li>https://unix.stackexchange.com/questions/367008/why-is-socket-path-length-limited-to-a-hundred-chars/367012#367012</li>
  * </ul>
  */
-export function getMutagenDataDir(path: string, log: Log) {
-  const hash = hashSync(path, { algorithm: "sha256" }).slice(0, 9)
+export function getMutagenDataDir({ ctx, log }: MutagenDaemonParams) {
+  const rawSyncPath = ctx.gardenDirPath
+  const hash = hashSync(rawSyncPath, { algorithm: "sha256" }).slice(0, 9)
   const shortPath = join(GARDEN_GLOBAL_PATH, MUTAGEN_DIR_NAME, hash)
-  log.verbose(
-    `Your Garden project path looks too long, that might cause errors while starting the syncs. Garden will create a new directory to manage syncs at path: ${shortPath}.`
-  )
+  log.debug(`The syncs will be managed from ${shortPath}.`)
+
   return shortPath
 }
 

core/src/plugin-context.ts
@@ -25,7 +25,7 @@ import { Memoize } from "typescript-memoize"
 import type { ParameterObject, ParameterValues } from "./cli/params.js"
 import type { NamespaceStatus } from "./types/namespace.js"
 
-type WrappedFromGarden = Pick<
+export type WrappedFromGarden = Pick<
   Garden,
   | "projectName"
   | "projectRoot"

core/src/plugins/kubernetes/commands/sync.ts
@@ -24,7 +24,7 @@ export const syncStatus: PluginCommand = {
   title: "Get the current sync status",
 
   handler: async ({ ctx, log }) => {
-    const dataDir = getMutagenDataDir(ctx.gardenDirPath, log)
+    const dataDir = getMutagenDataDir({ ctx, log })
     const mutagen = new PluginTool(mutagenCliSpec)
 
     if (!(await pathExists(dataDir))) {
@@ -58,7 +58,7 @@ export const syncPause: PluginCommand = {
   title: "Pause sync",
 
   handler: async ({ ctx, log }) => {
-    const dataDir = getMutagenDataDir(ctx.gardenDirPath, log)
+    const dataDir = getMutagenDataDir({ ctx, log })
     const mutagen = new PluginTool(mutagenCliSpec)
 
     if (!(await pathExists(dataDir))) {
@@ -104,7 +104,7 @@ export const syncResume: PluginCommand = {
   title: "Resume sync",
 
   handler: async ({ ctx, log }) => {
-    const dataDir = getMutagenDataDir(ctx.gardenDirPath, log)
+    const dataDir = getMutagenDataDir({ ctx, log })
     const mutagen = new PluginTool(mutagenCliSpec)
 
     if (!(await pathExists(dataDir))) {

core/test/integ/src/plugins/kubernetes/commands/sync.ts
@@ -33,7 +33,7 @@ describe("sync plugin commands", () => {
   after(async () => {
     if (cleanup) {
       cleanup()
-      const dataDir = getMutagenDataDir(garden.gardenDirPath, garden.log)
+      const dataDir = getMutagenDataDir({ ctx: garden, log: garden.log })
       await getMutagenMonitor({ log, dataDir }).stop()
       await cleanProject(garden.gardenDirPath)
     }