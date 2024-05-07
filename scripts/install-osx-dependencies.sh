f4aa78932931b6bdfc04719f400d1478c3fd4cfa
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2024-05-07 15:06:38 +0200

    chore(dev): use NodeJS `21.7.3` for dev setup

--- scripts/install-osx-dependencies.sh
+++ scripts/install-osx-dependencies.sh
@@ -21,8 +21,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 # install node
-nvm install v18
-nvm alias default v18
+nvm install v21
+nvm alias default v21
 nvm use default
 
 # install/update global packages
