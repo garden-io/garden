fcfb65ee7990a35462f2a282e28521b7028583c5
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2024-05-17 15:37:33 +0200

    chore: amend var names, logs and comments for better clarity

--- core/src/plugins/kubernetes/container/publish.ts
+++ core/src/plugins/kubernetes/container/publish.ts
@@ -21,23 +21,25 @@ export const k8sPublishContainerBuild: BuildActionHandler<"publish", ContainerBu
   const localImageId = action.getOutput("localImageId")
   // NOTE: this may contain a custom deploymentRegistry, from the kubernetes provider config
   // We cannot combine this publish method with the container plugin's publish method, because it won't have the context
-  const remoteImageId = action.getOutput("deploymentImageId")
+  const deploymentRegistryImageId = action.getOutput("deploymentImageId")
 
   if (provider.config.buildMode !== "local-docker") {
-    // First pull from the remote registry, then resume standard publish flow.
+    // First pull from the deployment registry, then resume standard publish flow.
     // This does mean we require a local docker as a go-between, but the upside is that we can rely on the user's
     // standard authentication setup, instead of having to re-implement or account for all the different ways the
     // user might be authenticating with their registries.
     // We also generally prefer this because the remote cluster very likely doesn't (and shouldn't) have
     // privileges to push to production registries.
-    log.info(`Pulling from remote registry...`)
-    await pullBuild({ ctx: k8sCtx, action, log, localId: localImageId, remoteId: remoteImageId })
+    log.info(`Pulling from deployment registry...`)
+    await pullBuild({ ctx: k8sCtx, action, log, localId: localImageId, remoteId: deploymentRegistryImageId })
   }
 
-  // optionally use the tag instead of the garden version, this requires that we tag the image locally
-  // before publishing to the remote registry
+  // Optionally use the tag instead of the garden version.
+  // This requires that we tag the image locally before publishing to the remote registry.
+  const remotePublishId = tagOverride
+    ? `${action.getOutput("deploymentImageName")}:${tagOverride}`
+    : deploymentRegistryImageId
 
-  const remotePublishId = tagOverride ? `${action.getOutput("deploymentImageName")}:${tagOverride}` : remoteImageId
   const taggedImages = [localImageId, remotePublishId]
   log.info({ msg: `Tagging images ${naturalList(taggedImages)}` })
   await containerHelpers.dockerCli({ cwd: action.getBuildPath(), args: ["tag", ...taggedImages], log, ctx })
