4f8a2d6d20a5a635afc59a4e845b2a62418d593e
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2024-07-10 14:05:16 +0200

    feat(kubernetes): plugin command to remove `garden-util` resources (#6278)
    * chore: remove unused constant
    
    * chore: use named constant as a deploy action name
    
    * feat(kubernetes): plugin command to remove `garden-util` resources
    
    * chore: print namespace in the log message

---
+++ core/src/plugins/kubernetes/commands/cleanup-garden-util.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2018-2024 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import type { KubernetesPluginContext } from "../config.js"
+import type { PluginCommand } from "../../../plugin/command.js"
+import { utilDeploymentName } from "../container/build/common.js"
+import { styles } from "../../../logger/styles.js"
+import { deleteResources } from "../kubectl.js"
+import type { KubernetesResource } from "../types.js"
+
+export const cleanupUtilDeployment: PluginCommand = {
+  name: "cleanup-util-deployment",
+  description: `Remove ${utilDeploymentName} utility deployment from the current namespace`,
+  title: `Cleanup ${utilDeploymentName} deployment`,
+  resolveGraph: true,
+
+  handler: async ({ ctx, log }) => {
+    const result = {}
+    const k8sCtx = ctx as KubernetesPluginContext
+    const provider = k8sCtx.provider
+
+    const namespace = provider.outputs["app-namespace"]
+    log.info({ msg: styles.highlight(`\nRemoving ${utilDeploymentName} deployment from namespace ${namespace}`) })
+
+    const targetKinds = ["Service", "Deployment"]
+    const resources: KubernetesResource[] = targetKinds.map((kind) => {
+      return { apiVersion: "v1", kind, metadata: { name: utilDeploymentName } }
+    })
+
+    for (const resource of resources) {
+      log.info(`Deleting ${resource.kind}/${resource.metadata.name}`)
+      await deleteResources({ ctx, log, provider, namespace, resources })
+    }
+
+    log.success("\nDone!")
+
+    return { result }
+  },
+}
