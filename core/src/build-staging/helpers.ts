garden-io/garden

commit 26644fcf53a2aa10cd3b7327b4022d104343c7bb

fix: allow relative symlinks to directories when using build staging (#6430)
* fix: allow relative symlinks to directories when using build staging

Allows using relative symlinks to directories and files when using build staging.

The goal is to reproduce symlinks in the target directory as-is, without having to resolve them.
The user would be responsible for making sure that both the relative symlink as well as the target
directory are included in the action.

If the symlink points to a file or directory outside the actions's source path, this throws an
error.

To avoid this becoming a breaking change, we keep the old behaviour if the symlink points to a
file. In that case, we resolve the symlink and copy the target file. If the target file is outside
 the action's source or build directory, log a warning.

In a future 0.14 release of Garden, we should remove that branch, only reproduce symlinks (without
resolving them recursively) and
throw an error if the link is out of bounds.

Fixes #2382

Co-authored-by: Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>

* test: add additional test for the error

Co-authored-by: vova <vladimir@garden.io>

* refactor: rename param object property

To keep more precise naming.

* refactor: introduce param object for constructor

* refactor: introduce param object for factory method

* chore: declare non re-assignable props as readonly

* refactor: named type for standard stats callback

* refactor: named type for resolve symlink callback stats callback

* refactor: param object for resolve symlink callback

* fix: restore accidentally deleted condition check

Co-authored-by: Steffen Neubauer <steffen@garden.io>

---------

Co-authored-by: Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
Co-authored-by: vova <vladimir@garden.io>
Co-authored-by: Vladimir Vagaytsev <10628074+vvagaytsev@users.noreply.github.com>

core/src/build-staging/helpers.ts
@@ -6,22 +6,28 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { readlink, copyFile, constants, utimes } from "fs"
+import { readlink, copyFile, constants, utimes, symlink } from "fs"
 import readdir from "@jsdevtools/readdir-enhanced"
-import { splitLast } from "../util/string.js"
+import { dedent, splitLast } from "../util/string.js"
 import { Minimatch } from "minimatch"
-import { isAbsolute, parse, basename, resolve } from "path"
+import { isAbsolute, parse, basename, resolve, join, dirname } from "path"
 import fsExtra from "fs-extra"
-const { ensureDir, Stats, lstat, remove } = fsExtra
-import { FilesystemError, InternalError, isErrnoException } from "../exceptions.js"
+import { ConfigurationError, FilesystemError, InternalError, isErrnoException } from "../exceptions.js"
 import type { AsyncResultCallback } from "async"
 import async from "async"
 import { round } from "lodash-es"
 import { promisify } from "util"
+import { styles } from "../logger/styles.js"
+import { emitNonRepeatableWarning } from "../warnings.js"
+import { type Log } from "../logger/log-entry.js"
+
+const { ensureDir, Stats, lstat, remove } = fsExtra
 
 export type MappedPaths = [string, string][]
 
 export interface CloneFileParams {
+  log: Log
+  sourceRoot: string
   from: string
   to: string
   allowDelete: boolean
@@ -37,7 +43,10 @@ type SyncCallback = AsyncResultCallback<SyncResult, Error>
 /**
  * Synchronizes (clones) a single file in one direction.
  */
-export function cloneFile({ from, to, allowDelete, statsHelper }: CloneFileParams, done: SyncCallback) {
+export function cloneFile(
+  { log, sourceRoot, from, to, allowDelete, statsHelper }: CloneFileParams,
+  done: SyncCallback
+) {
   // Stat the files
   async.parallel<ExtendedStats | null>(
     {
@@ -57,33 +66,60 @@ export function cloneFile({ from, to, allowDelete, statsHelper }: CloneFileParam
         return done(null, { skipped: true })
       }
 
-      // Follow symlink on source, if applicable
       if (sourceStats.isSymbolicLink()) {
-        if (sourceStats.target) {
-          sourceStats = sourceStats.target
-        } else {
-          // Symlink couldn't be resolved, so we ignore it
+        if (!sourceStats.targetPath) {
+          // This symlink failed validation (e.g. it is absolute, when absolute symlinks are not allowed)
           return done(null, { skipped: true })
+        }
+        const resolved = resolve(dirname(sourceStats.path), sourceStats.targetPath)
+        const outOfBounds = !resolved.startsWith(join(sourceRoot, "/"))
+        if (outOfBounds) {
+          const outOfBoundsMessage = dedent`
+            The action's source directory (when using ${styles.highlight("staged builds")}), or build directory (when using ${styles.highlight("copyFrom")}), must be self-contained.
+
+            Encountered a symlink at ${styles.highlight(sourceStats.path)} whose target ${styles.highlight(sourceStats.targetPath)} is out of bounds (not inside ${sourceRoot}).
+
+            In case this is not acceptable, you can disable build staging by setting ${styles.highlight("buildAtSource: true")} in the action configuration to disable build staging for this action.`
+          if (sourceStats.target?.isFile()) {
+            // For compatibility with older versions of garden that would allow symlink targets outside the root, if the target file existed, we only emit a warning here.
+            // TODO(0.14): Throw an error here
+            emitNonRepeatableWarning(
+              log,
+              outOfBoundsMessage + `\n\nWARNING: This will become an error in an upcoming release of Garden.`
+            )
+            // For compatibility with older versions of Garden, copy the target file instead of reproducing the symlink
+            // TODO(0.14): Only reproduce the symlink. The target file will be copied in another call to `cloneFile`.
+            sourceStats = sourceStats.target
+          } else {
+            // Note: If a symlink pointed to a directory, we threw another error "source is neither a symbolic link, nor a file" in previous versions of garden,
+            // so this is not a breaking change.
+            return done(
+              new ConfigurationError({
+                message: outOfBoundsMessage,
+              })
+            )
+          }
         }
       }
 
-      if (!sourceStats.isFile()) {
+      if (!sourceStats.isFile() && !sourceStats.isSymbolicLink()) {
         return done(
-          new FilesystemError({
+          // Using internal error here because if this happens, it's a logical error here in this code
+          new InternalError({
             message: `Error while copying from '${from}' to '${to}': Source is neither a symbolic link, nor a file.`,
           })
         )
       }
 
       if (targetStats) {
-        // If target is a directory and deletes are allowed, delete the directory before copying, otherwise throw
-        if (targetStats.isDirectory()) {
+        // If target is a directory or source is a symbolic link and deletes are allowed, delete the target before copying, otherwise throw
+        if (targetStats.isDirectory() || sourceStats.isSymbolicLink()) {
           if (allowDelete) {
             return remove(to, (removeErr) => {
               if (removeErr) {
                 return done(removeErr)
               } else {
-                return doClone({ from, to, sourceStats: sourceStats!, done, statsHelper })
+                return doClone({ from, to, sourceStats: sourceStats!, targetStats, done, statsHelper })
               }
             })
           } else {
@@ -101,7 +137,7 @@ export function cloneFile({ from, to, allowDelete, statsHelper }: CloneFileParam
         }
       }
 
-      return doClone({ from, to, sourceStats, done, statsHelper })
+      return doClone({ from, to, sourceStats, targetStats, done, statsHelper })
     }
   )
 }
@@ -112,14 +148,15 @@ export const cloneFileAsync = promisify(cloneFile) as (params: CloneFileParams) 
 interface CopyParams {
   from: string
   to: string
-  sourceStats: fsExtra.Stats
+  sourceStats: ExtendedStats
+  targetStats: ExtendedStats | null
   done: SyncCallback
   statsHelper: FileStatsHelper
   resolvedSymlinkPaths?: string[]
 }
 
 function doClone(params: CopyParams) {
-  const { from, to, done, sourceStats } = params
+  const { from, to, done, sourceStats, targetStats } = params
   const dir = parse(to).dir
 
   // TODO: take care of this ahead of time to avoid the extra i/o
@@ -128,11 +165,7 @@ function doClone(params: CopyParams) {
       return done(err)
     }
 
-    // COPYFILE_FICLONE instructs the function to use a copy-on-write reflink on platforms/filesystems where available
-    copyFile(from, to, constants.COPYFILE_FICLONE, (copyErr) => {
-      if (copyErr) {
-        return done(copyErr)
-      }
+    const setUtimes = () => {
       // Set the mtime on the cloned file to the same as the source file
       utimes(to, new Date(), sourceStats.mtimeMs / 1000, (utimesErr) => {
         if (utimesErr && (!isErrnoException(utimesErr) || utimesErr.code !== "ENOENT")) {
@@ -140,7 +173,46 @@ function doClone(params: CopyParams) {
         }
         done(null, { skipped: false })
       })
-    })
+    }
+
+    if (sourceStats.isSymbolicLink()) {
+      // reproduce the symbolic link. Validation happens before.
+      if (sourceStats.targetPath) {
+        symlink(
+          sourceStats.targetPath,
+          to,
+          // relevant on windows
+          // nodejs will auto-detect this on windows, but if the symlink is copied before the target then the auto-detection will get it wrong.
+          targetStats?.isDirectory() ? "dir" : "file",
+          (symlinkErr) => {
+            if (symlinkErr) {
+              return done(symlinkErr)
+            }
+
+            setUtimes()
+          }
+        )
+      } else {
+        return done(
+          new InternalError({
+            message: "Source is a symbolic link, but targetPath was null or undefined.",
+          })
+        )
+      }
+    } else if (sourceStats.isFile()) {
+      // COPYFILE_FICLONE instructs the function to use a copy-on-write reflink on platforms/filesystems where available
+      copyFile(from, to, constants.COPYFILE_FICLONE, (copyErr) => {
+        if (copyErr) {
+          return done(copyErr)
+        }
+
+        setUtimes()
+      })
+    } else {
+      throw new InternalError({
+        message: "Expected doClone source to be a file or a symbolic link.",
+      })
+    }
   })
 }
 
@@ -217,18 +289,34 @@ export async function scanDirectoryForClone(root: string, pattern?: string): Pro
   return mappedPaths
 }
 
+type ExtendedStatsCtorParams = {
+  path: string
+  target?: ExtendedStats | null
+  targetPath?: string | null
+}
+
 export class ExtendedStats extends Stats {
   path: string
   target?: ExtendedStats | null
+  // original relative or absolute path the symlink points to. This can be defined when target is null when the target does not exist, for instance.
+  targetPath?: string | null
 
-  constructor(path: string, target?: ExtendedStats | null) {
+  constructor({ path, target, targetPath }: ExtendedStatsCtorParams) {
     super()
     this.path = path
     this.target = target
+    this.targetPath = targetPath
   }
 
-  static fromStats(stats: fsExtra.Stats, path: string, target?: ExtendedStats | null) {
-    const o = new ExtendedStats(path, target)
+  static fromStats({
+    stats,
+    path,
+    target,
+    targetPath,
+  }: {
+    stats: fsExtra.Stats
+  } & ExtendedStatsCtorParams) {
+    const o = new ExtendedStats({ path, target, targetPath })
     Object.assign(o, stats)
     return o
   }
@@ -245,7 +333,13 @@ export interface ResolveSymlinkParams {
   _resolvedPaths?: string[]
 }
 
+type StatsCallback = (err: NodeJS.ErrnoException | null, stats: fsExtra.Stats) => void
 type ExtendedStatsCallback = (err: NodeJS.ErrnoException | null, stats: ExtendedStats | null) => void
+type ResolveSymlinkCallback = (params: {
+  err: NodeJS.ErrnoException | null
+  target: ExtendedStats | null
+  targetPath: string | null
+}) => void
 
 /**
  * A helper class for getting information about files/dirs, that caches the stats for the lifetime of the class, and
@@ -253,8 +347,8 @@ type ExtendedStatsCallback = (err: NodeJS.ErrnoException | null, stats: Extended
  * The idea is for an instance to be used for the duration of e.g. one sync flow, but not for longer.
  */
 export class FileStatsHelper {
-  private lstatCache: { [path: string]: fsExtra.Stats }
-  private extendedStatCache: { [path: string]: ExtendedStats | null }
+  private readonly lstatCache: { [path: string]: fsExtra.Stats }
+  private readonly extendedStatCache: { [path: string]: ExtendedStats | null }
 
   constructor() {
     this.lstatCache = {}
@@ -264,7 +358,7 @@ export class FileStatsHelper {
   /**
    * Calls fs.lstat on the given path, and caches the result.
    */
-  lstat(path: string, cb: (err: NodeJS.ErrnoException | null, stats: fsExtra.Stats) => void) {
+  lstat(path: string, cb: StatsCallback) {
     if (this.lstatCache[path]) {
       cb(null, this.lstatCache[path])
     } else {
@@ -312,11 +406,14 @@ export class FileStatsHelper {
         if (lstatErr) {
           cb(lstatErr, null)
         } else if (lstats.isSymbolicLink()) {
-          this.resolveSymlink({ path, allowAbsolute: allowAbsoluteSymlinks }, (symlinkErr, target) => {
-            cb(symlinkErr, ExtendedStats.fromStats(lstats, path, target))
-          })
+          this.resolveSymlink(
+            { path, allowAbsolute: allowAbsoluteSymlinks },
+            ({ err: symlinkErr, target, targetPath }) => {
+              cb(symlinkErr, ExtendedStats.fromStats({ stats: lstats, path, target, targetPath }))
+            }
+          )
         } else {
-          cb(null, ExtendedStats.fromStats(lstats, path))
+          cb(null, ExtendedStats.fromStats({ stats: lstats, path }))
         }
       })
     }
@@ -351,15 +448,15 @@ export class FileStatsHelper {
    * fs.Stats for the resolved target, if one can be resolved. If target cannot be found, the callback is resolved
    * with a null value.
    *
+   * The second callback parameter, `targetPath`, is the unresolved target of the symbolic link. The `targetPath` will be undefined
+   * if the symbolic link is absolute, and absolute symbolic links are not allowed.
+   *
    * `path` must be an absolute path (an error is thrown otherwise).
    *
    * By default, absolute symlinks are ignored, and if one is encountered the method resolves to null. Set
    * `allowAbsolute: true` to allow absolute symlinks.
    */
-  resolveSymlink(
-    params: ResolveSymlinkParams,
-    cb: (err: NodeJS.ErrnoException | null, target: ExtendedStats | null) => void
-  ) {
+  resolveSymlink(params: ResolveSymlinkParams, cb: ResolveSymlinkCallback) {
     const { path, allowAbsolute } = params
     const _resolvedPaths = params._resolvedPaths || [path]
 
@@ -370,15 +467,19 @@ export class FileStatsHelper {
     readlink(path, (readlinkErr, target) => {
       if (readlinkErr?.code === "ENOENT") {
         // Symlink target not found, so we ignore it
-        return cb(null, null)
+        return cb({ err: null, target: null, targetPath: null })
       } else if (readlinkErr) {
-        return cb(InternalError.wrapError(readlinkErr, "Error reading symlink"), null)
+        return cb({
+          err: InternalError.wrapError(readlinkErr, "Error reading symlink"),
+          target: null,
+          targetPath: null,
+        })
       }
 
       // Ignore absolute symlinks unless specifically allowed
       // TODO: also allow limiting links to a certain absolute path
       if (isAbsolute(target) && !allowAbsolute) {
-        return cb(null, null)
+        return cb({ err: null, target: null, targetPath: null })
       }
 
       // Resolve the symlink path
@@ -387,25 +488,37 @@ export class FileStatsHelper {
       // Stat the final path and return
       this.lstat(targetPath, (statErr, targetStats) => {
         if (statErr?.code === "ENOENT") {
-          // Can't find the final target, so we ignore it
-          cb(null, null)
+          // The symlink target does not exist. That's not an error.
+          cb({ err: null, target: null, targetPath: target })
         } else if (statErr) {
           // Propagate other errors
-          cb(statErr, null)
+          cb({ err: statErr, target: null, targetPath: null })
         } else if (targetStats.isSymbolicLink()) {
           // Keep resolving until we get to a real path
           if (_resolvedPaths.includes(targetPath)) {
             // We've gone into a symlink loop, so we ignore it
-            cb(null, null)
+            cb({ err: null, target: null, targetPath: target })
           } else {
             this.resolveSymlink(
               { path: targetPath, allowAbsolute, _resolvedPaths: [..._resolvedPaths, targetPath] },
-              cb
+              ({ err: innerResolveErr, target: innerStats, targetPath: _innerTarget }) => {
+                if (innerResolveErr) {
+                  cb({ err: innerResolveErr, target: null, targetPath: null })
+                } else {
+                  // make sure the original symlink target is not overridden by the recursive search here
+                  // TODO(0.14): In a future version of garden it would be better to simply reproduce relative symlinks, instead of resolving them and copying the target directories.
+                  cb({ err: null, target: innerStats, targetPath: target })
+                }
+              }
             )
           }
         } else {
           // Return with path and stats for final symlink target
-          cb(null, ExtendedStats.fromStats(targetStats, targetPath))
+          cb({
+            err: null,
+            target: ExtendedStats.fromStats({ stats: targetStats, path: targetPath }),
+            targetPath: target,
+          })
         }
       })
     })

core/src/build-staging/build-staging.ts
@@ -271,6 +271,8 @@ export class BuildStaging {
       const to = targetShouldBeDirectory || targetStat?.isDirectory() ? join(targetPath, sourceBasename) : targetPath
 
       await syncFileAsync({
+        log,
+        sourceRoot,
         from: sourceRoot,
         to,
         allowDelete: withDelete,
@@ -323,7 +325,7 @@ export class BuildStaging {
               ([fromRelative, toRelative], fileCb) => {
                 const from = joinWithPosix(sourceRoot, fromRelative)
                 const to = joinWithPosix(targetPath, toRelative)
-                cloneFile({ from, to, allowDelete: withDelete, statsHelper }, fileCb)
+                cloneFile({ log, sourceRoot, from, to, allowDelete: withDelete, statsHelper }, fileCb)
               },
               cb
             )

commit 0d32c107a07644ba55cfae027d1d894979d5b1a7

fix(core): avoid crash when using `copyFrom` together with symlinks
This commit resolves an error "Failed copying a file because a directory
exists at the target path" which was a regression introduced in #6430.

Fixes #6456

core/src/build-staging/helpers.ts
@@ -112,8 +112,8 @@ export function cloneFile(
       }
 
       if (toStats) {
-        // If target is a directory or source is a symbolic link and deletes are allowed, delete the target before copying, otherwise throw
-        if (toStats.isDirectory() || fromStats.isSymbolicLink()) {
+        // If target is a directory and deletes are allowed, delete the target before copying, otherwise throw
+        if (toStats.isDirectory()) {
           if (allowDelete) {
             return remove(to, (removeErr) => {
               if (removeErr) {
@@ -131,10 +131,24 @@ export function cloneFile(
           }
         }
 
-        // Skip if both files exist, size and mtime is the same (to a precision of 2 decimal points)
-        if (fromStats.size === toStats.size && round(fromStats.mtimeMs, 2) === round(toStats.mtimeMs, 2)) {
+        if (fromStats.isSymbolicLink() && toStats.isSymbolicLink() && fromStats.targetPath === toStats.targetPath) {
+          // skip if both symlinks are equal
+          return done(null, { skipped: true })
+        } else if (fromStats.size === toStats.size && round(fromStats.mtimeMs, 2) === round(toStats.mtimeMs, 2)) {
+          // Skip if both files exist, size and mtime is the same (to a precision of 2 decimal points)
           return done(null, { skipped: true })
         }
+      }
+
+      // if the target path exists, and it is a symlink, we must remove it first
+      if (toStats?.isSymbolicLink()) {
+        return remove(to, (removeErr) => {
+          if (removeErr) {
+            return done(removeErr)
+          } else {
+            return doClone({ from, to, fromStats: fromStats!, done, statsHelper })
+          }
+        })
       }
 
       return doClone({ from, to, fromStats, done, statsHelper })
@@ -175,29 +189,29 @@ function doClone(params: CopyParams) {
     }
 
     if (fromStats.isSymbolicLink()) {
-      // reproduce the symbolic link. Validation happens before.
-      if (fromStats.targetPath) {
-        symlink(
-          fromStats.targetPath,
-          to,
-          // relevant on windows
-          // nodejs will auto-detect this on windows, but if the symlink is copied before the target then the auto-detection will get it wrong.
-          fromStats.target?.isDirectory() ? "dir" : "file",
-          (symlinkErr) => {
-            if (symlinkErr) {
-              return done(symlinkErr)
-            }
-
-            setUtimes()
-          }
-        )
-      } else {
+      if (!fromStats.targetPath) {
         return done(
           new InternalError({
             message: "Source is a symbolic link, but targetPath was null or undefined.",
           })
         )
       }
+
+      // reproduce the symbolic link
+      symlink(
+        fromStats.targetPath,
+        to,
+        // relevant on windows
+        // nodejs will auto-detect this on windows, but if the symlink is copied before the target then the auto-detection will get it wrong.
+        fromStats.target?.isDirectory() ? "dir" : "file",
+        (symlinkErr) => {
+          if (symlinkErr) {
+            return done(symlinkErr)
+          }
+
+          setUtimes()
+        }
+      )
     } else if (fromStats.isFile()) {
       // COPYFILE_FICLONE instructs the function to use a copy-on-write reflink on platforms/filesystems where available
       copyFile(from, to, constants.COPYFILE_FICLONE, (copyErr) => {
@@ -234,7 +248,9 @@ export async function scanDirectoryForClone(root: string, pattern?: string): Pro
 
   // No wildcards, so we just read and return the entire set of files from the source directory.
   if (!pattern) {
-    return (await readdir.readdirAsync(root, { deep: true, filter: (stats) => stats.isFile() })).map((f) => [f, f])
+    return (
+      await readdir.readdirAsync(root, { deep: true, filter: (stats) => stats.isFile() || stats.isSymbolicLink() })
+    ).map((f) => [f, f])
   }
 
   // We have a pattern to match, so we go into the more complex routine.
@@ -482,10 +498,10 @@ export class FileStatsHelper {
       }
 
       // Resolve the symlink path
-      const targetPath = resolve(parse(path).dir, target)
+      const absoluteTarget = resolve(parse(path).dir, target)
 
       // Stat the final path and return
-      this.lstat(targetPath, (statErr, toStats) => {
+      this.lstat(absoluteTarget, (statErr, toStats) => {
         if (statErr?.code === "ENOENT") {
           // The symlink target does not exist. That's not an error.
           cb({ err: null, target: null, targetPath: target })
@@ -494,12 +510,12 @@ export class FileStatsHelper {
           cb({ err: statErr, target: null, targetPath: null })
         } else if (toStats.isSymbolicLink()) {
           // Keep resolving until we get to a real path
-          if (_resolvedPaths.includes(targetPath)) {
+          if (_resolvedPaths.includes(absoluteTarget)) {
             // We've gone into a symlink loop, so we ignore it
             cb({ err: null, target: null, targetPath: target })
           } else {
             this.resolveSymlink(
-              { path: targetPath, allowAbsolute, _resolvedPaths: [..._resolvedPaths, targetPath] },
+              { path: absoluteTarget, allowAbsolute, _resolvedPaths: [..._resolvedPaths, absoluteTarget] },
               ({ err: innerResolveErr, target: innerStats, targetPath: _innerTarget }) => {
                 if (innerResolveErr) {
                   cb({ err: innerResolveErr, target: null, targetPath: null })
@@ -515,7 +531,7 @@ export class FileStatsHelper {
           // Return with path and stats for final symlink target
           cb({
             err: null,
-            target: ExtendedStats.fromStats({ stats: toStats, path: targetPath }),
+            target: ExtendedStats.fromStats({ stats: toStats, path: absoluteTarget }),
             targetPath: target,
           })
         }

core/test/unit/src/build-staging/build-staging.ts
@@ -24,6 +24,7 @@ import { BuildTask } from "../../../../src/tasks/build.js"
 import type { ConfigGraph } from "../../../../src/graph/config-graph.js"
 import type { BuildAction } from "../../../../src/actions/build.js"
 import { DOCS_BASE_URL } from "../../../../src/constants.js"
+import { lstat, readlink, symlink } from "fs/promises"
 
 // TODO-G2: rename test cases to match the new graph model semantics
 
@@ -66,9 +67,26 @@ async function assertIdentical(sourceRoot: string, targetRoot: string, posixPath
     posixPaths.map(async (path) => {
       const sourcePath = joinWithPosix(sourceRoot, path)
       const targetPath = joinWithPosix(targetRoot, path)
-      const sourceData = (await readFile(sourcePath)).toString()
-      const targetData = (await readFile(targetPath)).toString()
-      expect(sourceData).to.equal(targetData)
+
+      const sourceStat = await lstat(sourcePath)
+      const targetStat = await lstat(targetPath)
+
+      expect(sourceStat.isFile()).to.equal(targetStat.isFile(), `${targetStat} unexpected isFile()`)
+      expect(sourceStat.isDirectory()).to.equal(targetStat.isDirectory(), `${targetStat} unexpected isDirectory()`)
+      expect(sourceStat.isSymbolicLink()).to.equal(
+        targetStat.isSymbolicLink(),
+        `${targetStat} unexpected isSymbolicLink()`
+      )
+
+      if (sourceStat.isSymbolicLink() && targetStat.isSymbolicLink()) {
+        expect(await readlink(sourcePath)).to.equal(await readlink(targetPath), `${targetStat} unexpected readlink()`)
+      }
+
+      if (sourceStat.isFile() && targetStat.isFile()) {
+        const sourceData = (await readFile(sourcePath)).toString()
+        const targetData = (await readFile(targetPath)).toString()
+        expect(sourceData).to.equal(targetData, `${targetStat} unexpected readFile()`)
+      }
     })
   )
 }
@@ -120,6 +138,45 @@ describe("BuildStaging", () => {
 
       await assertIdentical(sourceRoot, targetRoot, ["a", "subdir/c"])
       expect(await listFiles(targetRoot)).to.eql(["a", "subdir/c"])
+    })
+
+    it("reproduces relative symlinks", async () => {
+      const sourceRoot = join(tmpPath, "source")
+
+      // prepare source
+      const files = ["node_modules/foo/bar", "node_modules/bar/baz"]
+      await ensureDir(sourceRoot)
+      await populateDirectory(sourceRoot, files)
+      await ensureDir(join(sourceRoot, "node_modules/.bin"))
+      const barLinkPath = "node_modules/.bin/bar"
+      const bazLinkPath = "node_modules/.bin/baz"
+      const nonExistentLinkPath = "node_modules/.bin/doesnotexist"
+      await symlink("../foo/bar", join(sourceRoot, barLinkPath))
+      await symlink("../bar/baz", join(sourceRoot, bazLinkPath))
+      await symlink("../this/does/not/exist", join(sourceRoot, nonExistentLinkPath))
+
+      // all the files in source
+      const expectedFiles = [...files, barLinkPath, bazLinkPath, nonExistentLinkPath]
+
+      // sync withDelete = true
+      let targetRoot = join(tmpPath, "target1")
+      await ensureDir(targetRoot)
+      // first time
+      await sync({ log, sourceRoot, targetRoot, withDelete: true })
+      await assertIdentical(sourceRoot, targetRoot, expectedFiles)
+      // second time
+      await sync({ log, sourceRoot, targetRoot, withDelete: true })
+      await assertIdentical(sourceRoot, targetRoot, expectedFiles)
+
+      // sync withDelete = false
+      targetRoot = join(tmpPath, "target2")
+      await ensureDir(targetRoot)
+      // first time
+      await sync({ log, sourceRoot, targetRoot, withDelete: false })
+      await assertIdentical(sourceRoot, targetRoot, expectedFiles)
+      // second time
+      await sync({ log, sourceRoot, targetRoot, withDelete: false })
+      await assertIdentical(sourceRoot, targetRoot, expectedFiles)
     })
 
     it("throws if source relative path is absolute", async () => {