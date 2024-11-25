40ea4a9536fc23b6d3ad0e8c49523f409c5410ee
Vladimir Vagaytsev <10628074+vvagaytsev@users.noreply.github.com>
2024-11-25 09:32:24 +0100

    refactor: cache own keys of variables

--- core/src/config/template-contexts/variable-scopes.ts
+++ core/src/config/template-contexts/variable-scopes.ts
@@ -9,15 +9,27 @@
 import type { DeepPrimitiveMap } from "../common.js"
 import { InternalError } from "../../exceptions.js"
 import { uniq } from "lodash-es"
+import { Memoize } from "typescript-memoize"
 
-export function createVariableScope(...variableSources: DeepPrimitiveMap[]): DeepPrimitiveMap {
+/**
+ * Creates a proxy object that emulates the merging of the given variables.
+ *
+ * It takes the given variable scopes and does lookup operations in the order of the scope appearance,
+ * instead of merging the given variable scopes physically.
+ *
+ * The first scope in the input variable scopes takes the highest precedence.
+ *
+ * @param variableScopes the input variable scopes
+ */
+export function createVariableScope(...variableScopes: DeepPrimitiveMap[]): DeepPrimitiveMap {
+  let computedOwnKeys: readonly string[] | undefined
   const proxy = new Proxy({} as DeepPrimitiveMap, {
     get: (_target, key: string | symbol, _receiver: any) => {
       if (typeof key === "symbol") {
         return undefined
       }
 
-      for (const variableSource of variableSources) {
+      for (const variableSource of variableScopes) {
         if (key in variableSource) {
           return variableSource[key]
         }
@@ -31,7 +43,10 @@ export function createVariableScope(...variableSources: DeepPrimitiveMap[]): Dee
     },
 
     ownKeys() {
-      return uniq(variableSources.flatMap(Object.keys))
+      if (!computedOwnKeys) {
+        computedOwnKeys = uniq(variableScopes.flatMap(Object.keys))
+      }
+      return computedOwnKeys
     },
 
     getOwnPropertyDescriptor(_target, key) {
