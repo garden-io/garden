7da347ffee0140860e3635f3f6b8ce68bdfd5716
Emanuele Libralato <e.libralato@gmail.com>
2025-01-21 09:34:23 +0100

    fix: renaming files before uploading in script

--- scripts/sign-windows-binary.sh
+++ scripts/sign-windows-binary.sh
@@ -3,9 +3,10 @@
 # Initialize variables
 aws_profile=""
 file_path=""
+version=""
 
 # Loop through arguments
-# We expect a profile name and a file path as arguments
+# We expect a profile name, a file path and a release version as arguments
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
         --profile)
@@ -14,6 +15,15 @@ while [[ "$#" -gt 0 ]]; do
                 shift 2
             else
                 echo "Error: --profile requires a value." >&2
+                exit 1
+            fi
+            ;;
+        --version)
+            if [[ -n "$2" && "$2" != --* ]]; then
+                version="$2"
+                shift 2
+            else
+                echo "Error: --version requires a value." >&2
                 exit 1
             fi
             ;;
@@ -35,21 +45,33 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 # Ensure both options are provided
-if [[ -z "$aws_profile" || -z "$file_path" ]]; then
-    echo "Both --profile and --file options must be provided."
-    echo "Usage: $0 --profile <profile-name> --file <file-path>"
+if [[ -z "$aws_profile" || -z "$file_path" || -z "$version" ]]; then
+    echo "All --profile, --file and --version options must be provided."
+    echo "Usage: $0 --profile <profile-name> --file <file-path> --version <version>"
     exit 1
 fi
-
 
 s3_bucket=ib-signing-for-garden # s3 bucket name
 file_name=$(basename $file_path) # extract filename from full path
 file_folder=$(dirname $file_path) # extract folder from full path
 
+
+# Rename file based on the version
+# We want to make sure the file name is unique before uploading it to the signing bucket, in order to avoid
+# downloading an old file with the same name.
+if [[ "$version" == "edge-bonsai" ]]; then
+  file_name="garden-edge-bonsai-${CIRCLE_SHA1}.exe"
+  mv $file_path "$file_folder/$file_name"
+else
+  file_name="garden-$version.exe"
+  mv $file_path "$file_folder/$file_name"
+fi
+
+
+
 # Upload file to S3
 echo Starting upload of $file_path to signing bucket
-
-aws s3 cp $file_path s3://$s3_bucket/ --profile $aws_profile
+aws s3 cp $file_folder/$file_name s3://$s3_bucket/ --profile $aws_profile
 
 # Check if signed file exists
 check_for_signed_file=1
@@ -68,4 +90,8 @@ done
 echo "File successfully signed. Downloading signed file."
 aws s3 cp s3://$s3_bucket/signed/$file_name $file_folder/ --profile $aws_profile
 
-echo "Signed file downloaded to $file_folder/$file_name"
+# Rename file back to original name
+mv $file_folder/$file_name $file_path
+
+echo "Signed file downloaded to $file_path"
+echo "Done"
