331fe0b99c4f67fb4e4a615eb52903b2d2cb75ff
Steffen Neubauer <steffen@garden.io>
2025-01-22 13:44:52 +0100

    fix: add work-around for nodejs exit 0 crash

--- core/test/setup.ts
+++ core/test/setup.ts
@@ -13,11 +13,23 @@ import { getDefaultProfiler } from "../src/util/profiling.js"
 import { gardenEnv } from "../src/constants.js"
 import { testFlags } from "../src/util/util.js"
 import { initTestLogger, testProjectTempDirs } from "./helpers.js"
-
+import mocha from "mocha"
 import sourceMapSupport from "source-map-support"
+import { UnresolvedTemplateValue } from "../src/template/types.js"
 sourceMapSupport.install()
 
 initTestLogger()
+
+// Work-around for nodejs crash with exit code 0
+// This happens when unresolved template values are involved in expect(), it tries to render a diff which triggers the
+// "objectSpreadTrap" error and that causes nodejs to exit with code 0 for a mysterious reason.
+const origCanonicalize = mocha.utils.canonicalize
+mocha.utils.canonicalize = function (value, stack, typeHint) {
+  if (value instanceof UnresolvedTemplateValue) {
+    return `[${value.toString()}]`
+  }
+  return origCanonicalize(value, stack, typeHint)
+}
 
 // Global hooks
 export const mochaHooks = {
