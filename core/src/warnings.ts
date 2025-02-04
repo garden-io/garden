2022e0b5a8567b47577fe4515fc2cf6601260aad
Vladimir Vagaytsev <10628074+vvagaytsev@users.noreply.github.com>
2025-02-04 15:53:32 +0100

    chore: helper function to handle deprecated feature usages

--- core/src/warnings.ts
+++ core/src/warnings.ts
@@ -7,6 +7,10 @@
  */
 
 import type { Log } from "./logger/log-entry.js"
+import { dedent, deline } from "./util/string.js"
+import { DOCS_BASE_URL, GardenApiVersion } from "./constants.js"
+import { ConfigurationError } from "./exceptions.js"
+import { styles } from "./logger/styles.js"
 
 interface LoggerContext {
   readonly history: Set<string>
@@ -27,4 +31,41 @@ export function emitNonRepeatableWarning(log: Log, message: string) {
 
   log.warn(message)
   loggerContext.history.add(message)
+}
+
+type DeprecationWarningParams = {
+  apiVersion: GardenApiVersion
+  log: Log
+  featureDesc: string
+  hint?: string
+}
+
+const migrationGuideReference = deline`
+See Garden 0.14 Migration Guide at ${styles.link(`${DOCS_BASE_URL}/guides/migrating-to-0.14`)} for more details on the migration from 0.13 to 0.14.
+`
+
+function makeWarningFor_0_13({ featureDesc, hint }: { featureDesc: string; hint?: string }): string {
+  return dedent`
+  ${styles.bold("!!!!!!!!!!!!!!!!!!!! [DEPRECATION WARNING] !!!!!!!!!!!!!!!!!!!!")}
+  ${featureDesc} is deprecated in 0.13 and will be removed in 0.14.${!!hint ? `\n${hint}` : ""}
+  ${migrationGuideReference}
+  `
+}
+
+function makeErrorFor_0_14({ featureDesc, hint }: { featureDesc: string; hint?: string }): string {
+  return dedent`
+  ${featureDesc} has been removed in 0.14.${!!hint ? `\n${hint}` : ""}
+  ${migrationGuideReference}
+  `
+}
+
+export function reportDeprecatedFeatureUsage({ apiVersion, log, featureDesc, hint }: DeprecationWarningParams) {
+  if (apiVersion === GardenApiVersion.v2) {
+    const message = makeErrorFor_0_14({ featureDesc, hint })
+    // TODO: consider a separate error type?
+    throw new ConfigurationError({ message })
+  }
+
+  const warnMessage = makeWarningFor_0_13({ featureDesc, hint })
+  emitNonRepeatableWarning(log, warnMessage)
 }
