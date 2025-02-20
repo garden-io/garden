garden-io/garden

commit 4a2cd9c25a6b0ad5c9a42b3c26649797ef1a7816

fix(dev): fix unresolved templates in cmd results (#6850)
* fix(dev): fix warning message during init

* fix(dev): fix unresolved templates in cmd results

We were including the raw graph results in the results of commands
requested over HTTP from the live page. This was always unnecessary (and
the data structures involved were excessively large), but only started
resulting in errors with our recent templating improvements (previously,
we'd just leave the unresolved template strings in there).

We can clean this up further in 0.14 when we get rid of `graphResults`
from `ProcessCommandResult` (which will require fixing a bunch of test
cases which currently use that field).

core/src/commands/base.ts
@@ -35,7 +35,7 @@ import type { DeployState, ForwardablePort, ServiceIngress } from "../types/serv
 import { deployStates, forwardablePortSchema, serviceIngressSchema } from "../types/service.js"
 import type { GraphResultMapWithoutTask, GraphResultWithoutTask, GraphResults } from "../graph/results.js"
 import { splitFirst } from "../util/string.js"
-import type { ActionMode } from "../actions/types.js"
+import { actionStates, type ActionMode, type ActionState } from "../actions/types.js"
 import type { AnalyticsHandler } from "../analytics/analytics.js"
 import { withSessionContext } from "../util/open-telemetry/context.js"
 import { wrapActiveSpan } from "../util/open-telemetry/spans.js"
@@ -699,7 +699,7 @@ ${renderCommands(commands)}
 
 // fixme: These interfaces and schemas are mostly copied from their original locations. This is to ensure that
 // dynamically sized or nested fields don't accidentally get introduced to command results. We should find a neater
-// wat to manage all this.
+// way to manage all this.
 
 interface BuildResultForExport extends ProcessResultMetadata {
   buildLog?: string
@@ -730,6 +730,7 @@ interface DeployResultForExport extends ProcessResultMetadata {
   lastMessage?: string
   lastError?: string
   outputs?: PrimitiveMap
+  // TODO-0.14: Rename to deployState
   state: DeployState
 }
 
@@ -756,6 +757,7 @@ const deployResultForExportSchema = createSchema({
     lastError: joi.string().description("Latest error status message of the service (if any)."),
     outputs: joiVariables().description("A map of values output from the deployment."),
     runningReplicas: joi.number().description("How many replicas of the service are currently running."),
+    // TODO-0.14: Rename to deployState
     state: joi
       .string()
       .valid(...deployStates)
@@ -790,8 +792,8 @@ interface TestResultForExport extends ProcessResultMetadata {
 
 const testResultForExportSchema = createSchema({
   name: "test-result-for-export",
-  keys: () => ({}),
   extend: runResultForExportSchema,
+  keys: () => ({}),
 })
 
 export type ProcessResultMetadata = {
@@ -800,20 +802,21 @@ export type ProcessResultMetadata = {
   success: boolean
   error?: string
   inputVersion: string | null
+  actionState: ActionState
 }
 
 export interface ProcessCommandResult {
   aborted: boolean
   success: boolean
-  graphResults: GraphResultMapWithoutTask // TODO: Remove this.
+  graphResults: GraphResultMapWithoutTask // TODO: Remove this in 0.14.
   build: { [name: string]: BuildResultForExport }
   builds: { [name: string]: BuildResultForExport }
   deploy: { [name: string]: DeployResultForExport }
-  deployments: { [name: string]: DeployResultForExport } // alias for backwards-compatibility
+  deployments: { [name: string]: DeployResultForExport } // alias for backwards-compatibility (remove in 0.14)
   test: { [name: string]: TestResultForExport }
   tests: { [name: string]: TestResultForExport }
   run: { [name: string]: RunResultForExport }
-  tasks: { [name: string]: RunResultForExport } // alias for backwards-compatibility
+  tasks: { [name: string]: RunResultForExport } // alias for backwards-compatibility (remove in 0.14)
 }
 
 export const resultMetadataKeys = () => ({
@@ -831,6 +834,7 @@ export const resultMetadataKeys = () => ({
     .description(
       "Alias for `inputVersion`. The version of the task's inputs, before any resolution or execution happens. For action tasks, this will generally be the unresolved version."
     ),
+  actionState: joi.string().valid(...actionStates),
   outputs: joiVariables().description("A map of values output from the action's execution."),
 })
 
@@ -842,11 +846,11 @@ export const processCommandResultSchema = createSchema({
     // Hide this field from the docs, since we're planning to remove it.
     graphResults: joi.any().meta({ internal: true }),
     build: joiIdentifierMap(buildResultForExportSchema().keys(resultMetadataKeys()))
-      .description("A map of all executed Builds (or Builds scheduled/attempted) and information about the them.")
+      .description("A map of all executed Builds (or Builds scheduled/attempted) and information about them.")
       .meta({ keyPlaceholder: "<Build name>" }),
     builds: joiIdentifierMap(buildResultForExportSchema().keys(resultMetadataKeys()))
       .description(
-        "Alias for `build`. A map of all executed Builds (or Builds scheduled/attempted) and information about the them."
+        "Alias for `build`. A map of all executed Builds (or Builds scheduled/attempted) and information about them."
       )
       .meta({ keyPlaceholder: "<Build name>", deprecated: true }),
     deploy: joiIdentifierMap(deployResultForExportSchema().keys(resultMetadataKeys()))
@@ -929,8 +933,8 @@ function prepareBuildResult(graphResult: GraphResultWithoutTask): BuildResultFor
   if (buildResult) {
     return {
       ...common,
-      buildLog: buildResult && buildResult.buildLog,
-      fresh: buildResult && buildResult.fresh,
+      buildLog: buildResult.buildLog,
+      fresh: buildResult.fresh,
     }
   } else {
     return common
@@ -976,7 +980,9 @@ function prepareDeployResult(graphResult: GraphResultWithoutTask): DeployResultF
 }
 
 function prepareTestResult(graphResult: GraphResultWithoutTask): TestResultForExport & ProcessResultMetadata {
-  const common = commonResultFields(graphResult)
+  const common = {
+    ...commonResultFields(graphResult),
+  }
   const detail = graphResult.result?.detail
   if (detail) {
     return {
@@ -992,7 +998,9 @@ function prepareTestResult(graphResult: GraphResultWithoutTask): TestResultForEx
 }
 
 function prepareRunResult(graphResult: GraphResultWithoutTask): RunResultForExport & ProcessResultMetadata {
-  const common = commonResultFields(graphResult)
+  const common = {
+    ...commonResultFields(graphResult),
+  }
   const detail = graphResult.result?.detail
   if (detail) {
     return {
@@ -1016,6 +1024,7 @@ function commonResultFields(graphResult: GraphResultWithoutTask) {
     inputVersion: graphResult.inputVersion,
     // Here for backwards-compatibility
     version: graphResult.inputVersion,
+    actionState: graphResult.result.state as ActionState,
   }
 }
 
@@ -1051,7 +1060,8 @@ export async function handleProcessResults(
   const result: ProcessCommandResult = {
     aborted: false,
     success,
-    graphResults: graphResultsForExport, // TODO: Remove this.
+    // TODO-0.14: Remove graphResults from this type (will also require refactoring test cases that read from this field)
+    graphResults: graphResultsForExport,
     build: buildResults,
     builds: buildResults, // alias for `build`
     deploy: deployResults,

core/src/actions/base.ts
@@ -54,7 +54,7 @@ import type {
   ResolvedAction,
   ResolvedActionWrapperParams,
 } from "./types.js"
-import { actionKinds, actionStateTypes } from "./types.js"
+import { actionKinds, actionStates } from "./types.js"
 import { baseInternalFieldsSchema, varfileDescription } from "../config/base.js"
 import type { DeployAction } from "./deploy.js"
 import type { TestAction } from "./test.js"
@@ -298,7 +298,7 @@ export const actionStatusSchema = createSchema({
   keys: () => ({
     state: joi
       .string()
-      .allow(...actionStateTypes)
+      .allow(...actionStates)
       .only()
       .required()
       .description("The state of the action."),

core/src/plugin/base.ts
@@ -182,14 +182,15 @@ export const artifactsPathSchema = memoize(() =>
   joi.string().required().description("A directory path where the handler should write any exported artifacts to.")
 )
 
-export type RunState = "outdated" | "unknown" | "running" | "succeeded" | "failed" | "not-implemented"
+export const runStates = ["outdated", "unknown", "running", "succeeded", "failed", "not-implemented"] as const
+export type RunState = (typeof runStates)[number]
 
 export interface RunStatusForEventPayload {
   state: RunState
 }
 
 export const outputSchemaDocs = dedent`
-  The schema must be a single level object, with string keys. Each value must be a primitive (null, boolean, number or string).
+  The schema must be a single level object, with string keys. Each vaue must be a primitive (null, boolean, number or string).
 
   If no schema is provided, an error may be thrown if a plugin handler attempts to return an output key.
 

core/src/actions/types.ts
@@ -112,8 +112,8 @@ export interface ActionConfigTypes {
  *
  * See https://melvingeorge.me/blog/convert-array-into-string-literal-union-type-typescript
  */
-export const actionStateTypes = ["ready", "not-ready", "processing", "failed", "unknown"] as const
-export type ActionState = (typeof actionStateTypes)[number]
+export const actionStates = ["ready", "not-ready", "processing", "failed", "unknown"] as const
+export type ActionState = (typeof actionStates)[number]
 
 export interface ActionStatus<
   T extends BaseAction = BaseAction,

core/src/commands/get/get-actions.ts
@@ -8,7 +8,7 @@
 
 import { getActionState, getRelativeActionConfigPath } from "../../actions/helpers.js"
 import type { ActionKind, ActionState, ResolvedAction } from "../../actions/types.js"
-import { actionKinds, actionStateTypes } from "../../actions/types.js"
+import { actionKinds, actionStates } from "../../actions/types.js"
 import { BooleanParameter, ChoicesParameter, StringsParameter } from "../../cli/params.js"
 import { createSchema, joi, joiArray } from "../../config/common.js"
 import { printHeader } from "../../logger/util.js"
@@ -50,7 +50,7 @@ export const getActionsCmdOutputSchema = createSchema({
     type: joi.string().required().description(`Action Type (e.g. 'container').`),
     state: joi
       .string()
-      .allow(...actionStateTypes)
+      .allow(...actionStates)
       .only()
       .description("The state of the action."),
     path: joi.string().description("The relative path of the action config file."),

core/src/commands/serve.ts
@@ -186,6 +186,7 @@ export class ServeCommand<
             const distroName = getCloudDistributionName(defaultGarden.cloudDomain)
             const livePageUrl = cloudApi.getLivePageUrl({ shortId: session.shortId }).toString()
             const msg = dedent`\n${printEmoji("🌸", log)}Connected to ${distroName} ${printEmoji("🌸", log)}
+
               Follow the link below to stream logs, run commands, and more from the Garden dashboard ${printEmoji(
                 "👇",
                 log

core/src/events/action-status-events.ts
@@ -6,7 +6,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { actionStateTypes } from "../actions/types.js"
+import { actionStates } from "../actions/types.js"
 import type { BuildState } from "../plugin/handlers/Build/get-status.js"
 import type { ActionRuntime, RunState } from "../plugin/plugin.js"
 import type { DeployState } from "../types/service.js"
@@ -18,7 +18,7 @@ export type ActionStatusDetailedState = DeployState | BuildState | RunState
  * These are the states emitted in status events. Here, we include additional states to help distinguish status event
  * emitted around status/cache checks VS statuses emitted around the execution after a failed status check.
  */
-const actionStateTypesForEvent = [...actionStateTypes, "getting-status", "cached"] as const
+const actionStateTypesForEvent = [...actionStates, "getting-status", "cached"] as const
 /**
  * This type represents the lifecycle of an individual action execution as emitted to Cloud. Note that the
  * internal semantics are slightly different (e.g. Garden uses "ready" instead of "cached" internally).

core/src/plugin/handlers/Build/get-status.ts
@@ -21,7 +21,8 @@ import type { ActionStatus, ActionStatusMap, Resolved } from "../../../actions/t
  * - `built`: The build was completed successfully.
  * - `failed`: An error occurred while fetching or building.
  */
-export type BuildState = "fetching" | "fetched" | "outdated" | "building" | "built" | "failed" | "unknown"
+export const buildStates = ["fetching", "fetched", "outdated", "building", "built", "failed", "unknown"] as const
+export type BuildState = (typeof buildStates)[number]
 
 export interface BuildStatusForEventPayload {
   state: BuildState

commit 7a1689b79423d5fb76b78042514d9239cfbc74df

fix: fix regression in cmd result processing
This should fix a regression introduced in
https://github.com/garden-io/garden/pull/6850.

Because we added a lookup into `graphResult.result` and
`graphResult.result` can be null in certain circumstances (e.g. when the
action fails), we needed to guard against this (since we don't want an
internal error for such cases).

core/src/commands/base.ts
@@ -1024,7 +1024,7 @@ function commonResultFields(graphResult: GraphResultWithoutTask) {
     inputVersion: graphResult.inputVersion,
     // Here for backwards-compatibility
     version: graphResult.inputVersion,
-    actionState: graphResult.result.state as ActionState,
+    actionState: graphResult.result ? (graphResult.result.state as ActionState) : "unknown",
   }
 }
 