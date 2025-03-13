117174ef0174ef2148f40bd8180ed7084d4387f5
Vladimir Vagaytsev <10628074+vvagaytsev@users.noreply.github.com>
2025-03-13 20:30:23 +0100

    chore: remove duplicate license header

--- core/src/plugins/kubernetes/results-cache.ts
+++ core/src/plugins/kubernetes/results-cache.ts
@@ -12,13 +12,6 @@ import { currentResultSchemaVersion, kubernetesCacheableResultSchema } from "./r
 
 let resultCache: LocalResultCache<CacheableRunAction | CacheableTestAction, CacheableResult> | undefined
 
-/*
- * Copyright (C) 2018-2024 Garden Technologies, Inc. <info@garden.io>
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
 export function getResultCache(
   gardenDirPath: string
 ): LocalResultCache<CacheableRunAction | CacheableTestAction, CacheableResult> {
