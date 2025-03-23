2ef44357ec5d533c9e620b70861d4d5e33b6b317
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2025-03-23 18:27:30 +0200

    improvement(core): fixes to output reference logic
    We now infer an `executed: true`  dependency for action output
    references when the referenced action is a Build, regardless of whether
    the output is a static or a runtime output.
    
    This improves the user experience for the common use-case of referencing
    a container image in a runtime resource (like a `helm` Deploy), where
    the user intent is almost always that the referenced build should exist
    (i.e. the dependency should be processed) before the runtime resource is
    processed (i.e. deployed or run).
    
    This is because Builds generally don't have side-effects other than
    producing artifacts, whereas Deploys and Runs often do.
    
    Note: We could also always execute Test actions that are referenced,
    but we'll stick with only Builds for now.

--- core/test/unit/src/graph/actions.ts
+++ core/test/unit/src/graph/actions.ts
@@ -7,13 +7,15 @@
  */
 
 import type { TestGarden } from "../../../helpers.js"
-import { expectError, makeGarden, makeTempDir, noOpTestPlugin } from "../../../helpers.js"
+import { customizedTestPlugin, expectError, makeGarden, makeTempDir, noOpTestPlugin } from "../../../helpers.js"
 import { preprocessActionConfig } from "../../../../src/graph/actions.js"
 import type { RunActionConfig } from "../../../../src/actions/run.js"
 import { DEFAULT_RUN_TIMEOUT_SEC } from "../../../../src/constants.js"
 import type tmp from "tmp-promise"
 import { expect } from "chai"
 import { parseTemplateCollection } from "../../../../src/template/templated-collections.js"
+import type { BuildActionConfig } from "../../../../src/actions/build.js"
+import { joi } from "../../../../src/config/common.js"
 
 describe("preprocessActionConfig", () => {
   let tmpDir: tmp.DirectoryResult
@@ -21,6 +23,9 @@ describe("preprocessActionConfig", () => {
 
   before(async () => {
     tmpDir = await makeTempDir({ git: true, initialCommit: false })
+  })
+
+  beforeEach(async () => {
     garden = await makeGarden(tmpDir, noOpTestPlugin())
   })
 
@@ -69,6 +74,152 @@ describe("preprocessActionConfig", () => {
   })
 
   context("template strings", () => {
+    context("implicit dependencies inferred from actoin output references", () => {
+      context("a static output is referenced", () => {
+        it("should inject an executed=true dependency when the ref is a Build", async () => {
+          garden = await makeGarden(
+            tmpDir,
+            customizedTestPlugin({
+              name: "test",
+              createActionTypes: {
+                Build: [
+                  {
+                    name: "test",
+                    docs: "Test Build action with static output",
+                    schema: joi.object(),
+                    handlers: {},
+                    staticOutputsSchema: joi.object().keys({ someOutput: joi.string() }),
+                  },
+                ],
+              },
+            })
+          )
+
+          const depBuildConfig: BuildActionConfig = parseTemplateCollection({
+            value: {
+              internal: { basePath: tmpDir.path },
+              timeout: DEFAULT_RUN_TIMEOUT_SEC,
+              kind: "Build" as const,
+              type: "test",
+              name: "build-dep",
+              variables: {},
+              spec: { command: ["echo", "build-dep"] },
+            },
+            source: { path: [] },
+          })
+          const runConfig: RunActionConfig = parseTemplateCollection({
+            value: {
+              internal: { basePath: tmpDir.path },
+              timeout: DEFAULT_RUN_TIMEOUT_SEC,
+              kind: "Run" as const,
+              type: "exec",
+              name: "with-refs",
+              variables: {},
+              spec: { command: ["echo", "${actions.build.build-dep.outputs.someOutput}"] },
+            },
+            source: { path: [] },
+          })
+
+          const router = await garden.getActionRouter()
+          const actionTypes = await garden.getActionTypes()
+          const definition = actionTypes[runConfig.kind][runConfig.type]?.spec
+
+          const res = await preprocessActionConfig({
+            garden,
+            config: runConfig,
+            configsByKey: { "run.with-refs": runConfig, "build.build-dep": depBuildConfig },
+            actionTypes,
+            definition,
+            router,
+            linkedSources: {},
+            mode: "default",
+            log: garden.log,
+          })
+
+          expect(res.dependencies).to.eql([
+            {
+              explicit: false,
+              kind: "Build",
+              name: "build-dep",
+              needsExecutedOutputs: true, // <-----
+              needsStaticOutputs: false, // <-----
+              type: "test",
+            },
+          ])
+        })
+        it("should not inject an executed=true dependency when the ref is a Run", async () => {
+          garden = await makeGarden(
+            tmpDir,
+            customizedTestPlugin({
+              name: "test",
+              createActionTypes: {
+                Run: [
+                  {
+                    name: "test",
+                    docs: "Run action with static output",
+                    schema: joi.object(),
+                    handlers: {},
+                    staticOutputsSchema: joi.object().keys({ someOutput: joi.string() }),
+                  },
+                ],
+              },
+            })
+          )
+
+          const depRunConfig: RunActionConfig = parseTemplateCollection({
+            value: {
+              internal: { basePath: tmpDir.path },
+              timeout: DEFAULT_RUN_TIMEOUT_SEC,
+              kind: "Run" as const,
+              type: "test",
+              name: "run-dep",
+              variables: {},
+              spec: { command: ["echo", "run-dep"] },
+            },
+            source: { path: [] },
+          })
+          const runConfig: RunActionConfig = parseTemplateCollection({
+            value: {
+              internal: { basePath: tmpDir.path },
+              timeout: DEFAULT_RUN_TIMEOUT_SEC,
+              kind: "Run" as const,
+              type: "exec",
+              name: "with-refs",
+              variables: {},
+              spec: { command: ["echo", "${actions.run.run-dep.outputs.someOutput}"] },
+            },
+            source: { path: [] },
+          })
+
+          const router = await garden.getActionRouter()
+          const actionTypes = await garden.getActionTypes()
+          const definition = actionTypes[runConfig.kind][runConfig.type]?.spec
+
+          const res = await preprocessActionConfig({
+            garden,
+            config: runConfig,
+            configsByKey: { "run.with-refs": runConfig, "run.run-dep": depRunConfig },
+            actionTypes,
+            definition,
+            router,
+            linkedSources: {},
+            mode: "default",
+            log: garden.log,
+          })
+
+          expect(res.dependencies).to.eql([
+            {
+              explicit: false,
+              kind: "Run",
+              name: "run-dep",
+              needsExecutedOutputs: false, // <-----
+              needsStaticOutputs: true, // <-----
+              type: "test",
+            },
+          ])
+        })
+      })
+    })
     context("include/exclude configs", () => {
       it("should resolve variables in 'exclude' config", async () => {
         const config: RunActionConfig = parseTemplateCollection({
