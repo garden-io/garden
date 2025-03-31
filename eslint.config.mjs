9d0fe49c72486fd828e443d98d8ba2bb763d09f7
Vladimir Vagaytsev <10628074+vvagaytsev@users.noreply.github.com>
2025-03-31 17:08:34 +0200

    chore: add license header

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2018-2024 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { defineConfig, globalIgnores } from "eslint/config"
 import unusedImports from "eslint-plugin-unused-imports"
 import mocha from "eslint-plugin-mocha"
