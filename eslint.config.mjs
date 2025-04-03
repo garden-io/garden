47853179d71ce945988afa0bca32dd3c3ba527c5
Vladimir Vagaytsev <10628074+vvagaytsev@users.noreply.github.com>
2025-04-03 14:23:03 +0200

    chore: fix header in linter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -126,7 +126,7 @@ export default defineConfig([
         "block",
         [
           "",
-          " * Copyright (C) 2018-2024 Garden Technologies, Inc. <info@garden.io>",
+          " * Copyright (C) 2018-2025 Garden Technologies, Inc. <info@garden.io>",
           " *",
           " * This Source Code Form is subject to the terms of the Mozilla Public",
           " * License, v. 2.0. If a copy of the MPL was not distributed with this",
