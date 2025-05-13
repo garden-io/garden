a78e02a767c9f2b87c1b0131f9417c6fae84b994
Steffen Neubauer <steffen@garden.io>
2025-05-13 21:57:05 +0200

    feat: first working stream implementation with test

--- core/src/cloud/grow/buffered-event-stream.ts
+++ core/src/cloud/grow/buffered-event-stream.ts
@@ -6,22 +6,32 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+import { timestampFromDate } from "@bufbuild/protobuf/wkt"
 import { monotonicFactory } from "ulid"
 import type { GardenWithNewBackend } from "../../garden.js"
-import { registerCleanupFunction } from "../../util/util.js"
+import { registerCleanupFunction, sleep } from "../../util/util.js"
 import type { Log } from "../../logger/log-entry.js"
-import type { EventName, Events, GardenEventAnyListener } from "../../events/events.js"
+import type { CommandInfoPayload, EventName, Events, GardenEventAnyListener } from "../../events/events.js"
 import { LogLevel } from "../../logger/logger.js"
 import type { LogEntryEventPayload } from "../buffered-event-stream.js"
 import type {
   Event as GrpcEvent,
   GardenEventIngestionService,
+  EventResponse,
 } from "@buf/garden_grow-platform.bufbuild_es/private/events/events_pb.js"
 import { EventContextSchema, EventSchema } from "@buf/garden_grow-platform.bufbuild_es/private/events/events_pb.js"
 import { create } from "@bufbuild/protobuf"
-import type { Client } from "@connectrpc/connect"
+import { ConnectError, type Client } from "@connectrpc/connect"
 import type { WritableIterable } from "@connectrpc/connect/protocol"
 import { createWritableIterable } from "@connectrpc/connect/protocol"
+import {
+  GardenCommandEvent_GardenProjectMetadataSchema,
+  GardenCommandEvent_GitMetadata_GitRemoteSchema,
+  GardenCommandEvent_GitMetadataSchema,
+  GardenCommandEvent_InvocationMetadataSchema,
+  GardenCommandEvent_Status,
+  GardenCommandEventSchema,
+} from "@buf/garden_grow-platform.bufbuild_es/private/events/garden-command/garden-command_pb.js"
 
 const nextEventUlid = monotonicFactory()
 
@@ -44,6 +54,7 @@ export class GrowBufferedEventStream {
 
   private outputStream: WritableIterable<GrpcEvent> | undefined
   private closed: boolean
+  private readonly closeCallbacks: (() => void)[] = []
 
   constructor({
     garden,
@@ -68,7 +79,7 @@ export class GrowBufferedEventStream {
         return
       }
 
-      void this.handleEvent("sessionCancelled", {})
+      this.handleEvent("sessionCancelled", {})
       this.close().catch(() => {})
     })
 
@@ -80,11 +91,28 @@ export class GrowBufferedEventStream {
     this.log.root.events.onAny(this.logListener)
 
     this.eventListener = async (name, payload) => {
-      await this.handleEvent(name, payload)
+      this.handleEvent(name, payload)
     }
     this.garden.events.onAny(this.eventListener)
 
-    this.log.silly(() => "BufferedEventStream: Connected")
+    setTimeout(async () => {
+      this.log.debug("GrowBufferedEventStream: Starting loop")
+
+      while (!this.closed) {
+        this.log.debug("GrowBufferedEventStream: Connecting ...")
+
+        try {
+          await this.streamEvents()
+        } catch (err) {
+          if (err instanceof ConnectError) {
+            this.log.debug(`GrowBufferedEventStream: Error while streaming events: ${err}`)
+          } else {
+            // This will become an unhandled error and will cause the process to crash.
+            throw err
+          }
+        }
+      }
+    }, 0)
   }
 
   async close() {
@@ -95,23 +123,37 @@ export class GrowBufferedEventStream {
     this.garden.events.offAny(this.eventListener)
     this.log.root.events.offAny(this.logListener)
 
-    this.closed = true
-
-    try {
-      // TODO: flush the data
-      // await this.flushAll()
-      // this.log.debug("Done flushing all events and log entries.")
-    } catch (err) {
-      /**
-       * We don't throw an exception here, since a failure to stream events and log entries doesn't mean that the
-       * command failed.
-       */
-      this.log.error(`Error while flushing events and log entries: ${err}`)
+    if (this.eventBuffer.size === 0) {
+      this.log.debug(
+        "GrowBufferedEventStream: Close called and no events waiting for acknowledgement. Disconnecting..."
+      )
+      this.closed = true
+      // close the connection as well
+      this.outputStream?.close()
+      return
     }
+
+    // there are still events in the buffer, we need to wait for them to be acknowledged
+    const promise = new Promise<void>((resolve) => {
+      this.closeCallbacks.push(resolve)
+    })
+
+    // wait max 10 seconds for the events to be acknowledged
+    await Promise.race([
+      promise,
+      (async () => {
+        await sleep(10000)
+        this.log.warn(
+          "Not all events were acknowledged within 10 seconds. Information in Garden Cloud may be incomplete."
+        )
+      })(),
+    ])
+
+    this.outputStream?.close()
+    this.closed = true
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async handleEvent<T extends EventName>(name: T, payload: Events[T]) {
+  private handleEvent<T extends EventName>(name: T, payload: Events[T]) {
     const event: GrpcEvent = create(EventSchema, {
       eventId: nextEventUlid(),
       context: create(EventContextSchema, {
@@ -120,31 +162,158 @@ export class GrowBufferedEventStream {
       }),
     })
 
+    if (name === "commandInfo") {
+      this.handleCommandStarted(event, payload as any) // TODO: fix type
+    }
+    if (name === "sessionCompleted") {
+      this.handleCommandCompleted(event)
+    }
+
+    if (event.eventData === undefined) {
+      this.log.debug(`GrowBufferedEventStream: Ignoring event ${name} (ulid=${event.eventId})`)
+      return
+    }
+
+    this.log.debug(
+      () =>
+        `GrowBufferedEventStream: ${this.outputStream ? "Sending" : "Buffering"} event ${event.eventData.case} (ulid=${event.eventId})`
+    )
+
     this.eventBuffer.set(event.eventId, event)
-    await this.outputStream?.write(event)
+    void this.outputStream?.write(event)
   }
 
-  async streamEvents() {
-    this.outputStream = createWritableIterable<GrpcEvent>()
-    // this.eventBuffer.values(async (e) => this.outputStream?.write(e))
+  private handleCommandCompleted(event: GrpcEvent) {
+    event.eventData = {
+      case: "commandEvent",
+      value: create(GardenCommandEventSchema, {
+        // TODO: use ulid
+        // TODO: add sessionId to the event payload, otherwise this will not behave as expected
+        commandId: this.garden.sessionId,
 
-    try {
-      const ackStream = this.client.ingestEventStream(this.outputStream)
-      for await (const nextAck of ackStream) {
-        if (!nextAck.success) {
-          this.log.debug(`Server failed to process event with ulid=${nextAck.eventId}`)
-        }
+        // completed now
+        completedAt: timestampFromDate(new Date()),
+        status: GardenCommandEvent_Status.SUCCEEDED,
+      }),
+    }
+  }
 
-        this.eventBuffer.delete(nextAck.eventId)
-      }
-    } finally {
-      this.outputStream.close()
-      this.outputStream = undefined
+  private handleCommandStarted(event: GrpcEvent, payload: CommandInfoPayload) {
+    event.eventData = {
+      case: "commandEvent",
+      value: create(GardenCommandEventSchema, {
+        // TODO: use ulid
+        commandId: payload.sessionId,
+
+        // started now
+        startedAt: timestampFromDate(new Date()),
+        status: GardenCommandEvent_Status.IN_PROGRESS,
+
+        // TODO: expose if it is a custom command
+        //isCustomCommand: payload.isCustomCommand,
+
+        invocationMetadata: create(GardenCommandEvent_InvocationMetadataSchema, {
+          cwd: process.cwd(),
+          command: payload.name,
+          // TODO: args
+          //args: payload.args,
+          // TODO: tty detection
+          //interactiveTty: payload.interactiveTty,
+        }),
+
+        gitMetadata: create(GardenCommandEvent_GitMetadataSchema, {
+          // TODO: repository root
+          //repositoryRoot: payload.repositoryRoot,
+          headRefSha: payload.vcsCommitHash,
+          // TODO: expose raw ref name, might be a tag
+          // TODO: might be optional if the head is detached
+          headRefName: `refs/heads/${payload.vcsBranch}`,
+          // TODO: expose all remotes with their original names
+          gitRemotes: [
+            create(GardenCommandEvent_GitMetadata_GitRemoteSchema, {
+              name: "origin",
+              url: payload.vcsOriginUrl,
+            }),
+          ],
+        }),
+
+        gardenProjectMetadata: create(GardenCommandEvent_GardenProjectMetadataSchema, {
+          projectName: payload.projectName,
+
+          // TODO: expose all project data
+
+          // projectApiVersion: payload.projectApiVersion,
+
+          // projectId: payload.projectId,
+          // projectRootDir: payload.projectRootDir,
+
+          // namespaceName: payload.namespaceName,
+          // namespaceId: payload.namespaceId,
+
+          // environmentName: payload.environmentName,
+          // environmentId: payload.environmentId,
+        }),
+      }),
     }
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  handleLogEntry(logEntry: LogEntryEventPayload) {
+  private handleLogEntry(logEntry: LogEntryEventPayload) {
     // TODO: logs handling
+  }
+
+  private async streamEvents() {
+    this.outputStream = createWritableIterable<GrpcEvent>()
+
+    const ackStream = this.client.ingestEventStream(this.outputStream)
+
+    this.log.debug(() => "GrowBufferedEventStream: Connected")
+
+    try {
+      // We concurrently consume the ack stream and flush the buffer of unacknowledged events.
+      // Consuming and writing sequentially can cause deadlocks
+      await Promise.all([this.consumeAcks(ackStream), this.flushEventBuffer()])
+    } finally {
+      this.outputStream?.close()
+      this.outputStream = undefined
+    }
+  }
+
+  private async consumeAcks(ackStream: AsyncIterable<EventResponse>) {
+    for await (const nextAck of ackStream) {
+      if (!nextAck.success) {
+        // NOTE: We expect the server to also close the stream in case of an error, so we can retry emitting events on the next attempt.
+        this.log.debug(`Server failed to process event with ulid=${nextAck.eventId}`)
+      }
+
+      // Remove acknowledged event from the buffer
+      this.log.debug(() => `GrowBufferedEventStream: Received ack for event ${nextAck.eventId}`)
+      this.eventBuffer.delete(nextAck.eventId)
+
+      if (this.closeCallbacks.length && this.eventBuffer.size === 0) {
+        this.log.debug("All events have been acknowledged. Disconnecting...")
+        for (const callback of this.closeCallbacks) {
+          // Call all the callbacks to notify that the stream is closed
+          callback()
+        }
+        this.closeCallbacks.length = 0
+        break
+      }
+    }
+  }
+
+  private async flushEventBuffer() {
+    if (this.eventBuffer.size === 0) {
+      return
+    }
+
+    this.log.debug(() => `GrowBufferedEventStream: Flushing ${this.eventBuffer.size} events from the buffer`)
+    for (const event of this.eventBuffer.values()) {
+      if (!this.outputStream) {
+        this.log.debug(() => `GrowBufferedEventStream: Stream closed during flush`)
+        break
+      }
+      await this.outputStream.write(event)
+    }
   }
 }
